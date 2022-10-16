c4e2eb11eb3ca75866fadafc676eb8a2a6b9726d
rosenrose <crazytempler@gmail.com>
2022-10-16 22:33:07 +0900

    2839 설탕 배달

---
+++ rust/src/2839.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+    let mut counts = Vec::new();
+
+    for count_3kg in 0..=n / 3 {
+        for count_5kg in 0..=(n - count_3kg * 3) / 5 {
+            if (count_3kg * 3) + (count_5kg * 5) != n {
+                continue;
+            }
+
+            counts.push(count_3kg + count_5kg);
+        }
+    }
+
+    let result = if counts.is_empty() {
+        -1
+    } else {
+        *counts.iter().min().unwrap()
+    };
+
+    println!("{result}");
+}
