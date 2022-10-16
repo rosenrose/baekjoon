de87716abc8831c65ba7509fd41473b52cc3253d
rosenrose <crazytempler@gmail.com>
2022-10-16 23:47:22 +0900

    10989 수 정렬하기 3

---
+++ rust/src/10989.rs
@@ -0,0 +1,38 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+    const MAX_NUM: usize = 10000;
+
+    let mut count = [0; MAX_NUM + 1];
+    let (mut max, mut min) = (0, MAX_NUM);
+
+    for _ in 0..n {
+        buf.clear();
+        stdin.read_line(&mut buf).unwrap();
+        let num: usize = buf.trim().parse().unwrap();
+
+        count[num] += 1;
+
+        if num > max {
+            max = num;
+        }
+        if num < min {
+            min = num;
+        }
+    }
+
+    for num in min..=max {
+        for _ in 0..count[num] {
+            writeln!(stdout, "{num}").unwrap();
+        }
+    }
+}
