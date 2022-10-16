3afe54cf1cc291b6f2f69c54b064f9b4c1b4415a
rosenrose <crazytempler@gmail.com>
2022-10-16 19:15:52 +0900

    10950 A+B - 3

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,1 +1,14 @@
 fn main() {}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int(buf: &String) -> i32 {
+    buf.trim().parse().unwrap()
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
