ebd2b12d6238c3b3dec20f57c3804636884be54d
rosenrose <crazytempler@gmail.com>
2022-10-16 21:45:20 +0900

    2908 상수

---
+++ rust/src/2908.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let nums = buf.split_whitespace().map(|num| {
+        num.chars()
+            .rev()
+            .collect::<String>()
+            .parse::<i32>()
+            .unwrap()
+    });
+
+    println!("{}", nums.max().unwrap());
+}
