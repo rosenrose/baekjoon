b93e6ac62eb5b2aa3a3d5f5c27be4bee285f1bd2
rosenrose <crazytempler@gmail.com>
2022-10-17 00:11:53 +0900

    2108 통계학

---
+++ rust/src/2108.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    let mut arr: Vec<i32> = (0..n)
+        .map(|_| {
+            buf.clear();
+            stdin.read_line(&mut buf).unwrap();
+
+            buf.trim().parse::<i32>().unwrap()
+        })
+        .collect();
+
+    arr.sort();
+
+    let len = arr.len();
+    let middle = arr[len / 2];
+
+    const N: i32 = 4000;
+    let mut sum = 0;
+    let (mut min, mut max) = (N, -N);
+    let mut counts = HashMap::new();
+
+    for &num in arr.iter() {
+        sum += num;
+        if num < min {
+            min = num;
+        }
+        if num > max {
+            max = num;
+        }
+
+        counts.entry(num).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    let avg = sum as f64 / len as f64;
+
+    let max_count = counts.values().max().unwrap();
+    let mut max_counts: Vec<(&i32, &i32)> =
+        counts.iter().filter(|&(_, c)| c == max_count).collect();
+
+    let (most, _) = if max_counts.len() > 1 {
+        max_counts.sort_by_key(|i| i.0);
+        max_counts[1]
+    } else {
+        max_counts[0]
+    };
+
+    for value in [avg.round() as i32, middle, *most, max - min] {
+        writeln!(stdout, "{value}").unwrap();
+    }
+}
