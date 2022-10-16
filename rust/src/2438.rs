5c3c6a3055a3f3d731529d61ab2810f23002d4a4
rosenrose <crazytempler@gmail.com>
2022-10-16 19:37:28 +0900

    2438 별 찍기 - 1

---
+++ rust/src/2438.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: usize = buf.trim().parse().unwrap();
+
+    for i in 1..=n {
+        println!("{}", "*".repeat(i));
+    }
+}
