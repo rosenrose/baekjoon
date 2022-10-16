425ef02274df23d104f1b415bbad4c02cce7bc99
rosenrose <crazytempler@gmail.com>
2022-10-16 19:11:19 +0900

    2739 구구단

---
+++ rust/src/2739.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for i in 1..=9 {
+        println!("{n} * {i} = {}", n * i);
+    }
+}
