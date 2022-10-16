83c458d3bea5449784935973a14837aaf13ff353
rosenrose <crazytempler@gmail.com>
2022-10-16 21:39:28 +0900

    1157 단어 공부

---
+++ rust/src/1157.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let word = buf.trim().to_uppercase();
+    let letters: HashSet<char> = word.chars().collect();
+
+    let (mut most_used, mut max) = (' ', 0);
+    let mut is_duplicate = false;
+
+    for letter in letters.iter() {
+        let count = word.chars().filter(|c| c == letter).count();
+
+        if count == max {
+            is_duplicate = true;
+        }
+
+        if count > max {
+            is_duplicate = false;
+            (most_used, max) = (*letter, count);
+        }
+    }
+
+    if is_duplicate {
+        println!("?");
+        return;
+    }
+
+    println!("{most_used}");
+}
