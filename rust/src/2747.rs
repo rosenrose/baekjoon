a3935e555f15ed20c9704a9713efe4686f60aefb
rosenrose <crazytempler@gmail.com>
2022-10-17 00:41:17 +0900

    2747 피보나치 수

---
+++ rust/src/2747.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    println!("{}", fibo(n));
+}
+
+fn fibo(n: i32) -> i32 {
+    if n <= 1 {
+        return n;
+    }
+
+    let (mut a, mut b) = (1, 1);
+
+    for _ in 2..n {
+        (a, b) = (b, a + b);
+    }
+
+    b
+}
