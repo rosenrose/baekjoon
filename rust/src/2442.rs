ea9e7bcc08c1e2e124047b2cbab9166d72d403e2
rosenrose <crazytempler@gmail.com>
2022-10-17 01:43:42 +0900

    2442 별 찍기 - 5

---
+++ rust/src/2442.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: usize = buf.trim().parse().unwrap();
+    let width = n * 2 - 1;
+
+    for i in 1..=n {
+        let result = format!("{:^width$}", "*".repeat(i * 2 - 1));
+        println!("{}", result.trim_end());
+    }
+}
