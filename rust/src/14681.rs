fec0bca8c91af4dce93b453811967f0f839bca46
rosenrose <crazytempler@gmail.com>
2022-10-16 18:53:02 +0900

    14681 사분면 고르기

---
+++ rust/src/14681.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut buf = String::new();
+
+    let nums: Vec<i32> = (0..2)
+        .map(|_| {
+            read_line(&mut buf);
+            parse_int(&buf)
+        })
+        .collect();
+
+    match (nums[0], nums[1]) {
+        (x, y) if x > 0 && y > 0 => println!("{}", 1),
+        (x, y) if x < 0 && y > 0 => println!("{}", 2),
+        (x, y) if x < 0 && y < 0 => println!("{}", 3),
+        (x, y) if x > 0 && y < 0 => println!("{}", 4),
+        _ => (),
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int(buf: &String) -> i32 {
+    buf.trim().parse().unwrap()
+}
