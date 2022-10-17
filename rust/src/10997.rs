40f8ad4dc45c9c66772a907a02227c656247f8a3
rosenrose <crazytempler@gmail.com>
2022-10-18 03:27:52 +0900

    10997 별 찍기 - 22

---
+++ rust/src/10997.rs
@@ -0,0 +1,49 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let n: usize = buf.trim().parse().unwrap();
+    let result: Vec<String> = print(n);
+
+    for r in result {
+        writeln!(stdout, "{}", r.trim_end()).unwrap();
+    }
+}
+
+fn print(n: usize) -> Vec<String> {
+    if n == 1 {
+        return vec!["*".to_string()];
+    }
+
+    let mut inner = print(n - 1);
+    if n == 2 {
+        inner.push("*".to_string());
+        inner.push("*".to_string());
+    }
+
+    let width = 4 * n - 3;
+    let mut result = Vec::new();
+
+    result.push("*".repeat(width));
+    result.push(format!("{:width$}", "*"));
+
+    for i in 0..inner.len() {
+        result.push(format!(
+            "* {}{}",
+            inner[i],
+            if i == 0 { "**" } else { " *" }
+        ));
+    }
+
+    result.push(format!("*{:blank$}*", "", blank = width - 2));
+    result.push("*".repeat(width));
+
+    result
+}
