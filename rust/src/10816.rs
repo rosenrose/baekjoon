739f98135b91c926b2a494c8ce327bbdc1de7f4a
rosenrose <crazytempler@gmail.com>
2022-10-19 02:23:48 +0900

    10816 숫자 카드 2

---
+++ rust/src/10816.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+    buf.clear();
+    stdin.read_line(&mut buf).unwrap();
+
+    let mut counts = HashMap::new();
+    let parse_int = |s: &str| s.parse::<i32>().unwrap();
+
+    for num in buf.split_whitespace().map(parse_int) {
+        counts.entry(num).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    stdin.read_line(&mut buf).unwrap();
+    buf.clear();
+    stdin.read_line(&mut buf).unwrap();
+
+    for num in buf.split_whitespace().map(parse_int) {
+        let count = match counts.get(&num) {
+            Some(c) => *c,
+            None => 0,
+        };
+
+        write!(stdout, "{count} ").unwrap();
+    }
+}
