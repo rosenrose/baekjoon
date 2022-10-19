dbe7dc75d552fd79f544aed55ff61d6bc72c6f5a
rosenrose <crazytempler@gmail.com>
2022-10-19 14:35:16 +0900

    1764 듣보잡

---
+++ rust/src/1764.rs
@@ -0,0 +1,44 @@
+use std::collections::HashSet;
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    if let [n, m] = parse_int_vec(&buf)[..] {
+        let not_heard: HashSet<String> = (0..n)
+            .map(|_| {
+                buf.clear();
+                stdin.read_line(&mut buf).unwrap();
+
+                buf.trim().to_string()
+            })
+            .collect();
+
+        let mut not_heard_seen = Vec::new();
+
+        for _ in 0..m {
+            buf.clear();
+            stdin.read_line(&mut buf).unwrap();
+            let not_seen = buf.trim().to_string();
+
+            if not_heard.contains(&not_seen) {
+                not_heard_seen.push(not_seen);
+            }
+        }
+
+        not_heard_seen.sort();
+
+        writeln!(stdout, "{}", not_heard_seen.len()).unwrap();
+        writeln!(stdout, "{}", not_heard_seen.join("\n")).unwrap();
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
---
+++ rust/src/util.rs
@@ -0,0 +1,35 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    stdin.read_line(&mut buf).unwrap();
+    writeln!(stdout, "").unwrap();
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int(buf: &String) -> i32 {
+    buf.trim().parse().unwrap()
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
+
+fn parse_str_vec(buf: &String) -> Vec<&str> {
+    buf.split_whitespace().collect()
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
