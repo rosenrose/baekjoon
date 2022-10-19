41ff86d958076867bc860529abdadf0acdc0eec9
rosenrose <crazytempler@gmail.com>
2022-10-19 16:55:30 +0900

    1085 직사각형에서 탈출

---
+++ rust/src/1085.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [x, y, w, h] = parse_int_vec(&buf)[..] {
+        let horizontal_min = x.min(w - x);
+        let vertical_min = y.min(h - y);
+
+        println!("{}", horizontal_min.min(vertical_min));
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
