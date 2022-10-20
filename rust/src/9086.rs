7b0e1dd005bca5b2c2801e304d63eb6ef2995b65
rosenrose <crazytempler@gmail.com>
2022-10-20 15:01:16 +0900

    9086 문자열

---
+++ rust/src/9086.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for _ in 0..n {
+        read_line(&mut buf);
+        let mut input = buf.trim().chars();
+
+        let first = input.next().unwrap();
+        let last = match input.next_back() {
+            Some(c) => c,
+            None => first,
+        };
+
+        println!("{first}{last}");
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
