9397b5e82558a57f01553df69fc4e840a4932f4b
rosenrose <crazytempler@gmail.com>
2022-10-20 23:11:11 +0900

    2577 숫자의 개수

---
+++ rust/src/2577.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut buf = String::new();
+
+    let product = (0..3)
+        .map(|_| {
+            read_line(&mut buf);
+            buf.trim().parse::<i32>().unwrap()
+        })
+        .reduce(|a, b| a * b)
+        .unwrap()
+        .to_string();
+
+    for digit in '0'..='9' {
+        let count = product.chars().filter(|&c| c == digit).count();
+
+        println!("{count}");
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
