0874ba35649a3b8c6bf4e6e629ac3d739d17faec
rosenrose <crazytempler@gmail.com>
2022-10-21 01:09:22 +0900

    2875 대회 or 인턴

---
+++ rust/src/2875.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [mut girls, mut boys, k] = parse_int_vec(&buf)[..] {
+        for _ in 0..k {
+            if girls >= boys * 2 {
+                girls -= 1;
+            } else {
+                boys -= 1;
+            }
+        }
+
+        let teams = if girls >= boys * 2 { boys } else { girls / 2 };
+
+        println!("{teams}");
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
