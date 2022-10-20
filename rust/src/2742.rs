ab893a1c7b03aaa0473194e941ab577cbeb80333
rosenrose <crazytempler@gmail.com>
2022-10-20 23:14:59 +0900

    2742 기찍 N

---
+++ rust/src/2742.rs
@@ -0,0 +1,17 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for i in (1..=n).rev() {
+        writeln!(stdout, "{i}").unwrap();
+    }
+}
