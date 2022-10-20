3568d63196f8bb288f37e6af7fad56886a97694d
rosenrose <crazytempler@gmail.com>
2022-10-20 13:41:26 +0900

    5597 과제 안 내신 분..?

---
+++ rust/src/5597.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut buf = String::new();
+    const N: usize = 30;
+
+    let mut is_submit = [false; N];
+
+    for _ in 0..N - 2 {
+        read_line(&mut buf);
+        let num: usize = buf.trim().parse().unwrap();
+
+        is_submit[num - 1] = true;
+    }
+
+    let absent = is_submit
+        .iter()
+        .enumerate()
+        .filter(|(_, &s)| !s)
+        .map(|(num, _)| num + 1);
+
+    for num in absent {
+        println!("{num}");
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
