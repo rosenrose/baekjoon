c84e29b9f7f3fae287ad6ea64379fbe8ffeff979
rosenrose <crazytempler@gmail.com>
2022-10-21 16:55:25 +0900

    1920 수 찾기

---
+++ rust/src/1920.rs
@@ -0,0 +1,34 @@
+use std::collections::HashSet;
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+    buf.clear();
+    stdin.read_line(&mut buf).unwrap();
+
+    let nums1 = parse_int_set(&buf);
+    stdin.read_line(&mut buf).unwrap();
+    buf.clear();
+    stdin.read_line(&mut buf).unwrap();
+
+    let nums2 = buf.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+
+    for num in nums2 {
+        let has_num = match nums1.get(&num) {
+            Some(_) => 1,
+            None => 0,
+        };
+
+        writeln!(stdout, "{has_num}").unwrap();
+    }
+}
+
+fn parse_int_set(buf: &String) -> HashSet<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
