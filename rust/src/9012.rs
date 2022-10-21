fa691bb9a3bb4be20f195e69f5855937f294e44e
rosenrose <crazytempler@gmail.com>
2022-10-21 17:20:30 +0900

    9012 괄호

---
+++ rust/src/9012.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    'outer: for _ in 0..n {
+        read_line(&mut buf);
+        let input = buf.trim().chars();
+
+        let mut stack = Vec::new();
+
+        for c in input {
+            match c {
+                '(' => stack.push(0),
+                ')' => {
+                    if let None = stack.pop() {
+                        println!("NO");
+                        continue 'outer;
+                    }
+                }
+                _ => (),
+            };
+        }
+
+        println!("{}", if stack.len() == 0 { "YES" } else { "NO" });
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
