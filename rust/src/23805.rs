49796ffae41850d3b85d579179c7165a893859ae
rosenrose <crazytempler@gmail.com>
2022-10-21 15:23:37 +0900

    23805 골뱅이 찍기 - 돌아간 ㄹ

---
+++ rust/src/23805.rs
@@ -0,0 +1,25 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let n: usize = buf.trim().parse().unwrap();
+    let at = "@".repeat(n);
+    let blank = n;
+
+    for _ in 0..n {
+        writeln!(stdout, "{}{:blank$}{at}", at.repeat(3), "").unwrap();
+    }
+    for _ in 0..n * 3 {
+        writeln!(stdout, "{at}{:blank$}{at}{:blank$}{at}", "", "").unwrap();
+    }
+    for _ in 0..n {
+        writeln!(stdout, "{at}{:blank$}{}", "", at.repeat(3),).unwrap();
+    }
+}
