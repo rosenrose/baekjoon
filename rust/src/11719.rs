88299e1765bd2193ad3999473b3f7b9000dafb15
rosenrose <crazytempler@gmail.com>
2022-10-24 15:03:42 +0900

    11719 그대로 출력하기 2

---
+++ rust/src/11719.rs
@@ -0,0 +1,17 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    println!("{buf}");
+
+    /* loop {
+        buf.clear();
+
+        match std::io::stdin().read_line(&mut buf).unwrap() {
+            0 => return,
+            _ => print!("{buf}"),
+        };
+    } */
+}
