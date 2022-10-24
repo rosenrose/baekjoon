70d55f05ccd151b60bb7d2c979796871de997edb
rosenrose <crazytempler@gmail.com>
2022-10-24 16:32:14 +0900

    11721 열 개씩 끊어 출력하기

---
+++ rust/src/11721.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let chars: Vec<String> = buf.trim().chars().map(|c| c.to_string()).collect();
+
+    for word in chars.chunks(10) {
+        println!("{}", word.join(""));
+    }
+}
