4daddaf96007490f01746600bd40d3d77b63f517
rosenrose <crazytempler@gmail.com>
2022-10-24 10:42:44 +0900

    6064 추가

--- rust/src/6064.rs
+++ rust/src/6064.rs
@@ -11,6 +11,97 @@ fn main() {
 
     let t: i32 = buf.trim().parse().unwrap();
 
+    for _ in 0..t {
+        buf.clear();
+        stdin.read_line(&mut buf).unwrap();
+
+        if let [m, n, x, y] = parse_int_vec(&buf)[..] {
+            let lcm = get_lcm(m, n);
+            let (gcd, mut a, mut b) = get_ex_gcd(m, n);
+            // println!("{lcm} {gcd} {a} {b]");
+            if (y - x) % gcd != 0 {
+                writeln!(stdout, "-1").unwrap();
+                continue;
+            }
+
+            let multiply = (y - x) / gcd;
+
+            a *= multiply;
+            a %= n / gcd;
+
+            b *= -multiply;
+            b %= m / gcd;
+
+            let mut left = m * a + x;
+            let mut right = n * b + y;
+            while left <= 0 {
+                left += lcm;
+            }
+            while right <= 0 {
+                right += lcm;
+            }
+            assert_eq!(left, right);
+            // println!("{left} {right}");
+
+            writeln!(stdout, "{left}").unwrap();
+        }
+    }
+}
+
+fn get_lcm(a: i32, b: i32) -> i32 {
+    (a * b) / get_gcd(a, b)
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        (a, b) = (b, a % b);
+
+        if b == 0 {
+            return a;
+        }
+    }
+}
+
+fn get_ex_gcd(a: i32, b: i32) -> (i32, i32, i32) {
+    let (mut r1, mut r2) = (a, b);
+    let (mut s1, mut s2) = (1, 0);
+    let (mut t1, mut t2) = (0, 1);
+    let mut q;
+
+    loop {
+        q = r1 / r2;
+        (r1, r2) = (r2, r1 % r2);
+
+        (s1, s2) = (s2, s1 - s2 * q);
+        (t1, t2) = (t2, t1 - t2 * q);
+
+        if r2 == 0 {
+            // if s1 < 0 {
+            //     s1 += b;
+            //     t1 -= a;
+            // }
+            return (r1, s1, t1);
+        }
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
+
+/* use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let t: i32 = buf.trim().parse().unwrap();
+
     'outer: for _ in 0..t {
         buf.clear();
         stdin.read_line(&mut buf).unwrap();
@@ -23,11 +114,11 @@ fn main() {
             } else {
                 (y, if x == m { 0 } else { x })
             };
-            let (multi, div) = (m.max(n), m.min(n));
+            let (mod1, mod2) = (m.max(n), m.min(n));
 
-            for i in (start_year..=total_years).step_by(multi) {
-                if i % div == compare_year {
-                    writeln!(stdout, "{i}").unwrap();
+            for year in (start_year..=total_years).step_by(mod1) {
+                if year % mod2 == compare_year {
+                    writeln!(stdout, "{year}").unwrap();
                     continue 'outer;
                 }
             }
@@ -54,3 +145,4 @@ fn get_gcd(mut a: usize, mut b: usize) -> usize {
 fn parse_int_vec(buf: &String) -> Vec<usize> {
     buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
+ */
