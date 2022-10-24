cd741e6ac34c067367307b3dbb67cc277f8fd72a
rosenrose <crazytempler@gmail.com>
2022-10-24 16:59:20 +0900

    2563 색종이

---
+++ rust/src/2563.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    const SIZE: usize = 10;
+    let mut board = [[false; 100]; 100];
+    let mut count = 0;
+
+    for _ in 0..n {
+        read_line(&mut buf);
+
+        if let [x_gap, y_gap] = parse_int_vec(&buf)[..] {
+            for y in y_gap..y_gap + SIZE {
+                for x in x_gap..x_gap + SIZE {
+                    if board[y][x] {
+                        continue;
+                    }
+
+                    board[y][x] = true;
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("{count}");
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<usize> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
