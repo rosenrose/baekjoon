af42c9353c4dbffebc606e1ae8871a1c2f3019f5
rosenrose <crazytempler@gmail.com>
2022-10-26 15:03:36 +0900

    5337 웰컴 5338 마이크로소프트 로고 5339 콜센터

---
+++ rust/src/5339.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!(r"     /~\");
+    println!(r"    ( oo|");
+    println!(r"    _\=/_");
+    println!(r"   /  _  \");
+    println!(r"  //|/.\|\\");
+    println!(r" ||  \ /  ||");
+    println!(r"============");
+    println!(r"|          |");
+    println!(r"|          |");
+    println!(r"|          |");
+}
