e5507818d2256e17171dfa969e9c17648fcb8e22
rosenrose <crazytempler@gmail.com>
2022-10-26 16:24:10 +0900

    14652 나는 행복합니다~

---
+++ rust/src/14652.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [_, m, k] = parse_int_vec(&buf)[..] {
+        println!("{} {}", k / m, k % m);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
