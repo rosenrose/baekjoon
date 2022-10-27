2d659c1cc9df57df4ff3ab01f80e0732b4548dc4
rosenrose <crazytempler@gmail.com>
2022-10-27 13:50:55 +0900

    25314 코딩은 체육과목 입니다

---
+++ rust/src/25314.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for _ in 0..n / 4 {
+        print!("long ");
+    }
+
+    println!("int");
+}
