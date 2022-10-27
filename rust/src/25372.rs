704cc502ba7e65a3932edd9d5176da26d066e092
rosenrose <crazytempler@gmail.com>
2022-10-27 13:47:43 +0900

    25372 성택이의 은밀한 비밀번호

---
+++ rust/src/25372.rs
@@ -0,0 +1,25 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    let mut stdin = stdin.lock();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for _ in 0..n {
+        buf.clear();
+        stdin.read_line(&mut buf).unwrap();
+
+        let is_valid = match buf.trim().len() {
+            6..=9 => "yes",
+            _ => "no",
+        };
+
+        writeln!(stdout, "{is_valid}").unwrap();
+    }
+}
