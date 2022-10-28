c10d12ee7046311da1896eacba783e423710dfd7
rosenrose <crazytempler@gmail.com>
2022-10-28 16:27:46 +0900

    2609 최대공약수와 최소공배수

---
+++ rust/src/2609.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b] = parse_int_vec(&buf)[..] {
+        let gcd = get_gcd(a, b);
+        let lcm = (a * b) / gcd;
+
+        println!("{gcd}\n{lcm}");
+    }
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        (a, b) = (b, a % b);
+
+        if b == 0 {
+            return a;
+        }
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
