cfad50c1f13eddea7b6e7c415326a5ea7ad4eb24
rosenrose <crazytempler@gmail.com>
2022-10-28 16:32:37 +0900

    1934 최소공배수

---
+++ rust/src/1934.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for _ in 0..n {
+        read_line(&mut buf);
+
+        if let [a, b] = parse_int_vec(&buf)[..] {
+            println!("{}", (a * b) / get_gcd(a, b));
+        }
+    }
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        (a, b) = (b, a % b);
+
+        if b == 0 {
+            return a;
+        }
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
