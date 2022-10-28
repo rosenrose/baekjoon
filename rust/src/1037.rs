2ff1944166c39a9c4fa227ff7bcb7b2b2dd743a4
rosenrose <crazytempler@gmail.com>
2022-10-28 16:24:07 +0900

    1037 약수

---
+++ rust/src/1037.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+    read_line(&mut buf);
+
+    let mut nums = parse_int_vec(&buf);
+    nums.sort();
+
+    println!("{}", nums[0] * nums[nums.len() - 1]);
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
