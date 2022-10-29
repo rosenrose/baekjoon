767a9a68cc68ae826b3e17995f6e55d25aae6917
rosenrose <crazytempler@gmail.com>
2022-10-29 23:26:27 +0900

    3046 R2

---
+++ rust/src/3046.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [r1, s] = parse_int_vec(&buf)[..] {
+        println!("{}", s * 2 - r1);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
