f907be0292647c3768306ab29edd8aa7e164b593
rosenrose <crazytempler@gmail.com>
2022-10-30 01:15:14 +0900

    10824 네 수

---
+++ rust/src/10824.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b, c, d] = parse_str_vec(&buf)[..] {
+        let parse_int = |s: String| s.parse::<i64>().unwrap();
+
+        let ab = parse_int([a, b].join(""));
+        let cd = parse_int([c, d].join(""));
+
+        println!("{}", ab + cd);
+    }
+}
+
+fn parse_str_vec(buf: &String) -> Vec<&str> {
+    buf.split_whitespace().collect()
+}
