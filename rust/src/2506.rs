0ebefcaf3256bce9ac3118e576cbf34eb6ba2fdc
rosenrose <crazytempler@gmail.com>
2022-10-30 02:49:08 +0900

    2506 점수계산

---
+++ rust/src/2506.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+    read_line(&mut buf);
+
+    let score: usize = buf
+        .trim()
+        .replace(' ', "")
+        .split(|c| c == '0')
+        .filter(|c| !c.is_empty())
+        .map(|one_str| one_str.chars().enumerate().map(|(i, _)| i + 1))
+        .flatten()
+        .sum();
+
+    println!("{score}");
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
