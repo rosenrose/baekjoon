110971792cd1f6571351e3ac8104675837db9626
rosenrose <crazytempler@gmail.com>
2022-10-31 01:12:22 +0900

    1371 가장 많은 글자

---
+++ rust/src/01/1371.rs
@@ -0,0 +1,28 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut max_count = 0;
+    let counts: Vec<(char, usize)> = ('a'..='z')
+        .map(|letter| {
+            let count = buf.trim().chars().filter(|&c| c == letter).count();
+            if count > max_count {
+                max_count = count;
+            }
+
+            (letter, count)
+        })
+        .collect();
+
+    let mut max_counts: Vec<String> = counts
+        .iter()
+        .filter(|(_, count)| *count == max_count)
+        .map(|(c, _)| c.to_string())
+        .collect();
+
+    max_counts.sort();
+
+    println!("{}", max_counts.concat());
+}
