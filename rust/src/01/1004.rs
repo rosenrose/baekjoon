8865a979c0b4e65842324f8d72f256416cc0d1ed
rosenrose <crazytempler@gmail.com>
2022-10-30 16:14:47 +0900

    1004 수정

--- rust/src/01/1004.rs
+++ rust/src/01/1004.rs
@@ -11,27 +11,27 @@ fn main() {
             let (start, end) = ((x1, y1), (x2, y2));
 
             read_line(&mut buf);
-            let planets = parse_int(&buf);
+            let planet_count = parse_int(&buf);
 
             let mut count = 0;
 
-            for _ in 0..planets {
+            for _ in 0..planet_count {
                 read_line(&mut buf);
 
                 if let [cx, cy, r] = parse_int_vec(&buf)[..] {
                     let planet = ((cx, cy), r as f64);
 
                     let is_start_inside_planet = is_point_inside_circle(start, planet);
-                    let is_end_inside_plaent = is_point_inside_circle(end, planet);
+                    let is_end_inside_planet = is_point_inside_circle(end, planet);
 
                     if is_start_inside_planet {
                         count += 1;
                     }
-                    if is_end_inside_plaent {
+                    if is_end_inside_planet {
                         count += 1;
                     }
 
-                    if is_start_inside_planet && is_end_inside_plaent {
+                    if is_start_inside_planet && is_end_inside_planet {
                         count -= 2;
                     }
                 }
