bb1633492c3d3107dfe19c353506e3424ab59ed3
rosenrose <crazytempler@gmail.com>
2022-10-31 01:17:37 +0900

    5988 홀수일까 짝수일까

---
+++ rust/src/05/5988.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for _ in 0..n {
+        read_line(&mut buf);
+
+        let last = buf
+            .trim()
+            .chars()
+            .rev()
+            .nth(0)
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
+
+        println!("{}", if last % 2 == 0 { "even" } else { "odd" });
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
