867efbb5491cc31181c51da0ae69ab5bf08360f2
rosenrose <crazytempler@gmail.com>
2022-11-01 02:45:29 +0900

    25191 치킨댄스를 추는 곰곰이를 본 임스

---
+++ rust/src/25/25191.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+    read_line(&mut buf);
+
+    if let [a, b] = parse_int_vec(&buf)[..] {
+        println!("{}", n.min((a / 2) + b));
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
