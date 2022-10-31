32a67747a45d45efdd145c1e5eb4e40f9cda810b
rosenrose <crazytempler@gmail.com>
2022-10-31 15:42:20 +0900

    1373 수정

--- rust/src/01/1373.rs
+++ rust/src/01/1373.rs
@@ -2,19 +2,11 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
-    let len = buf.trim().len();
-
-    let bin = if len % 3 == 0 {
-        buf.trim().to_string()
-    } else {
-        let pad = 3 - (len % 3);
-        format!("{}{}", "0".repeat(pad), buf.trim())
-    };
-
-    let bin: Vec<String> = bin.chars().map(|c| c.to_string()).collect();
+    let bin: Vec<String> = buf.trim().chars().map(|c| c.to_string()).collect();
 
     let oct: String = bin
-        .chunks(3)
+        .rchunks(3)
+        .rev()
         .map(|s| {
             let digit = u32::from_str_radix(&s.concat(), 2).unwrap();
             char::from_digit(digit, 8).unwrap()
