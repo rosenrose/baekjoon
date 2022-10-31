eb8b75ff26e5f09b1631df6d585ef745bcce0ade
rosenrose <crazytempler@gmail.com>
2022-11-01 02:20:43 +0900

    19698 헛간 청약

---
+++ rust/src/19/19698.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, w, h, l] = parse_int_vec(&buf)[..] {
+        let width = w / l;
+        let height = h / l;
+
+        println!("{}", n.min(width * height));
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
