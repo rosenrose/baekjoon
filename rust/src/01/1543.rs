2d2b1e151df609d1984b83e63ba241369cd722a3
rosenrose <crazytempler@gmail.com>
2022-10-31 15:03:57 +0900

    1543 수정

--- rust/src/01/1543.rs
+++ rust/src/01/1543.rs
@@ -6,31 +6,27 @@ fn main() {
     read_line(&mut buf);
 
     let keyword = buf.trim().to_string();
-    let (doc_len, key_len) = (document.len(), keyword.len());
 
-    if doc_len < key_len {
-        println!("0");
-        return;
-    }
-
-    let mut count = 0;
-    let mut i = 0;
-
-    while i + key_len <= doc_len {
-        if &document[i..i + key_len] == &keyword[..] {
-            count += 1;
-            i += key_len;
-
-            continue;
-        }
-
-        i += 1;
-    }
-
-    println!("{count}");
+    println!("{}", document.matches(&keyword).count());
 }
 
 fn read_line(buf: &mut String) {
     buf.clear();
     std::io::stdin().read_line(buf).unwrap();
 }
+
+/*
+let mut count = 0;
+let mut i = 0;
+
+while i + key_len <= doc_len {
+    if &document[i..i + key_len] == &keyword[..] {
+        count += 1;
+        i += key_len;
+
+        continue;
+    }
+
+    i += 1;
+}
+ */
