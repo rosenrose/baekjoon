359b6bfcb1a8e171bbe4220604e9beac0afc5c82
rosenrose <crazytempler@gmail.com>
2022-11-01 02:32:48 +0900

    23795 사장님 도박은 재미로 하셔야 합니다

---
+++ rust/src/23/23795.rs
@@ -0,0 +1,15 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let sum: i32 = buf
+        .lines()
+        .rev()
+        .skip(1)
+        .map(|s| s.parse::<i32>().unwrap())
+        .sum();
+
+    println!("{sum}");
+}
