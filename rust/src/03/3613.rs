fee0e0d6276792ef78d10d74191c52c52737037a
rosenrose <crazytempler@gmail.com>
2022-10-31 17:18:27 +0900

    3613 Java vs C++

---
+++ rust/src/03/3613.rs
@@ -0,0 +1,68 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let name = buf.trim();
+
+    if name.chars().all(char::is_lowercase) {
+        println!("{name}");
+        return;
+    }
+
+    let first = name.chars().nth(0).unwrap();
+
+    if first.is_uppercase() {
+        println!("Error!");
+        return;
+    }
+
+    let format;
+
+    if name.contains('_') {
+        let last = name.chars().last().unwrap();
+
+        if first == '_' || last == '_' {
+            println!("Error!");
+            return;
+        }
+        if name.split('_').any(str::is_empty) {
+            println!("Error!");
+            return;
+        }
+        if name.chars().any(char::is_uppercase) {
+            println!("Error!");
+            return;
+        }
+
+        format = "cpp";
+    } else {
+        format = "java";
+    }
+
+    let converted: Vec<String> = match format {
+        "cpp" => name
+            .split('_')
+            .enumerate()
+            .map(|(i, s)| {
+                if i == 0 {
+                    s.to_string()
+                } else {
+                    format!("{}{}", s.chars().nth(0).unwrap().to_uppercase(), &s[1..])
+                }
+            })
+            .collect(),
+        "java" => name
+            .chars()
+            .map(|c| {
+                if c.is_uppercase() {
+                    format!("_{}", c.to_lowercase())
+                } else {
+                    c.to_string()
+                }
+            })
+            .collect(),
+        _ => Vec::new(),
+    };
+
+    println!("{}", converted.concat());
+}
