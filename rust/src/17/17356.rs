335a6f8dc88738d84bd2c5730d6d5412d4c142e1
rosenrose <crazytempler@gmail.com>
2022-11-01 01:10:51 +0900

    17356 욱 제

---
+++ rust/src/17/17356.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b] = parse_float_vec(&buf)[..] {
+        let m = (b - a) / 400.0;
+
+        println!("{:.10}", 1.0 / (1.0 + 10.0_f64.powf(m)));
+    }
+}
+
+fn parse_float_vec(buf: &String) -> Vec<f64> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
