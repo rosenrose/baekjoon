ef62a8954a8bf9b36a7228915a145d62b0e00e2f
rosenrose <crazytempler@gmail.com>
2022-11-01 15:43:36 +0900

    25591 푸앙이와 종윤이

---
+++ rust/src/25/25591.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [x, y] = parse_int_vec(&buf)[..] {
+        let (a, b) = (100 - x, 100 - y);
+        let (c, d) = (100 - (a + b), a * b);
+        let (q, r) = (d / 100, d % 100);
+
+        println!("{a} {b} {c} {d} {q} {r}");
+        println!("{} {r}", c + q);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
