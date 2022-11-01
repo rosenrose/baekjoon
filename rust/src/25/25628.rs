935a72838716ddf4afbe917bb14d25a4b59cb7a5
rosenrose <crazytempler@gmail.com>
2022-11-01 16:09:03 +0900

    25628 햄버거 만들기

---
+++ rust/src/25/25628.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b] = parse_int_vec(&buf)[..] {
+        println!("{}", b.min(a / 2));
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
