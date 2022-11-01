e66fab0f753c298f156ead648078aec8d5be936d
rosenrose <crazytempler@gmail.com>
2022-11-01 15:54:13 +0900

    25600 Triathlon

---
+++ rust/src/25/25600.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+    let mut max_score = 0;
+
+    for _ in 0..n {
+        read_line(&mut buf);
+
+        if let [a, d, g] = parse_int_vec(&buf)[..] {
+            let mut score = a * (d + g);
+
+            if a == d + g {
+                score *= 2;
+            }
+
+            if score > max_score {
+                max_score = score;
+            }
+        }
+    }
+
+    println!("{max_score}");
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
