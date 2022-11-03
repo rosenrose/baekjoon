9f83066bf854658531c1e963e9021e1982dcd9c5
rosenrose <crazytempler@gmail.com>
2022-11-03 16:41:11 +0900

    17496 스타후르츠

---
+++ rust/src/17/17496.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, t, c, p] = parse_int_vec(&buf)[..] {
+        let mut harvest_days = n / t;
+
+        if n % t == 0 {
+            harvest_days -= 1;
+        }
+
+        println!("{}", harvest_days * c * p);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
