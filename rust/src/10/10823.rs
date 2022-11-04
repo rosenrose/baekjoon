462cf0a096b5affeb01131c5d02ed0a4f9c98600
rosenrose <crazytempler@gmail.com>
2022-11-04 18:41:56 +0900

    10823 수정

--- rust/src/10/10823.rs
+++ rust/src/10/10823.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut buf = String::new();
     stdin().read_to_string(&mut buf).unwrap();
 
-    let input = buf.trim().replace('\r', "").replace('\n', "");
+    let input = buf.replace(['\r', '\n'], "");
 
     let sum: i32 = input.split(',').map(|s| s.parse::<i32>().unwrap()).sum();
 
