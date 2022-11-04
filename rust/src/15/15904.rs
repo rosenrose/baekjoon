5e089fabd4075363546ca102c767f7db7416293a
rosenrose <crazytempler@gmail.com>
2022-11-04 23:26:43 +0900

    15904 UCPC는 무엇의 약자일까?

---
+++ rust/src/15/15904.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    const COMPARE: &str = "UCPC";
+
+    let comapre_chars: Vec<char> = "UCPC".chars().collect();
+    let mut stack = String::new();
+
+    for c in buf.trim().chars() {
+        if c == comapre_chars[stack.len()] {
+            stack.push(c);
+        }
+
+        if stack == COMPARE {
+            println!("I love UCPC");
+            return;
+        }
+
+        if stack.len() >= COMPARE.len() {
+            break;
+        }
+    }
+
+    println!("I hate UCPC");
+}
