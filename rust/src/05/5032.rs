e7654501c868a4edb2485b3990c9554c2c9995ea
rosenrose <crazytempler@gmail.com>
2022-11-05 14:48:55 +0900

    5032 탄산 음료

---
+++ rust/src/05/5032.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [mut empty, found, exchange] = parse_int_vec(&buf)[..] {
+        empty += found;
+        let mut count = 0;
+
+        while empty >= exchange {
+            let new = empty / exchange;
+
+            count += new;
+            empty = (empty % exchange) + new;
+        }
+
+        println!("{count}");
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
