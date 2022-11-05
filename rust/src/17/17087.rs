a963126aa8ddf19c4ce324e317c3dca1cb394b7c
rosenrose <crazytempler@gmail.com>
2022-11-05 21:45:38 +0900

    17087 숨바꼭질 6

---
+++ rust/src/17/17087.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    if let [_, s] = parse_int_vec(&buf)[..] {
+        read_line(&mut buf);
+
+        let diffs = buf
+            .split_whitespace()
+            .map(|num| num.parse::<i32>().unwrap().abs_diff(s));
+
+        let gcd = get_gcd(diffs);
+
+        println!("{gcd}");
+    }
+}
+
+fn get_gcd<I>(nums: I) -> u32
+where
+    I: Iterator<Item = u32>,
+{
+    nums.reduce(|mut a, mut b| loop {
+        (a, b) = (b, a % b);
+
+        if b == 0 {
+            return a;
+        }
+    })
+    .unwrap()
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
