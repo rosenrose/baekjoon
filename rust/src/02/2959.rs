6859496716c8b18927573a705757df1d666dbd4b
rosenrose <crazytempler@gmail.com>
2022-11-07 23:05:35 +0900

    2959 거북이

---
+++ rust/src/02/2959.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b, c, d] = parse_int_vec(&buf)[..] {
+        let combinations = [
+            (a.min(b), c.min(d)),
+            (a.min(c), b.min(d)),
+            (a.min(d), b.min(c)),
+        ];
+
+        let (w, h) = combinations.iter().max_by_key(|(w, h)| w * h).unwrap();
+
+        println!("{}", w * h);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
