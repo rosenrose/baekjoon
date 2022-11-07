5f678339c578c4d153097817c4afa646bd656c3b
rosenrose <crazytempler@gmail.com>
2022-11-07 17:45:06 +0900

    2576 홀수

---
+++ rust/src/02/2576.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut buf = String::new();
+    let mut min = 100;
+
+    let sum = (0..7)
+        .map(|_| {
+            read_line(&mut buf);
+            parse_int(&buf)
+        })
+        .filter(|i| i % 2 == 1)
+        .fold(0, |sum, i| {
+            if i < min {
+                min = i;
+            }
+
+            sum + i
+        });
+
+    if sum == 0 {
+        println!("-1");
+        return;
+    }
+
+    println!("{sum}\n{min}");
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int(buf: &String) -> i32 {
+    buf.trim().parse().unwrap()
+}
