00b46b2d6d6ba2818ac9bcb8834ef56ee47caf83
rosenrose <crazytempler@gmail.com>
2022-11-08 11:10:20 +0900

    11050 이항 계수 1

---
+++ rust/src/11/11050.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, k] = parse_int_vec(&buf)[..] {
+        println!("{}", combination_num(n, k));
+    }
+}
+
+fn combination_num(n: i32, r: i32) -> i32 {
+    if n == r || r == 0 {
+        return 1;
+    }
+
+    // combination_num(n - 1, r - 1) + combination_num(n - 1, r)
+    (n - r + 1..=n).product::<i32>() / (1..=r).product::<i32>()
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
