37919b9c0b34fd458b98f0acf7ab9f0e51f27553
rosenrose <crazytempler@gmail.com>
2022-11-08 22:02:55 +0900

    2960 에라토스테네스의 체

---
+++ rust/src/02/2960.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, k] = parse_int_vec(&buf)[..] {
+        let mut deleted: usize = 0;
+        get_prime_sieve(n, k as i32, &mut deleted);
+
+        println!("{deleted}");
+    }
+}
+
+fn get_prime_sieve(num: usize, mut k: i32, deleted: &mut usize) -> Vec<bool> {
+    let mut prime_sieve = vec![true; num + 1];
+    prime_sieve[0] = false;
+    prime_sieve[1] = false;
+
+    (2..=num).for_each(|i| {
+        if !prime_sieve[i] {
+            return;
+        }
+
+        for j in (i..=num).step_by(i) {
+            if !prime_sieve[j] {
+                continue;
+            }
+
+            prime_sieve[j] = false;
+            k -= 1;
+
+            if k == 0 {
+                *deleted = j;
+            }
+        }
+    });
+
+    prime_sieve
+}
+fn parse_int_vec(buf: &String) -> Vec<usize> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
