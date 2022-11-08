f47114f562860280c9b2f4bbe4b8b716878c1160
rosenrose <crazytempler@gmail.com>
2022-11-08 14:25:40 +0900

    11944 NN

---
+++ rust/src/11/11944.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, m] = parse_int_vec(&buf)[..] {
+        let n_str = n.to_string().repeat(n);
+
+        println!("{}", &n_str[..m.min(n_str.len())]);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<usize> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
