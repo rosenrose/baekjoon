cd79ac9b925694cd252e4d3c7e21c4f23044e1d6
rosenrose <crazytempler@gmail.com>
2022-11-09 16:51:34 +0900

    2476 2480 수정

--- rust/src/02/2476.rs
+++ rust/src/02/2476.rs
@@ -4,7 +4,7 @@ fn main() {
 
     let n: i32 = buf.trim().parse().unwrap();
 
-    let max = (0..n)
+    let max_prize = (0..n)
         .map(|_| {
             read_line(&mut buf);
             let nums = parse_int_vec(&buf);
@@ -12,15 +12,15 @@ fn main() {
             match nums[..] {
                 [a, b, c] if a == b && b == c => 10000 + a * 1000,
                 [a, b, _] if a == b => 1000 + a * 100,
-                [_, b, c] if b == c => 1000 + b * 100,
-                [a, _, c] if c == a => 1000 + c * 100,
+                [a, _, b] if a == b => 1000 + a * 100,
+                [_, a, b] if a == b => 1000 + a * 100,
                 _ => nums.iter().max().unwrap() * 100,
             }
         })
         .max()
         .unwrap();
 
-    println!("{max}");
+    println!("{max_prize}");
 }
 
 fn read_line(buf: &mut String) {
--- rust/src/02/2480.rs
+++ rust/src/02/2480.rs
@@ -7,8 +7,8 @@ fn main() {
     let prize = match nums[..] {
         [a, b, c] if a == b && b == c => 10000 + a * 1000,
         [a, b, _] if a == b => 1000 + a * 100,
-        [_, b, c] if b == c => 1000 + b * 100,
-        [a, _, c] if c == a => 1000 + c * 100,
+        [a, _, b] if a == b => 1000 + a * 100,
+        [_, a, b] if a == b => 1000 + a * 100,
         _ => nums.iter().max().unwrap() * 100,
     };
 
