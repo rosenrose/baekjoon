3409f481cfa048da9f57ae85d88c15a4359cbf4d
rosenrose <crazytempler@gmail.com>
2022-11-10 16:26:49 +0900

    3049 수정

--- rust/src/03/3049.rs
+++ rust/src/03/3049.rs
@@ -2,19 +2,8 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
-    let n: usize = buf.trim().parse().unwrap();
-    let mut an = vec![0, 0];
-    let mut bn = vec![0, 0];
-    let mut cn = vec![0, 0];
-
-    for i in 2..=n {
-        an.push(i - 1);
-        bn.push(bn[i - 1] + an[i - 1]);
-        cn.push(cn[i - 1] + bn[i - 1]);
-    }
-    // println!("{an:?}\n{bn:?}\n{cn:?}");
-
-    let points = cn[n] * n / 4;
+    let n: i32 = buf.trim().parse().unwrap();
+    let points = (n - 3..=n).product::<i32>() / (1..=4).product::<i32>();
 
     println!("{points}");
 }
