eb4e8997defac0d8ef14dd3dd5b5590bbded9159
rosenrose <crazytempler@gmail.com>
2022-11-11 16:09:19 +0900

    2929 머신 코드

---
+++ rust/src/02/2929.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let tokens: Vec<_> = buf.trim().split(char::is_uppercase).skip(1).collect();
+
+    let nop_count = tokens
+        .iter()
+        .take(tokens.len() - 1)
+        .fold(0, |acc, params| acc + 3 - (params.len() % 4));
+
+    println!("{nop_count:?}");
+}
