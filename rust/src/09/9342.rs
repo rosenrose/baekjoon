2e1c66704a7db612e0d43ca563aee8754e44afd5
rosenrose <crazytempler@gmail.com>
2022-11-11 16:34:36 +0900

    9342 수정

--- rust/src/09/9342.rs
+++ rust/src/09/9342.rs
@@ -4,13 +4,19 @@ fn main() {
 
     let n: i32 = buf.trim().parse().unwrap();
 
-    for _ in 0..n {
+    'outer: for _ in 0..n {
         read_line(&mut buf);
 
         let mut state = Some(1);
         // https://cyberzhg.github.io/toolbox/min_dfa?regex=KEF8QnxDfER8RXxGKT9BK0YrQysoQXxCfEN8RHxFfEYpPw==
+        let accept_chars = 'A'..='F';
 
         for c in buf.trim().chars() {
+            if !accept_chars.contains(&c) {
+                println!("Good");
+                continue 'outer;
+            }
+
             state = get_state(state, c);
 
             if state.is_none() {
@@ -28,11 +34,11 @@ fn main() {
 #[rustfmt::skip]
 fn get_state(state: Option<i32>, input: char) -> Option<i32> {
   match (state, input) {
-      (Some(1), 'A') => Some(2), (Some(1), 'B'|'C'|'D'|'E'|'F') => Some(3), (Some(1), _) => None,
-      (Some(2), 'A') => Some(2), (Some(2), 'F') => Some(4),                 (Some(2), _) => None,
-      (Some(3), 'A') => Some(2),                                            (Some(3), _) => None,
-      (Some(4), 'C') => Some(5), (Some(4), 'F') => Some(4),                 (Some(4), _) => None, 
-      (Some(5), 'C') => Some(5), (Some(5), 'A'|'B'|'D'|'E'|'F') => Some(6), (Some(5), _) => None,
+      (Some(1), 'A') => Some(2), (Some(1), _) => Some(3),
+      (Some(2), 'A') => Some(2), (Some(2), 'F') => Some(4), (Some(2), _) => None,
+      (Some(3), 'A') => Some(2), (Some(3), _) => None,
+      (Some(4), 'C') => Some(5), (Some(4), 'F') => Some(4), (Some(4), _) => None,
+      (Some(5), 'C') => Some(5), (Some(5), _) => Some(6),
       (Some(6), _) => None,
       _ => None,
   }
