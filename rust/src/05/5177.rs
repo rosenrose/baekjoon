0f973c36bb6fb66b6b4c135a47a670e64f46faf3
rosenrose <crazytempler@gmail.com>
2022-11-11 17:27:29 +0900

    5177 출력 형식이 잘못되었습니다

---
+++ rust/src/05/5177.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    for i in 1..=n {
+        read_line(&mut buf);
+        let s1 = buf.to_string();
+
+        read_line(&mut buf);
+        let s2 = buf.to_string();
+
+        println!(
+            "Data Set {i}: {}",
+            if sanitize(&s1) == sanitize(&s2) {
+                "equal"
+            } else {
+                "not equal"
+            }
+        );
+
+        if i < n {
+            println!("");
+        }
+    }
+}
+
+fn sanitize(input: &String) -> String {
+    let mut cleaned = input.trim().to_lowercase();
+
+    while cleaned.contains("  ") {
+        cleaned = cleaned.replace("  ", " ");
+    }
+
+    cleaned = cleaned
+        .replace('[', "(")
+        .replace('{', "(")
+        .replace(']', ")")
+        .replace('}', ")")
+        .replace(';', ",");
+
+    for sp in ["(", ")", ".", ",", ":"] {
+        let left = format!(" {sp}");
+        let right = format!("{sp} ");
+
+        while cleaned.contains(&left) {
+            cleaned = cleaned.replace(&left, sp)
+        }
+        while cleaned.contains(&right) {
+            cleaned = cleaned.replace(&right, sp)
+        }
+    }
+
+    cleaned
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
