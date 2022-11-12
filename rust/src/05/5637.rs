abe49fe92527eff451b3c3e68af4d08a87158ba1
rosenrose <crazytempler@gmail.com>
2022-11-12 17:35:39 +0900

    5637 가장 긴 단어

---
+++ rust/src/05/5637.rs
@@ -0,0 +1,27 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut longest_len = 0;
+
+    let words: Vec<_> = buf
+        .split(|c| match c {
+            'a'..='z' | 'A'..='Z' | '-' => false,
+            _ => true,
+        })
+        .filter(|s| {
+            longest_len = s.len().max(longest_len);
+            !s.is_empty()
+        })
+        .collect();
+
+    let first_longest = words
+        .iter()
+        .find(|w| w.len() == longest_len)
+        .unwrap()
+        .to_lowercase();
+
+    println!("{first_longest}");
+}
