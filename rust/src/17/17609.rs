b5f2466afd33d01f063651232a04eb73173e8b4b
rosenrose <crazytempler@gmail.com>
2022-11-13 01:03:41 +0900

    17609 회문

---
+++ rust/src/17/17609.rs
@@ -0,0 +1,76 @@
+use std::io::{stdin, stdout, BufWriter, Read, Write};
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let (mut stdin, mut stdout) = (stdin.lock(), BufWriter::new(stdout.lock()));
+
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf).unwrap();
+
+    let mut lines = buf.lines();
+    lines.next();
+
+    lines.for_each(|line| {
+        let (is_palin, is_pseudo_palin) = is_palindrome(line);
+
+        writeln!(
+            stdout,
+            "{}",
+            if is_palin {
+                if is_pseudo_palin {
+                    1
+                } else {
+                    0
+                }
+            } else {
+                2
+            }
+        )
+        .unwrap();
+    });
+}
+
+fn is_palindrome(word: &str) -> (bool, bool) {
+    let len = word.len();
+
+    if len <= 1 {
+        return (true, false);
+    }
+
+    let (mut i, mut j) = (0, len - 1);
+    let chars: Vec<_> = word.chars().collect();
+
+    loop {
+        if i >= j {
+            break;
+        }
+
+        if chars[i] == chars[j] {
+            i += 1;
+            j -= 1;
+            continue;
+        }
+
+        if is_palindrome_recur(&chars[i + 1..=j]) || is_palindrome_recur(&chars[i..=j - 1]) {
+            return (true, true);
+        }
+
+        return (false, false);
+    }
+
+    (true, false)
+}
+
+fn is_palindrome_recur(chars: &[char]) -> bool {
+    let len = chars.len();
+
+    if len <= 1 {
+        return true;
+    }
+
+    if chars[0] != chars[len - 1] {
+        return false;
+    }
+
+    is_palindrome_recur(&chars[1..len - 1])
+}
