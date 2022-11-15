616a4b8a17b52e4404ffea9b8aa093e9a8217ea0
rosenrose <crazytempler@gmail.com>
2022-11-15 15:05:06 +0900

    5582 공통 부분 문자열

---
+++ rust/src/05/5582.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let s: Vec<_> = buf.trim().chars().collect();
+    read_line(&mut buf);
+
+    let t = buf.trim();
+
+    let mut table = vec![vec![0; s.len() + 1]; t.len() + 1];
+    let mut max_len = 0;
+    // let (mut x, mut y) = (0, 0);
+
+    for (i, t_char) in t.char_indices() {
+        for j in 0..s.len() {
+            if t_char != s[j] {
+                continue;
+            }
+
+            table[i + 1][j + 1] = table[i][j] + 1;
+            max_len = table[i + 1][j + 1].max(max_len);
+            // if table[i + 1][j + 1] > max_len {
+            //     max_len = table[i + 1][j + 1];
+            //     (x, y) = (j, i);
+            // }
+        }
+    }
+
+    // let mut lc_string = String::new();
+
+    // for offset in 0..max_len {
+    //     assert_eq!(s[x - offset], t.chars().nth(y - offset).unwrap());
+    //     lc_string.push(s[x - offset]);
+    // }
+
+    // lc_string = lc_string.chars().rev().collect();
+    // println!("{lc_string}");
+
+    println!("{max_len}");
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
