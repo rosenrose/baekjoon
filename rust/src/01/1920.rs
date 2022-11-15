4b21a494428261dd15a598f4033e61b9cd4c5670
rosenrose <crazytempler@gmail.com>
2022-11-16 01:10:57 +0900

    1920 수정

--- rust/src/01/1920.rs
+++ rust/src/01/1920.rs
@@ -1,27 +1,30 @@
 use std::collections::HashSet;
-use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+use std::io::{stdin, stdout, BufWriter, Read, Write};
 
 fn main() {
     let (stdin, stdout) = (stdin(), stdout());
     let (mut stdin, mut stdout) = (stdin.lock(), BufWriter::new(stdout.lock()));
 
     let mut buf = String::new();
-    stdin.read_line(&mut buf).unwrap();
-    buf.clear();
-    stdin.read_line(&mut buf).unwrap();
+    stdin.read_to_string(&mut buf).unwrap();
 
-    let nums1 = parse_int_set(&buf);
-    stdin.read_line(&mut buf).unwrap();
-    buf.clear();
-    stdin.read_line(&mut buf).unwrap();
+    let mut lines = buf.lines();
+    lines.next();
 
-    let nums2 = buf.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+    let nums1 = parse_int_set(lines.next().unwrap());
+    lines.next();
+
+    let nums2 = lines.next().unwrap().split_whitespace().map(parse_int);
 
     for num in nums2 {
         writeln!(stdout, "{}", if nums1.contains(&num) { 1 } else { 0 }).unwrap();
     }
 }
 
-fn parse_int_set(buf: &String) -> HashSet<i32> {
-    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+fn parse_int(buf: &str) -> i32 {
+    buf.parse().unwrap()
+}
+
+fn parse_int_set(buf: &str) -> HashSet<i32> {
+    buf.split_whitespace().map(parse_int).collect()
 }
