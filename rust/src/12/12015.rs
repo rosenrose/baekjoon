f190b28eb1b8f8fa2fd61b593288e3ac28e4a53c
rosenrose <crazytempler@gmail.com>
2022-11-16 00:09:57 +0900

    12015 가장 긴 증가하는 부분 수열 2

---
+++ rust/src/12/12015.rs
@@ -0,0 +1,34 @@
+use std::io::{stdin, stdout, BufWriter, Read, Write};
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let (mut stdin, mut stdout) = (stdin.lock(), BufWriter::new(stdout.lock()));
+
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf).unwrap();
+
+    let mut lines = buf.lines();
+    lines.next();
+
+    let mut a = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut lis = vec![a.next().unwrap()];
+
+    for num in a {
+        if num > *lis.last().unwrap() {
+            lis.push(num);
+        }
+
+        let pos = match lis.binary_search(&num) {
+            Ok(i) => i,
+            Err(i) => i,
+        };
+
+        lis[pos] = num;
+    }
+
+    writeln!(stdout, "{}", lis.len()).unwrap();
+}
