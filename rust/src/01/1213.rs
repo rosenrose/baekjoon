18590bc17ce7405ec90066417d80a10191bcaed7
rosenrose <crazytempler@gmail.com>
2022-11-16 02:33:18 +0900

    1213 팰린드롬 만들기

---
+++ rust/src/01/1213.rs
@@ -0,0 +1,37 @@
+use std::collections::BTreeMap;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut counts = BTreeMap::new();
+
+    buf.trim().chars().for_each(|ch| {
+        counts.entry(ch).and_modify(|c| *c += 1).or_insert(1);
+    });
+
+    let odds: Vec<_> = counts
+        .iter()
+        .filter(|(_, &c)| c % 2 == 1)
+        .map(|(ch, _)| ch)
+        .collect();
+
+    if odds.len() > 1 {
+        println!("I'm Sorry Hansoo");
+        return;
+    }
+
+    let mut palindrome = match odds.get(0) {
+        Some(c) => c.to_string(),
+        None => String::new(),
+    };
+
+    for (&ch, &count) in counts.iter().rev() {
+        for _ in 0..count / 2 {
+            palindrome.insert(0, ch);
+            palindrome.push(ch);
+        }
+    }
+
+    println!("{palindrome}");
+}
