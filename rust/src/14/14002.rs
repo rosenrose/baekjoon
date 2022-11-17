db68299f615ea8d58f203841b9690ccf95fd182c
rosenrose <crazytempler@gmail.com>
2022-11-17 15:23:05 +0900

    14002 가장 긴 증가하는 부분 수열 4

---
+++ rust/src/14/14002.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+    read_line(&mut buf);
+
+    let mut a = buf.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+    let mut lis_len_arr = vec![a.next().unwrap()];
+    let mut index_arr = vec![(lis_len_arr[0], 0)];
+    let mut lis = Vec::new();
+
+    for num in a {
+        if num > *lis_len_arr.last().unwrap() {
+            index_arr.push((num, lis_len_arr.len()));
+            lis_len_arr.push(num);
+            continue;
+        }
+
+        let pos = match lis_len_arr.binary_search(&num) {
+            Ok(i) => i,
+            Err(i) => i,
+        };
+
+        index_arr.push((num, pos));
+        lis_len_arr[pos] = num;
+    }
+
+    for &(num, index) in index_arr.iter().rev() {
+        if index + 1 == lis_len_arr.len() {
+            lis.push(num);
+            lis_len_arr.pop();
+        }
+
+        if lis_len_arr.is_empty() {
+            break;
+        }
+    }
+
+    println!("{}", lis.len());
+
+    for num in lis.iter().rev() {
+        print!("{num} ");
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
