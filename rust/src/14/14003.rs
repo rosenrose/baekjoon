15ebed07b63a03de3d19aefb5b7512a466577d6a
rosenrose <crazytempler@gmail.com>
2022-11-17 15:25:51 +0900

    14003 가장 긴 증가하는 부분 수열 5

---
+++ rust/src/14/14003.rs
@@ -0,0 +1,54 @@
+use std::io::{stdin, stdout, BufWriter, Read, Write};
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let (mut stdin, mut stdout) = (stdin.lock(), BufWriter::new(stdout.lock()));
+
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf).unwrap();
+
+    let mut lines = buf.lines();
+    lines.next();
+
+    let mut a = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut lis_len_arr = vec![a.next().unwrap()];
+    let mut index_arr = vec![(lis_len_arr[0], 0)];
+    let mut lis = Vec::new();
+
+    for num in a {
+        if num > *lis_len_arr.last().unwrap() {
+            index_arr.push((num, lis_len_arr.len()));
+            lis_len_arr.push(num);
+            continue;
+        }
+
+        let pos = match lis_len_arr.binary_search(&num) {
+            Ok(i) => i,
+            Err(i) => i,
+        };
+
+        index_arr.push((num, pos));
+        lis_len_arr[pos] = num;
+    }
+
+    for &(num, index) in index_arr.iter().rev() {
+        if index + 1 == lis_len_arr.len() {
+            lis.push(num);
+            lis_len_arr.pop();
+        }
+
+        if lis_len_arr.is_empty() {
+            break;
+        }
+    }
+
+    writeln!(stdout, "{}", lis.len()).unwrap();
+
+    for num in lis.iter().rev() {
+        write!(stdout, "{num} ").unwrap();
+    }
+}
