5cac3473cffd2a5d82f1f5d8d54c996f497d9f0a
rosenrose <crazytempler@gmail.com>
2022-11-17 17:49:00 +0900

    2751 힙 정렬 추가

--- rust/src/02/2751.rs
+++ rust/src/02/2751.rs
@@ -1,5 +1,104 @@
 use std::io::{stdin, stdout, BufWriter, Read, Write};
 
+struct Heap<T> {
+    tree: Vec<T>,
+}
+
+impl<T> Heap<T>
+where
+    T: Copy + std::cmp::PartialOrd + std::fmt::Debug,
+{
+    fn new() -> Self {
+        Self { tree: Vec::new() }
+    }
+
+    fn get_parent(&self, i: usize) -> Option<(usize, T)> {
+        if i > 0 {
+            let idx = (i - 1) / 2;
+            Some((idx, self.tree[idx]))
+        } else {
+            None
+        }
+    }
+
+    fn get_children(&self, i: usize) -> (Option<(usize, T)>, Option<(usize, T)>) {
+        let left_idx = i * 2 + 1;
+        let right_idx = i * 2 + 2;
+
+        let left = match self.tree.get(left_idx) {
+            Some(v) => Some((left_idx, *v)),
+            None => None,
+        };
+        let right = match self.tree.get(right_idx) {
+            Some(v) => Some((right_idx, *v)),
+            None => None,
+        };
+
+        (left, right)
+    }
+
+    fn insert(&mut self, value: T) {
+        self.tree.push(value);
+
+        Self::up_heapify(self, self.tree.len() - 1, value);
+    }
+
+    fn remove(&mut self) -> Option<T> {
+        if self.tree.is_empty() {
+            return None;
+        }
+
+        let len = self.tree.len();
+
+        if len > 1 {
+            self.tree.swap(0, len - 1);
+        }
+
+        let ret = self.tree.pop().unwrap();
+
+        Self::down_heapify(self, 0);
+
+        Some(ret)
+    }
+
+    fn up_heapify(&mut self, i: usize, value: T) {
+        match Self::get_parent(self, i) {
+            Some((parent_idx, parent_value)) => {
+                if parent_value < value {
+                    self.tree.swap(parent_idx, i);
+                }
+
+                Self::up_heapify(self, parent_idx, value);
+            }
+            None => return,
+        }
+    }
+
+    fn down_heapify(&mut self, i: usize) {
+        let (child_idx, child_val) = match Self::get_children(self, i) {
+            (Some((left_idx, left_val)), Some((right_idx, right_val))) => {
+                if left_val > right_val {
+                    (left_idx, left_val)
+                } else {
+                    (right_idx, right_val)
+                }
+            }
+            (Some((left_idx, left_val)), None) => (left_idx, left_val),
+            (None, Some((right_idx, right_val))) => (right_idx, right_val),
+            (None, None) => return,
+        };
+
+        if child_val > self.tree[i] {
+            self.tree.swap(i, child_idx);
+            Self::down_heapify(self, child_idx);
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.tree.is_empty()
+    }
+}
+
 fn main() {
     let (stdin, stdout) = (stdin(), stdout());
     let (mut stdin, mut stdout) = (stdin.lock(), BufWriter::new(stdout.lock()));
@@ -10,11 +109,24 @@ fn main() {
     let mut lines = buf.lines();
     let n: usize = lines.next().unwrap().parse().unwrap();
 
-    let mut arr: Vec<i32> = lines.map(|line| line.parse().unwrap()).collect();
+    // let mut arr: Vec<i32> = lines.map(|line| line.parse().unwrap()).collect();
 
-    merge_sort(&mut arr[..], n);
+    // merge_sort(&mut arr[..], n);
 
-    for num in arr {
+    // for num in arr {
+    //     writeln!(stdout, "{num}").unwrap();
+    // }
+
+    let mut arr = Heap::new();
+    lines.for_each(|line| arr.insert(line.parse::<i32>().unwrap()));
+
+    let mut sorted = Vec::new();
+
+    while !arr.is_empty() {
+        sorted.push(arr.remove().unwrap());
+    }
+
+    for num in sorted.iter().rev() {
         writeln!(stdout, "{num}").unwrap();
     }
 }
@@ -53,5 +165,5 @@ fn merge_sort(arr: &mut [i32], len: usize) {
         }
     }
 
-    arr.clone_from_slice(&temp[..]);
+    arr.copy_from_slice(&temp[..]);
 }
