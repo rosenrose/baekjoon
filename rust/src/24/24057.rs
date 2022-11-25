da47ff9ec7360f57993c3645ee5f98a96438ab3d
rosenrose <crazytempler@gmail.com>
2022-11-25 14:42:54 +0900

    24057 수정

--- rust/src/24/24057.rs
+++ rust/src/24/24057.rs
@@ -14,53 +14,76 @@ impl Real {
         let (a, b, c, d) = (args[0], args[1], args[2], args[3]);
         Self { a, b, c, d }
     }
+
+    fn normalized(self) -> Self {
+        let (mut a, mut b, mut c, mut d) = (self.a, self.b, self.c, self.d);
+
+        if c == 0 {
+            d = 0;
+        }
+
+        let gcd = get_gcd([a, b, c].into_iter().filter(|&i| i != 0));
+        (a, b, c) = (a / gcd, b / gcd, c / gcd);
+
+        if a < 0 {
+            a *= -1;
+            b *= -1;
+            c *= -1;
+        }
+
+        Self { a, b, c, d }
+    }
 }
 impl Add for Real {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Self::from(normalize(
-            self.a * other.a,
-            other.a * self.b + self.a * other.b,
-            other.a * self.c + self.a * other.c,
-            self.d,
-        ))
+        Self {
+            a: self.a * other.a,
+            b: other.a * self.b + self.a * other.b,
+            c: other.a * self.c + self.a * other.c,
+            d: self.d,
+        }
+        .normalized()
     }
 }
 impl Sub for Real {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
-        Self::from(normalize(
-            self.a * other.a,
-            other.a * self.b - self.a * other.b,
-            other.a * self.c - self.a * other.c,
-            self.d,
-        ))
+        Self {
+            a: self.a * other.a,
+            b: other.a * self.b - self.a * other.b,
+            c: other.a * self.c - self.a * other.c,
+            d: self.d,
+        }
+        .normalized()
     }
 }
 impl Mul for Real {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        Self::from(normalize(
-            self.a * other.a,
-            self.b * other.b + self.c * other.c * self.d,
-            other.b * self.c + self.b * other.c,
-            self.d,
-        ))
+        Self {
+            a: self.a * other.a,
+            b: self.b * other.b + self.c * other.c * self.d,
+            c: other.b * self.c + self.b * other.c,
+            d: self.d,
+        }
+        .normalized()
     }
 }
 impl Div for Real {
     type Output = Self;
 
     fn div(self, other: Self) -> Self {
-        Self::from(normalize(
-            self.a * (other.b.pow(2) - other.c.pow(2) * self.d),
-            other.a * (self.b * other.b - self.c * other.c * self.d),
-            other.a * (other.b * self.c - self.b * other.c),
-            self.d,
-        ))
+        Self {
+            a: self.a * (other.b.pow(2) - other.c.pow(2) * self.d),
+            b: other.a * (self.b * other.b - self.c * other.c * self.d),
+            c: other.a * (other.b * self.c - self.b * other.c),
+            d: self.d,
+        }
+        .normalized()
     }
 }
 
@@ -80,26 +103,6 @@ fn main() {
     let b = Real::from(parse_int_vec(&buf));
 
     println!("{}\n{}\n{}\n{}", a + b, a - b, a * b, a / b);
-}
-
-fn normalize(mut a: i64, mut b: i64, mut c: i64, mut d: i64) -> Vec<i64> {
-    if c == 0 {
-        d = 0;
-    }
-    if d == 0 {
-        c = 0;
-    }
-
-    let gcd = get_gcd([a, b, c].into_iter().filter(|&i| i != 0));
-    (a, b, c) = (a / gcd, b / gcd, c / gcd);
-
-    if a < 0 {
-        a *= -1;
-        b *= -1;
-        c *= -1;
-    }
-
-    vec![a, b, c, d]
 }
 
 fn get_gcd<I>(nums: I) -> i64
