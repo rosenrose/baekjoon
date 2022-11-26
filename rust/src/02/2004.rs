688878015864d49c8661cbc315c5efc38ebc761e
rosenrose <crazytempler@gmail.com>
2022-11-26 16:47:08 +0900

    2004 조합 0의 개수

---
+++ rust/src/02/2004.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, m] = parse_int_vec(&buf)[..] {
+        let count_2 =
+            get_multiple_count(n, 2) - get_multiple_count(n - m, 2) - get_multiple_count(m, 2);
+        let count_5 =
+            get_multiple_count(n, 5) - get_multiple_count(n - m, 5) - get_multiple_count(m, 5);
+
+        println!("{}", count_2.min(count_5));
+    }
+}
+
+fn get_multiple_count(num: i64, divisor: i64) -> i64 {
+    let (mut count, mut exp) = (0, divisor);
+
+    while exp <= num {
+        count += num / exp;
+        exp *= divisor;
+    }
+
+    count
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i64> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
