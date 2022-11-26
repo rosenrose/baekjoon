7cdea6ab79c52e764931fadcaf0ce68fa75b66b2
rosenrose <crazytempler@gmail.com>
2022-11-26 15:11:10 +0900

    1431 시리얼 번호

---
+++ rust/src/01/1431.rs
@@ -0,0 +1,49 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+
+    let n: i32 = buf.trim().parse().unwrap();
+    let mut numbers = parse_str_vec_lines(&mut buf, n);
+
+    let digit_sum = |s: &String| {
+        s.chars()
+            .filter_map(|c| {
+                if c.is_numeric() {
+                    Some(c.to_digit(10).unwrap())
+                } else {
+                    None
+                }
+            })
+            .sum::<u32>()
+    };
+
+    numbers.sort_by(|a, b| {
+        if a.len() == b.len() {
+            let (a_sum, b_sum) = (digit_sum(a), digit_sum(b));
+
+            if a_sum == b_sum {
+                a.cmp(b)
+            } else {
+                a_sum.cmp(&b_sum)
+            }
+        } else {
+            a.len().cmp(&b.len())
+        }
+    });
+
+    println!("{}", numbers.join("\n"));
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_str_vec_lines(buf: &mut String, n: i32) -> Vec<String> {
+    (0..n)
+        .map(|_| {
+            read_line(buf);
+            buf.trim().to_string()
+        })
+        .collect()
+}
