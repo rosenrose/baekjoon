e34cc53b096eba4adf32039e358ff2dc47084243
rosenrose <crazytempler@gmail.com>
2022-11-26 14:12:38 +0900

    2749 11444 수정

--- rust/src/11/11444.rs
+++ rust/src/11/11444.rs
@@ -1,3 +1,52 @@
+use std::ops::Mul;
+
+struct Matrix {
+    matrix: Vec<Vec<i64>>,
+}
+
+impl Matrix {
+    fn from(matrix: Vec<Vec<i64>>) -> Self {
+        Self { matrix }
+    }
+
+    fn rem(self, m: i64) -> Self {
+        Self::from(
+            self.matrix
+                .iter()
+                .map(|row| row.iter().map(|num| num % m).collect())
+                .collect(),
+        )
+    }
+}
+
+impl Clone for Matrix {
+    fn clone(&self) -> Self {
+        Self::from((*self.matrix.clone()).to_vec())
+    }
+}
+
+impl Mul for Matrix {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self::from(
+            self.matrix
+                .iter()
+                .map(|row| {
+                    (0..self.matrix.len())
+                        .map(|i| {
+                            row.iter()
+                                .enumerate()
+                                .map(|(j, num)| num * other.matrix[j][i])
+                                .sum()
+                        })
+                        .collect()
+                })
+                .collect(),
+        )
+    }
+}
+
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
@@ -12,46 +61,23 @@ fn fibo_rem(n: i64) -> i64 {
         return n;
     }
 
-    let mut fibo_matrix = vec![vec![1, 1], vec![1, 0]];
+    let mut fibo_matrix = Matrix::from(vec![vec![1, 1], vec![1, 0]]);
     fibo_matrix = pow_rem(&mut fibo_matrix, n, 1_000_000_007);
 
-    fibo_matrix[0][1]
+    fibo_matrix.matrix[0][1]
 }
 
-fn pow_rem(base: &mut Vec<Vec<i64>>, exp: i64, m: i64) -> Vec<Vec<i64>> {
+fn pow_rem(base: &mut Matrix, exp: i64, m: i64) -> Matrix {
     if exp == 1 {
-        return matrix_mod(base, m);
+        return base.clone().rem(m);
     }
 
-    let mut rem = pow_rem(base, exp / 2, m);
-    rem = matrix_mod(
-        &matrix_mulitply(&matrix_mod(&rem, m), &matrix_mod(&rem, m)),
-        m,
-    );
+    let mut remainder = pow_rem(base, exp / 2, m);
+    remainder = (remainder.clone().rem(m) * remainder.rem(m)).rem(m);
 
     if exp % 2 == 0 {
-        rem
+        remainder
     } else {
-        matrix_mod(
-            &matrix_mulitply(&matrix_mod(&rem, m), &matrix_mod(&base, m)),
-            m,
-        )
+        (remainder.rem(m) * base.clone().rem(m)).rem(m)
     }
-}
-
-fn matrix_mulitply(a: &Vec<Vec<i64>>, b: &Vec<Vec<i64>>) -> Vec<Vec<i64>> {
-    a.iter()
-        .map(|row| {
-            (0..a.len())
-                .map(|i| row.iter().enumerate().map(|(j, num)| num * b[j][i]).sum())
-                .collect()
-        })
-        .collect()
-}
-
-fn matrix_mod(matrix: &Vec<Vec<i64>>, m: i64) -> Vec<Vec<i64>> {
-    matrix
-        .iter()
-        .map(|row| row.iter().map(|num| num % m).collect())
-        .collect()
 }
