6c4caeed313da965229fd11db70efa5ac00e7a62
rosenrose <crazytempler@gmail.com>
2022-11-28 19:45:16 +0900

    1157 수정

--- rust/src/01/1157.rs
+++ rust/src/01/1157.rs
@@ -1,32 +1,28 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
     let word = buf.trim().to_uppercase();
-    let letters: HashSet<char> = word.chars().collect();
+    let mut letter_counts = HashMap::new();
+    let mut max_count = 1;
 
-    let (mut most_used, mut max) = ('\0', 0);
-    let mut is_duplicate = false;
-
-    for letter in letters {
-        let count = word.chars().filter(|&c| c == letter).count();
-
-        if count == max {
-            is_duplicate = true;
-        }
-
-        if count > max {
-            is_duplicate = false;
-            (most_used, max) = (letter, count);
-        }
+    for ch in word.chars() {
+        letter_counts
+            .entry(ch)
+            .and_modify(|c| {
+                *c += 1;
+                max_count = max_count.max(*c);
+            })
+            .or_insert(1);
     }
 
-    if is_duplicate {
-        println!("?");
-        return;
-    }
+    let mut most_letters = letter_counts.iter().filter(|(_, &c)| c == max_count);
+    let (most_letter, _) = most_letters.next().unwrap();
 
-    println!("{most_used}");
+    match most_letters.next() {
+        Some(_) => println!("?"),
+        None => println!("{most_letter}"),
+    }
 }
