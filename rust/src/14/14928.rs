b71a615ae20442c248aad73df173b0f3eb221ebf
rosenrose <crazytempler@gmail.com>
2022-12-01 17:44:25 +0900

    14928 수정

--- rust/src/14/14928.rs
+++ rust/src/14/14928.rs
@@ -3,22 +3,16 @@ fn main() {
     std::io::stdin().read_line(&mut buf).unwrap();
 
     const M: i64 = 20000303;
-
     let mut exp_remainder = 1 % M;
-    let remainder = buf
-        .trim()
-        .chars()
-        .rev()
-        .map(|c| {
-            let num = c.to_digit(10).unwrap() as i64;
-            let rem = ((num % M) * (exp_remainder % M)) % M;
 
-            exp_remainder = ((exp_remainder % M) * (10 % M)) % M;
+    let remainder = buf.trim().chars().rev().fold(0, |acc, c| {
+        let num = c.to_digit(10).unwrap() as i64;
+        let rem = ((num % M) * (exp_remainder % M)) % M;
 
-            rem
-        })
-        .sum::<i64>()
-        % M;
+        exp_remainder = ((exp_remainder % M) * (10 % M)) % M;
+
+        (acc % M + rem % M) % M
+    });
 
     println!("{remainder}");
 }
