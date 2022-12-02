2556e860d9c95327e21e6fc35bef5bb558cd95c1
rosenrose <crazytempler@gmail.com>
2022-12-02 14:32:28 +0900

    25344 행성 정렬

---
+++ rust/src/25/25344.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut buf = String::new();
+    read_line(&mut buf);
+    read_line(&mut buf);
+
+    let periods = parse_int_vec(&buf);
+    let total_period = periods.windows(3).fold(1, |lcm, chunk| {
+        get_lcm(chunk.iter().map(|i| *i).chain([lcm]))
+    });
+
+    println!("{total_period}");
+}
+
+fn get_lcm<I>(nums: I) -> i32
+where
+    I: Iterator<Item = i32>,
+{
+    nums.reduce(|a, b| a / get_gcd(a, b) * b).unwrap()
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        (a, b) = (b, a % b);
+
+        if b == 0 {
+            return a;
+        }
+    }
+}
+
+fn read_line(buf: &mut String) {
+    buf.clear();
+    std::io::stdin().read_line(buf).unwrap();
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
