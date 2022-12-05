2b52c7f510a474b0ad7a10de4d78e731294d33a7
rosenrose <crazytempler@gmail.com>
2022-12-05 23:59:04 +0900

    2965 캥거루 세마리

---
+++ rust/src/02/2965.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b, c] = parse_int_vec(&buf)[..] {
+        let max_diff = (b - a).max(c - b);
+
+        println!("{}", max_diff - 1);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
