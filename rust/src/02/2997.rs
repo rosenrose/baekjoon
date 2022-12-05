c5c99fc66a1d2d021cabcec37b7787715df8a537
rosenrose <crazytempler@gmail.com>
2022-12-06 00:34:02 +0900

    2997 네 번째 수

---
+++ rust/src/02/2997.rs
@@ -0,0 +1,21 @@
+use std::cmp::Ordering;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut nums = parse_int_vec(&buf);
+    nums.sort();
+
+    if let [a, b, c] = nums[..] {
+        match (b - a).cmp(&(c - b)) {
+            Ordering::Equal => println!("{}", c + (c - b)),
+            Ordering::Less => println!("{}", b + (b - a)),
+            Ordering::Greater => println!("{}", a + (c - b)),
+        }
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
