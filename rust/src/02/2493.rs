996aca7935e50a4601bd4bf5fa5e037927ebc83b
rosenrose <crazytempler@gmail.com>
2022-12-06 19:06:22 +0900

    2493 수정

--- rust/src/02/2493.rs
+++ rust/src/02/2493.rs
@@ -1,50 +1,53 @@
-use std::io::{stdin, stdout, BufWriter, Read, Write};
+use std::fmt::Write;
+use std::io::{stdin, Read};
 
 fn main() {
-    let (stdin, stdout) = (stdin(), stdout());
-    let (mut stdin, mut stdout) = (stdin.lock(), BufWriter::new(stdout.lock()));
+    let stdin = stdin();
+    let mut stdin = stdin.lock();
 
     let mut buf = String::new();
     stdin.read_to_string(&mut buf).unwrap();
 
+    let input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut output = String::new();
+
     let mut towers = Vec::new();
 
-    buf.lines()
-        .next_back()
-        .unwrap()
-        .split_whitespace()
-        .map(|s| s.parse::<i32>().unwrap())
-        .for_each(|height| {
-            if towers.is_empty() {
-                towers.push((height, 0));
-                return;
-            }
+    for height in input.skip(1) {
+        if towers.is_empty() {
+            towers.push((height, 0));
+            continue;
+        }
 
-            let (last_height, mut last_recv) = *towers.last().unwrap();
+        let (last_height, mut last_recv) = *towers.last().unwrap();
 
-            if last_height > height {
-                towers.push((height, towers.len()));
-                return;
-            }
+        if last_height > height {
+            towers.push((height, towers.len()));
+            continue;
+        }
 
-            if last_recv == 0 {
-                towers.push((height, 0));
-                return;
-            }
+        if last_recv == 0 {
+            towers.push((height, 0));
+            continue;
+        }
 
-            let (mut heighest, mut heighest_recv) = towers[last_recv - 1];
+        let (mut heighest, mut heighest_recv) = towers[last_recv - 1];
 
-            while heighest_recv != 0 && heighest < height {
-                last_recv = heighest_recv;
-                (heighest, heighest_recv) = towers[heighest_recv - 1];
-            }
+        while heighest_recv != 0 && heighest < height {
+            last_recv = heighest_recv;
+            (heighest, heighest_recv) = towers[heighest_recv - 1];
+        }
 
-            let receive = if heighest < height { 0 } else { last_recv };
+        let receive = if heighest < height { 0 } else { last_recv };
 
-            towers.push((height, receive));
-        });
+        towers.push((height, receive));
+    }
 
     for (_, i) in towers {
-        write!(stdout, "{i} ").unwrap();
+        write!(output, "{i} ").unwrap();
     }
+
+    print!("{output}");
 }
