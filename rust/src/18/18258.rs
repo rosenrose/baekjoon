32540d4477590b20e62443ce1b1d09fde70aea57
rosenrose <crazytempler@gmail.com>
2022-12-08 05:21:59 +0900

    18258 큐 2

---
+++ rust/src/18/18258.rs
@@ -0,0 +1,45 @@
+use std::collections::VecDeque;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let stdin = stdin();
+    let mut stdin = stdin.lock();
+
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf).unwrap();
+
+    let mut input = buf.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = parse_int(input.next().unwrap());
+    let mut queue = VecDeque::new();
+
+    for _ in 0..n {
+        let result = match input.next().unwrap() {
+            "pop" => queue.pop_front().unwrap_or(-1),
+            "size" => queue.len() as i32,
+            "empty" => {
+                if queue.is_empty() {
+                    1
+                } else {
+                    0
+                }
+            }
+            "front" => *queue.front().unwrap_or(&-1),
+            "back" => *queue.back().unwrap_or(&-1),
+            _ => {
+                queue.push_back(parse_int(input.next().unwrap()));
+                continue;
+            }
+        };
+
+        writeln!(output, "{result}").unwrap();
+    }
+
+    print!("{output}");
+}
+
+fn parse_int(buf: &str) -> i32 {
+    buf.parse().unwrap()
+}
