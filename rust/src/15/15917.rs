08c8f48d746600becce086213e6b834cb8498bd0
rosenrose <crazytempler@gmail.com>
2022-12-08 16:48:29 +0900

    15917 노솔브 방지문제야!!

---
+++ rust/src/15/15917.rs
@@ -0,0 +1,21 @@
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let stdin = stdin();
+    let mut stdin = stdin.lock();
+
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf).unwrap();
+
+    let input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut output = String::new();
+
+    for num in input.skip(1) {
+        writeln!(output, "{}", if num & -num == num { 1 } else { 0 }).unwrap();
+    }
+
+    print!("{output}");
+}
