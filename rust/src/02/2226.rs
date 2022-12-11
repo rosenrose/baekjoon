fc36f58ee3821030f3295a41209e2876090f3574
rosenrose <crazytempler@gmail.com>
2022-12-11 18:30:14 +0900

    2226 수정

--- rust/src/02/2226.rs
+++ rust/src/02/2226.rs
@@ -1,4 +1,92 @@
+use std::fmt;
+
 const EXP: i128 = 10_000_000_000_000_000_000_000_000_000_000_000_000;
+
+struct BigInt {
+    nums: Vec<i128>,
+}
+
+impl BigInt {
+    fn add(&mut self, num: i128) -> Self {
+        self.nums[0] += num;
+        let mut carry = self.nums[0] / EXP;
+
+        for i in 1..self.nums.len() {
+            if carry == 0 {
+                break;
+            }
+
+            self.nums[i - 1] %= EXP;
+            self.nums[i] += carry;
+            carry = self.nums[i] / EXP;
+        }
+
+        if carry > 0 {
+            self.nums.push(carry);
+        }
+
+        Self {
+            nums: self.nums.clone(),
+        }
+    }
+
+    fn sub(&mut self, num: i128) -> Self {
+        self.nums[0] -= num;
+        let mut carry = if self.nums[0] < 0 { -1 } else { 0 };
+
+        for i in 1..self.nums.len() {
+            if carry == 0 {
+                break;
+            }
+
+            self.nums[i - 1] += EXP;
+            self.nums[i] -= carry;
+            carry = if self.nums[i] < 0 { -1 } else { 0 };
+        }
+
+        while self.nums.len() > 0 && *self.nums.last().unwrap() == 0 {
+            self.nums.pop();
+        }
+
+        Self {
+            nums: self.nums.clone(),
+        }
+    }
+
+    fn mul(&self, num: i128) -> Self {
+        let mut carry = 0;
+        let mut result: Vec<_> = self
+            .nums
+            .iter()
+            .map(|n| {
+                let temp = carry + n * num;
+                carry = temp / EXP;
+
+                temp % EXP
+            })
+            .collect();
+
+        if carry > 0 {
+            result.push(carry);
+        }
+
+        Self { nums: result }
+    }
+}
+
+impl fmt::Display for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, num) in self.nums.iter().rev().enumerate() {
+            if i == 0 {
+                write!(f, "{num}").unwrap();
+            } else {
+                write!(f, "{num:037}").unwrap();
+            }
+        }
+
+        write!(f, "")
+    }
+}
 
 fn main() {
     let mut buf = String::new();
@@ -12,80 +100,13 @@ fn main() {
     // });
     // println!("{bin}");
 
-    let count = (2..=n).fold(vec![0], |acc, num| {
+    let count = (2..=n).fold(BigInt { nums: vec![0] }, |acc, num| {
         if num % 2 == 0 {
-            add(mul(acc, 2), 1)
+            acc.mul(2).add(1)
         } else {
-            sub(mul(acc, 2), 1)
+            acc.mul(2).sub(1)
         }
     });
 
-    for (i, num) in count.iter().rev().enumerate() {
-        if i == 0 {
-            print!("{num}");
-        } else {
-            print!("{num:037}");
-        }
-    }
-}
-
-fn add(mut a: Vec<i128>, b: i128) -> Vec<i128> {
-    a[0] += b;
-    let mut carry = a[0] / EXP;
-
-    for i in 1..a.len() {
-        if carry == 0 {
-            break;
-        }
-
-        a[i - 1] %= EXP;
-        a[i] += carry;
-        carry = a[i] / EXP;
-    }
-
-    if carry > 0 {
-        a.push(carry);
-    }
-
-    a
-}
-
-fn sub(mut a: Vec<i128>, b: i128) -> Vec<i128> {
-    a[0] -= b;
-    let mut carry = if a[0] < 0 { -1 } else { 0 };
-
-    for i in 1..a.len() {
-        if carry == 0 {
-            break;
-        }
-
-        a[i - 1] += EXP;
-        a[i] -= carry;
-        carry = if a[i] < 0 { -1 } else { 0 };
-    }
-
-    while a.len() > 0 && *a.last().unwrap() == 0 {
-        a.pop();
-    }
-
-    a
-}
-
-fn mul(a: Vec<i128>, b: i128) -> Vec<i128> {
-    let mut carry = 0;
-    let mut result: Vec<_> = a
-        .iter()
-        .map(|num| {
-            let temp = carry + num * b;
-            carry = temp / EXP;
-
-            temp % EXP
-        })
-        .collect();
-
-    if carry > 0 {
-        result.push(carry);
-    }
-
-    result
+    println!("{count}");
 }
