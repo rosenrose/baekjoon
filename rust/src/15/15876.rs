cf2e7db0abc9b0b48b1db4139917b9e331b222bb
rosenrose <crazytempler@gmail.com>
2022-12-12 16:02:37 +0900

    15876 Binary Counting

---
+++ rust/src/15/15876.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, k] = parse_int_vec(&buf)[..] {
+        let k = k - 1;
+        let max_len = k + n * 4;
+        let mut bin = String::new();
+
+        for i in 0.. {
+            bin = [&bin[..], &format!("{i:b}")[..]].concat();
+
+            if bin.len() > max_len {
+                break;
+            }
+        }
+        // println!("{bin}");
+        for i in 0..5 {
+            print!("{} ", bin.chars().nth(k + n * i).unwrap());
+        }
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<usize> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
