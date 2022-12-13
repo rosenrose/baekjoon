b0cd0410909a929b8c104d85a61457b3cddcddd7
rosenrose <crazytempler@gmail.com>
2022-12-13 22:37:34 +0900

    2981 검문

---
+++ rust/src/02/2981.rs
@@ -0,0 +1,44 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let input = buf.lines().map(|s| s.parse::<i32>().unwrap());
+
+    let nums: Vec<_> = input.skip(1).collect();
+    let diffs = (1..nums.len()).map(|i| nums[i].abs_diff(nums[i - 1]));
+    let gcd = get_gcd(diffs);
+
+    let mut divisors = (1..)
+        .take_while(|i| i * i <= gcd)
+        .fold(Vec::new(), |mut acc, i| {
+            if gcd % i == 0 {
+                acc.push(i);
+                acc.push(gcd / i);
+            }
+
+            acc
+        });
+
+    divisors.dedup();
+    divisors.sort();
+
+    for num in divisors.iter().skip(1) {
+        print!("{num} ");
+    }
+}
+
+fn get_gcd<I>(nums: I) -> u32
+where
+    I: Iterator<Item = u32>,
+{
+    nums.reduce(|mut a, mut b| loop {
+        if b == 0 {
+            return a;
+        }
+
+        (a, b) = (b, a % b);
+    })
+    .unwrap()
+}
