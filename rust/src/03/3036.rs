c9e7b89125c31e3c652ad966df3502c6f0eafee1
rosenrose <crazytempler@gmail.com>
2022-12-13 22:47:36 +0900

    3036 링

---
+++ rust/src/03/3036.rs
@@ -0,0 +1,27 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    input.skip(1).reduce(|first, ring| {
+        let gcd = get_gcd(first, ring);
+        println!("{}/{}", first / gcd, ring / gcd);
+
+        first
+    });
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        if b == 0 {
+            return a;
+        }
+
+        (a, b) = (b, a % b);
+    }
+}
