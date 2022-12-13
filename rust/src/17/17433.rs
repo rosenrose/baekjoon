727dde9f7ffc5acf766d73c00005aa2a9bb1c61e
rosenrose <crazytempler@gmail.com>
2022-12-13 22:59:16 +0900

    17433 신비로운 수

---
+++ rust/src/17/17433.rs
@@ -0,0 +1,44 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    for _ in 0..input.next().unwrap() {
+        let n = input.next().unwrap();
+
+        if n == 1 {
+            println!("INFINITY");
+            continue;
+        }
+
+        let nums: Vec<_> = (0..n).map(|_| input.next().unwrap()).collect();
+        let diffs = (1..nums.len()).map(|i| nums[i].abs_diff(nums[i - 1]));
+        let gcd = get_gcd(diffs);
+
+        if gcd == 0 {
+            println!("INFINITY");
+            continue;
+        }
+
+        println!("{gcd}");
+    }
+}
+
+fn get_gcd<I>(nums: I) -> u32
+where
+    I: Iterator<Item = u32>,
+{
+    nums.reduce(|mut a, mut b| loop {
+        if b == 0 {
+            return a;
+        }
+
+        (a, b) = (b, a % b);
+    })
+    .unwrap()
+}
