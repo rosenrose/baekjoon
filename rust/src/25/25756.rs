36f2e54b3dcd2d60444a2963325cd01b3cc7e0fe
rosenrose <crazytempler@gmail.com>
2022-12-13 17:02:20 +0900

    25756 방어율 무시 계산하기

---
+++ rust/src/25/25756.rs
@@ -0,0 +1,22 @@
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<f64>().unwrap() / 100.0);
+    let mut output = String::new();
+
+    let mut v = 0.0;
+
+    for def in input.skip(1) {
+        v = 1.0 - (1.0 - v) * (1.0 - def);
+
+        writeln!(output, "{:.6}", v * 100.0).unwrap();
+    }
+
+    print!("{output}");
+}
