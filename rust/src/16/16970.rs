2f069a5b595c3bca378e8fc782ef54da8c1d66ea
rosenrose <crazytempler@gmail.com>
2022-12-13 19:39:31 +0900

    16970 정수 좌표의 개수

---
+++ rust/src/16/16970.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, m, k] = parse_int_vec(&buf)[..] {
+        let points: Vec<_> = (0..=n).flat_map(|x| (0..=m).map(move |y| (x, y))).collect();
+        let mut count = 0;
+
+        for i in 0..points.len() - 1 {
+            for j in i + 1..points.len() {
+                let ((x1, y1), (x2, y2)) = (points[i], points[j]);
+                let (width, height) = (x1.abs_diff(x2), y1.abs_diff(y2));
+
+                if get_gcd(width, height) + 1 != k {
+                    continue;
+                }
+
+                count += 1;
+            }
+        }
+
+        println!("{count}");
+    }
+}
+
+fn get_gcd(mut a: u32, mut b: u32) -> u32 {
+    loop {
+        if b == 0 {
+            return a;
+        }
+
+        (a, b) = (b, a % b);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<u32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
