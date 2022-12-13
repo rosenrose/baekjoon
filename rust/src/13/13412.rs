71f4f1e11cdf807e4d696a6b88edb988291a1e7f
rosenrose <crazytempler@gmail.com>
2022-12-13 16:01:21 +0900

    13412 서로소 쌍

---
+++ rust/src/13/13412.rs
@@ -0,0 +1,38 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let input = buf.lines().map(|s| s.parse::<i32>().unwrap());
+
+    for n in input.skip(1) {
+        if n == 1 {
+            println!("1");
+            continue;
+        }
+
+        let pairs = (1..)
+            .take_while(|i| i * i < n)
+            .filter(|&i| {
+                if n % i != 0 {
+                    return false;
+                }
+
+                get_gcd(i, n / i) == 1
+            })
+            .count();
+
+        println!("{pairs}");
+    }
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        if b == 0 {
+            return a;
+        }
+
+        (a, b) = (b, a % b);
+    }
+}
