a1d042a6eca880ff3946f985c0253dd0503a2427
rosenrose <crazytempler@gmail.com>
2022-12-15 14:58:48 +0900

    10166 수정

--- rust/src/10/10166.rs
+++ rust/src/10/10166.rs
@@ -5,31 +5,79 @@ fn main() {
     std::io::stdin().read_line(&mut buf).unwrap();
 
     if let [d1, d2] = parse_int_vec(&buf)[..] {
-        let seats: HashSet<_> = (d1..=d2)
-            .flat_map(|d| {
-                let angle = (360, d);
+        let euler_phi = get_euler_phi(d2 as usize);
+        let mut checked = vec![false; (d2 + 1) as usize];
+        let mut count = 0;
 
-                (0..d).map(move |i| {
-                    let gcd = get_gcd(angle.0 * i, angle.1);
-                    (angle.0 * i / gcd, angle.1 / gcd)
-                })
-            })
-            .collect();
+        for mut d in d1..=d2 {
+            let mut divisors = (1..)
+                .take_while(|i| i * i <= d)
+                .fold(Vec::new(), |mut acc, i| {
+                    if d % i == 0 {
+                        acc.push(i);
+                        acc.push(d / i);
+                    }
 
-        println!("{}", seats.len());
-    }
-}
+                    acc
+                });
+            divisors.dedup();
 
-fn get_gcd(mut a: usize, mut b: usize) -> usize {
-    loop {
-        if b == 0 {
-            return a;
+            for divisor in divisors {
+                let div = divisor as usize;
+
+                if checked[div] {
+                    d -= euler_phi[div];
+                }
+
+                checked[div] = true;
+            }
+
+            count += d;
         }
 
-        (a, b) = (b, a % b);
+        println!("{count}");
     }
 }
 
-fn parse_int_vec(buf: &String) -> Vec<usize> {
+fn get_euler_phi(num: usize) -> Vec<i32> {
+    let min_factors = get_min_factors(num);
+
+    (2..=num).fold(vec![0, 1], |mut acc, mut i| {
+        let mut phi = i as i32;
+        let mut factors = HashSet::new();
+
+        while i > 1 {
+            factors.insert(min_factors[i]);
+            i /= min_factors[i] as usize;
+        }
+
+        phi = factors.iter().fold(phi, |acc, &p| acc * (p - 1) / p);
+        acc.push(phi);
+
+        acc
+    })
+}
+
+fn get_min_factors(num: usize) -> Vec<i32> {
+    let mut min_factors: Vec<_> = (0..=num as i32).collect();
+
+    for i in (2..).take_while(|i| i * i <= num) {
+        if min_factors[i] != i as i32 {
+            continue;
+        }
+
+        for j in (i * i..=num).step_by(i) {
+            if min_factors[j] != j as i32 {
+                continue;
+            }
+
+            min_factors[j] = i as i32;
+        }
+    }
+
+    min_factors
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
     buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
