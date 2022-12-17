323909d375b4c0ec122e2450b9a28e172bd4f348
rosenrose <crazytempler@gmail.com>
2022-12-17 22:31:00 +0900

    13977 이항 계수와 쿼리

---
+++ rust/src/13/13977.rs
@@ -0,0 +1,60 @@
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+const M: i64 = 1_000_000_007;
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i64>().unwrap());
+    let mut output = String::new();
+
+    let factorial_rem = (1..=4_000_000).fold(vec![1], |mut acc, i| {
+        acc.push((*acc.last().unwrap() * i) % M);
+        acc
+    });
+
+    for _ in 0..input.next().unwrap() {
+        let (n, k) = (input.next().unwrap(), input.next().unwrap());
+
+        writeln!(output, "{}", combination_rem(n, k, &factorial_rem)).unwrap();
+    }
+
+    print!("{output}");
+}
+
+fn combination_rem(n: i64, r: i64, factorial_rem: &Vec<i64>) -> i64 {
+    if n == r || r == 0 {
+        return 1;
+    }
+
+    let a = factorial_rem[n as usize];
+    let b = mod_inverse_rem(
+        (factorial_rem[(n - r) as usize] * factorial_rem[r as usize]) % M,
+        M,
+    );
+
+    (a * b) % M
+}
+
+fn mod_inverse_rem(n: i64, modular: i64) -> i64 {
+    pow_rem(n, modular - 2)
+}
+
+fn pow_rem(mut base: i64, mut exp: i64) -> i64 {
+    let mut rem = 1;
+
+    while exp > 0 {
+        if exp % 2 == 1 {
+            rem = (rem * base) % M;
+        }
+
+        base = (base * base) % M;
+        exp /= 2;
+    }
+
+    rem
+}
