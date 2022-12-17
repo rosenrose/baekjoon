74e5756915cbff20a55d895e82aa5b47aa4ff23d
rosenrose <crazytempler@gmail.com>
2022-12-17 14:54:10 +0900

    13171 A

---
+++ rust/src/13/13171.rs
@@ -0,0 +1,28 @@
+use std::io::{stdin, Read};
+
+const M: i64 = 1_000_000_007;
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf.lines().map(|s| s.parse::<i64>().unwrap());
+    let (a, x) = (input.next().unwrap(), input.next().unwrap());
+
+    println!("{}", pow_rem(a, x));
+}
+
+fn pow_rem(base: i64, exp: i64) -> i64 {
+    if exp == 1 {
+        return base % M;
+    }
+
+    let mut rem = pow_rem(base, exp / 2);
+    rem = (rem * rem) % M;
+
+    if exp % 2 == 0 {
+        rem
+    } else {
+        (rem * (base % M)) % M
+    }
+}
