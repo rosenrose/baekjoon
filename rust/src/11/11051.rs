71fd47e902641f80ef0d1ffb65d216b9ae8922f3
rosenrose <crazytempler@gmail.com>
2022-12-17 22:08:36 +0900

    11051 11401 16134 수정

--- rust/src/11/11051.rs
+++ rust/src/11/11051.rs
@@ -1,66 +1,51 @@
-const M: i32 = 10007;
+const M: i32 = 10_007;
 
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
     if let [n, k] = parse_int_vec(&buf)[..] {
-        println!("{}", combination_num_rem(n, k));
+        println!("{}", combination_rem(n, k));
     }
 }
 
-fn combination_num_rem(n: i32, r: i32) -> i32 {
+fn combination_rem(n: i32, r: i32) -> i32 {
     if n == r || r == 0 {
         return 1;
     }
 
-    let mul_rem = |acc, i| (acc % M * i % M) % M;
+    let mul_rem = |acc, i| (acc * (i % M)) % M;
 
     let a = (n - r + 1..=n).fold(1, mul_rem);
-    let b = mod_inverse((1..=r).fold(1, mul_rem), M).unwrap();
+    let b = mod_inverse_rem((1..=r).fold(1, mul_rem), M);
 
-    (a % M * b % M) % M
+    (a * b) % M
 }
 
-fn mod_inverse(n: i32, modular: i32) -> Option<i32> {
-    let (gcd, inverse, _) = get_ex_gcd(n, modular);
-
-    if gcd != 1 {
-        return None;
-    }
-
-    Some(inverse)
+fn mod_inverse_rem(n: i32, modular: i32) -> i32 {
+    pow_rem(n, modular - 2)
 }
 
-fn get_ex_gcd(a: i32, b: i32) -> (i32, i32, i32) {
-    let (mut r1, mut r2) = (a, b);
-    let (mut s1, mut s2) = (1, 0);
-    let (mut t1, mut t2) = (0, 1);
-    let mut q;
+fn pow_rem(mut base: i32, mut exp: i32) -> i32 {
+    let mut rem = 1;
 
-    loop {
-        q = r1 / r2;
-        (r1, r2) = (r2, r1 % r2);
-
-        (s1, s2) = (s2, s1 - s2 * q);
-        (t1, t2) = (t2, t1 - t2 * q);
-
-        if r2 == 0 {
-            if s1 < 0 {
-                s1 += b;
-                t1 -= a;
-            }
-
-            return (r1, s1, t1);
+    while exp > 0 {
+        if exp % 2 == 1 {
+            rem = (rem * base) % M;
         }
+
+        base = (base * base) % M;
+        exp /= 2;
     }
+
+    rem
 }
 
 fn parse_int_vec(buf: &String) -> Vec<i32> {
     buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
 
-// fn combination_num_rem(n: i32, r: i32, cache: &mut HashMap<(i32, i32), i32>) -> i32 {
+// fn combination_rem(n: i32, r: i32, cache: &mut HashMap<(i32, i32), i32>) -> i32 {
 //     if n == r || r == 0 {
 //         return 1;
 //     }
@@ -68,8 +53,8 @@ fn parse_int_vec(buf: &String) -> Vec<i32> {
 //     let mut get_or_insert = |n: i32, r: i32| match cache.get(&(n, r)) {
 //         Some(i) => *i,
 //         None => {
-//             let ret = combination_num_rem(n, r, cache);
-//             cache.insert((n, r), ret % M);
+//             let ret = combination_rem(n, r, cache);
+//             cache.insert((n, r), ret);
 
 //             ret
 //         }
--- rust/src/16/16134.rs
+++ rust/src/16/16134.rs
@@ -5,55 +5,40 @@ fn main() {
     std::io::stdin().read_line(&mut buf).unwrap();
 
     if let [n, k] = parse_int_vec(&buf)[..] {
-        println!("{}", combination_num_rem(n, k));
+        println!("{}", combination_rem(n, k));
     }
 }
 
-fn combination_num_rem(n: i64, r: i64) -> i64 {
+fn combination_rem(n: i64, r: i64) -> i64 {
     if n == r || r == 0 {
         return 1;
     }
 
-    let mul_rem = |acc, i| ((acc % M) * (i % M)) % M;
+    let mul_rem = |acc, i| (acc * (i % M)) % M;
 
     let a = (n - r + 1..=n).fold(1, mul_rem);
-    let b = mod_inverse((1..=r).fold(1, mul_rem), M).unwrap();
+    let b = mod_inverse_rem((1..=r).fold(1, mul_rem), M);
 
     (a * b) % M
 }
 
-fn mod_inverse(n: i64, modular: i64) -> Option<i64> {
-    let (gcd, inverse, _) = get_ex_gcd(n, modular);
-
-    if gcd != 1 {
-        return None;
-    }
-
-    Some(inverse)
+fn mod_inverse_rem(n: i64, modular: i64) -> i64 {
+    pow_rem(n, modular - 2)
 }
 
-fn get_ex_gcd(a: i64, b: i64) -> (i64, i64, i64) {
-    let (mut r1, mut r2) = (a, b);
-    let (mut s1, mut s2) = (1, 0);
-    let (mut t1, mut t2) = (0, 1);
-    let mut q;
+fn pow_rem(mut base: i64, mut exp: i64) -> i64 {
+    let mut rem = 1;
 
-    loop {
-        q = r1 / r2;
-        (r1, r2) = (r2, r1 % r2);
-
-        (s1, s2) = (s2, s1 - s2 * q);
-        (t1, t2) = (t2, t1 - t2 * q);
-
-        if r2 == 0 {
-            if s1 < 0 {
-                s1 += b;
-                t1 -= a;
-            }
-
-            return (r1, s1, t1);
+    while exp > 0 {
+        if exp % 2 == 1 {
+            rem = (rem * base) % M;
         }
+
+        base = (base * base) % M;
+        exp /= 2;
     }
+
+    rem
 }
 
 fn parse_int_vec(buf: &String) -> Vec<i64> {
