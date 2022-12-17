046c4845d763f7f361e4bd08e693c7648f046baf
rosenrose <crazytempler@gmail.com>
2022-12-17 16:51:10 +0900

    2086 피보나치 수의 합

---
+++ rust/src/02/2086.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+const M: i64 = 1_000_000_000;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b] = parse_int_vec(&buf)[..] {
+        let mut cache = HashMap::new();
+
+        println!(
+            "{}",
+            (fibo_rem(b + 2, &mut cache) - fibo_rem(a + 1, &mut cache) + M) % M
+        );
+    }
+}
+
+fn fibo_rem(n: i64, cache: &mut HashMap<i64, i64>) -> i64 {
+    if n <= 1 {
+        return n;
+    }
+    if n == 2 {
+        return 1;
+    }
+
+    let i = n / 2;
+    let j = if n % 2 == 0 { i } else { i + 1 };
+
+    let mut get_or_insert = |n: i64| match cache.get(&n) {
+        Some(num) => *num,
+        None => {
+            let ret = fibo_rem(n, cache);
+            cache.insert(n, ret);
+
+            ret
+        }
+    };
+
+    let (a, b) = (
+        get_or_insert(i) * get_or_insert(j - 1),
+        get_or_insert(i + 1) * get_or_insert(j),
+    );
+
+    (a % M + b % M) % M
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i64> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
