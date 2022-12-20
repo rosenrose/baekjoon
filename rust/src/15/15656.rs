0a41a938b50b4df8cddbe497adf72d2f595c64fd
rosenrose <crazytempler@gmail.com>
2022-12-20 17:17:42 +0900

    15656 N과 M (7)

---
+++ rust/src/15/15656.rs
@@ -0,0 +1,43 @@
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut output = String::new();
+
+    input.next();
+    let m = input.next().unwrap();
+
+    let mut nums: Vec<_> = input.collect();
+    nums.sort();
+
+    let mut selected = Vec::new();
+
+    product(&nums, m, 0, &mut selected, &mut output);
+
+    print!("{output}");
+}
+
+fn product(nums: &Vec<i32>, m: i32, start: usize, selected: &mut Vec<i32>, output: &mut String) {
+    if m == 0 {
+        for num in selected {
+            write!(output, "{num} ").unwrap();
+        }
+        writeln!(output, "").unwrap();
+
+        return;
+    }
+
+    for &num in nums.iter().skip(start) {
+        selected.push(num);
+
+        product(nums, m - 1, 0, selected, output);
+
+        selected.pop();
+    }
+}
