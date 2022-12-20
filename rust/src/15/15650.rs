f2211d03d7d1e6ff3409cb2d360133d3dc2e993a
rosenrose <crazytempler@gmail.com>
2022-12-20 16:59:41 +0900

    15650 N과 M (2)

---
+++ rust/src/15/15650.rs
@@ -0,0 +1,50 @@
+use std::fmt::Write;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut output = String::new();
+
+    if let [n, m] = parse_int_vec(&buf)[..] {
+        let nums: Vec<_> = (1..=n).collect();
+        let mut selected = Vec::new();
+
+        combination(&nums, m, 0, &mut selected, &mut output);
+
+        print!("{output}");
+    }
+}
+
+fn combination(
+    nums: &Vec<i32>,
+    m: i32,
+    start: usize,
+    selected: &mut Vec<i32>,
+    output: &mut String,
+) {
+    if m == 0 {
+        for num in selected {
+            write!(output, "{num} ").unwrap();
+        }
+        writeln!(output, "").unwrap();
+
+        return;
+    }
+
+    for i in start..nums.len() - (m as usize - 1) {
+        if selected.contains(&nums[i]) {
+            continue;
+        }
+
+        selected.push(nums[i]);
+
+        combination(nums, m - 1, i + 1, selected, output);
+
+        selected.pop();
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
