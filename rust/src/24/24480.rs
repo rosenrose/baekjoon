fd01f3b848b338aa0f67c34fed9b09dc8e0a0d3e
rosenrose <crazytempler@gmail.com>
2022-12-23 02:51:21 +0900

    24480 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ rust/src/24/24480.rs
@@ -0,0 +1,56 @@
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    let (n, m, r) = (input(), input(), input());
+
+    let mut adjacency_list = (0..m).fold(vec![Vec::new(); n + 1], |mut acc, _| {
+        let (u, v) = (input(), input());
+        acc[u].push(v);
+        acc[v].push(u);
+
+        acc
+    });
+
+    for list in adjacency_list.iter_mut() {
+        (*list).sort_unstable();
+    }
+
+    dfs(&adjacency_list, r);
+}
+
+fn dfs(graph: &Vec<Vec<usize>>, start: usize) {
+    let mut visited = vec![0; graph.len()];
+    let mut stack = vec![start];
+    let mut count = 1;
+    let mut output = String::new();
+
+    while !stack.is_empty() {
+        let node = stack.pop().unwrap();
+
+        if visited[node] != 0 {
+            continue;
+        }
+
+        visited[node] = count;
+        count += 1;
+
+        for &neighbor in graph[node].iter() {
+            stack.push(neighbor);
+        }
+    }
+
+    for order in visited.iter().skip(1) {
+        writeln!(output, "{order}").unwrap();
+    }
+
+    print!("{output}");
+}
