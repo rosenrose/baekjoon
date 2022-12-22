007752d56de8a32ee0d569fdba4e4a0c0bc84bde
rosenrose <crazytempler@gmail.com>
2022-12-23 03:04:26 +0900

    2606 바이러스

---
+++ rust/src/02/2606.rs
@@ -0,0 +1,47 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    let (nodes, edges) = (input(), input());
+
+    let adjacency_list = (0..edges).fold(vec![Vec::new(); nodes + 1], |mut acc, _| {
+        let (u, v) = (input(), input());
+        acc[u].push(v);
+        acc[v].push(u);
+
+        acc
+    });
+
+    dfs(&adjacency_list, 1);
+}
+
+fn dfs(graph: &Vec<Vec<usize>>, start: usize) {
+    let mut discovered = vec![false; graph.len()];
+    discovered[start] = true;
+
+    let mut stack = vec![start];
+    let mut count = 0;
+
+    while !stack.is_empty() {
+        let node = stack.pop().unwrap();
+
+        for &neighbor in graph[node].iter() {
+            if discovered[neighbor] {
+                continue;
+            }
+
+            discovered[neighbor] = true;
+            stack.push(neighbor);
+            count += 1;
+        }
+    }
+
+    println!("{count}");
+}
