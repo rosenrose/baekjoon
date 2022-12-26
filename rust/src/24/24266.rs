09bf075a202f8be0a65a9489c9ec79ba5f53c5e4
rosenrose <crazytempler@gmail.com>
2022-12-26 23:57:01 +0900

    24266 알고리즘 수업 - 알고리즘의 수행 시간 5

---
+++ rust/src/24/24266.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i64 = buf.trim().parse().unwrap();
+
+    println!("{}\n{}", n * n * n, 3);
+}
