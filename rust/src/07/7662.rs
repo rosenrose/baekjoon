f754bc8cfc30159a7ea88d6ca95d1b5b73cad356
rosenrose <crazytempler@gmail.com>
2022-12-26 19:14:41 +0900

    7662 이중 우선순위 큐

---
+++ rust/src/07/7662.rs
@@ -0,0 +1,60 @@
+use std::collections::BTreeMap;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf.split_ascii_whitespace();
+
+    for _ in 0..parse_int(input.next().unwrap()) {
+        let n = parse_int(input.next().unwrap());
+        let mut map = BTreeMap::new();
+
+        for _ in 0..n {
+            let (op, num) = (input.next().unwrap(), parse_int(input.next().unwrap()));
+
+            match op {
+                "I" => {
+                    map.entry(num).and_modify(|c| *c += 1).or_insert(1);
+                }
+                "D" => {
+                    if map.is_empty() {
+                        continue;
+                    }
+
+                    let key = *(if num == -1 {
+                        map.keys().nth(0)
+                    } else {
+                        map.keys().last()
+                    })
+                    .unwrap();
+
+                    match map.get(&key).unwrap() {
+                        1 => {
+                            map.remove(&key);
+                        }
+                        value => {
+                            map.insert(key, value - 1);
+                        }
+                    }
+                }
+                _ => (),
+            }
+            // println!("{map:?}");
+        }
+
+        if map.is_empty() {
+            println!("EMPTY");
+            continue;
+        }
+
+        let (min, max) = (map.keys().nth(0).unwrap(), map.keys().last().unwrap());
+
+        println!("{max} {min}");
+    }
+}
+
+fn parse_int(buf: &str) -> i32 {
+    buf.parse().unwrap()
+}
