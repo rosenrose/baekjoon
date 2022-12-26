90b06e8f75fc9fffd0e1d7e6f0439af681fe90bd
rosenrose <crazytempler@gmail.com>
2022-12-26 20:52:38 +0900

    5073 삼각형과 세 변

---
+++ rust/src/05/5073.rs
@@ -0,0 +1,24 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    loop {
+        match (
+            input.next().unwrap(),
+            input.next().unwrap(),
+            input.next().unwrap(),
+        ) {
+            (0, 0, 0) => return,
+            (a, b, c) if a.max(b.max(c)) * 2 >= a + b + c => println!("Invalid"),
+            (a, b, c) if a == b && b == c => println!("Equilateral"),
+            (a, b, c) if a == b || b == c || c == a => println!("Isosceles"),
+            _ => println!("Scalene"),
+        }
+    }
+}
--- rust/src/10/10101.rs
+++ rust/src/10/10101.rs
@@ -4,22 +4,12 @@ fn main() {
     let mut buf = String::new();
     stdin().read_to_string(&mut buf).unwrap();
 
-    let angles: Vec<_> = buf.lines().map(|s| s.parse::<i32>().unwrap()).collect();
+    let input = buf.lines().map(|s| s.parse::<i32>().unwrap());
 
-    if angles.iter().sum::<i32>() != 180 {
-        println!("Error");
-        return;
+    match input.collect::<Vec<_>>()[..] {
+        [a, b, c] if a + b + c != 180 => println!("Error"),
+        [a, b, c] if a == b && b == c => println!("Equilateral"),
+        [a, b, c] if a == b || b == c || c == a => println!("Isosceles"),
+        _ => println!("Scalene"),
     }
-
-    if angles.iter().all(|&angle| angle == 60) {
-        println!("Equilateral");
-        return;
-    }
-
-    if angles[0] == angles[1] || angles[0] == angles[2] || angles[1] == angles[2] {
-        println!("Isosceles");
-        return;
-    }
-
-    println!("Scalene");
 }
