da0796e15828eac95b0ac50f46d782e493273329
rosenrose <crazytempler@gmail.com>
2022-12-28 00:43:42 +0900

    1759 암호 만들기

---
+++ rust/src/01/1759.rs
@@ -0,0 +1,47 @@
+use std::io::{stdin, Read};
+
+const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf.split_ascii_whitespace();
+
+    let len: i32 = input.next().unwrap().parse().unwrap();
+    input.next();
+
+    let mut chars: Vec<_> = input.collect();
+    chars.sort();
+
+    let mut selected = String::new();
+
+    combination(&chars, len, 0, &mut selected);
+}
+
+fn combination(chars: &Vec<&str>, m: i32, start: usize, selected: &mut String) {
+    if m == 0 {
+        if selected.matches(VOWELS).count() < 1 {
+            return;
+        }
+        if selected.matches(|c| !VOWELS.contains(&c)).count() < 2 {
+            return;
+        }
+
+        println!("{selected}");
+
+        return;
+    }
+
+    for i in start..chars.len() - (m as usize - 1) {
+        if selected.contains(chars[i]) {
+            continue;
+        }
+
+        selected.push_str(chars[i]);
+
+        combination(chars, m - 1, i + 1, selected);
+
+        selected.pop();
+    }
+}
