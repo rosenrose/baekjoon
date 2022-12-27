43dd18afff0bfea599ef798c4890f48b5b3c7d2a
rosenrose <crazytempler@gmail.com>
2022-12-28 00:12:35 +0900

    6603 로또

---
+++ rust/src/06/6603.rs
@@ -0,0 +1,46 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    while let Some(k) = input.next() {
+        if k == 0 {
+            break;
+        }
+
+        let nums: Vec<_> = (0..k).map(|_| input.next().unwrap()).collect();
+        let mut selected = Vec::new();
+
+        combination(&nums, 6, 0, &mut selected);
+
+        println!("");
+    }
+}
+
+fn combination(nums: &Vec<i32>, m: i32, start: usize, selected: &mut Vec<i32>) {
+    if m == 0 {
+        for num in selected {
+            print!("{num} ");
+        }
+        println!("");
+
+        return;
+    }
+
+    for i in start..nums.len() - (m as usize - 1) {
+        if selected.contains(&nums[i]) {
+            continue;
+        }
+
+        selected.push(nums[i]);
+
+        combination(nums, m - 1, i + 1, selected);
+
+        selected.pop();
+    }
+}
