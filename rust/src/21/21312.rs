9f607d947c7b60f85f0d09c43d255156033e764d
rosenrose <crazytempler@gmail.com>
2022-12-31 00:02:00 +0900

    21312 홀짝 칵테일

---
+++ rust/src/21/21312.rs
@@ -0,0 +1,22 @@
+use std::cmp::Ordering;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [a, b, c] = parse_int_vec(&buf)[..] {
+        let mut tastes = [a, b, c, a * b, a * c, b * c, a * b * c];
+
+        tastes.sort_by(|a, b| match (a % 2, b % 2) {
+            (1, 0) => Ordering::Greater,
+            (0, 1) => Ordering::Less,
+            _ => a.cmp(b),
+        });
+
+        println!("{}", tastes.iter().last().unwrap());
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
