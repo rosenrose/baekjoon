20ff55d04dc9ec42b77082f66b9d29baf5fd1b36
rosenrose <crazytempler@gmail.com>
2022-12-30 17:04:15 +0900

    1197 최소 스패닝 트리

---
+++ rust/src/01/1197.rs
@@ -0,0 +1,66 @@
+use std::io::{stdin, Read};
+
+struct DisjointSet {
+    set: Vec<usize>,
+}
+
+impl DisjointSet {
+    fn make(n: usize) -> Self {
+        Self {
+            set: (0..=n).collect(),
+        }
+    }
+
+    fn find(&mut self, a: usize) -> usize {
+        if self.set[a] != a {
+            self.set[a] = self.find(self.set[a]);
+        }
+
+        self.set[a]
+    }
+
+    fn union(&mut self, a: usize, b: usize) {
+        let (a, b) = (self.find(a), self.find(b));
+
+        if a == b {
+            return;
+        }
+
+        self.set[b] = a;
+    }
+
+    fn is_same(&mut self, a: usize, b: usize) -> bool {
+        self.find(a) == self.find(b)
+    }
+}
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    let (v, e) = (input() as usize, input());
+
+    let mut disjoint_set = DisjointSet::make(v);
+    let mut edges: Vec<_> = (0..e)
+        .map(|_| (input() as usize, input() as usize, input()))
+        .collect();
+
+    edges.sort_unstable_by_key(|&(_, _, weight)| weight);
+
+    let min_weight: i32 = edges
+        .iter()
+        .filter_map(|&(a, b, c)| {
+            (!disjoint_set.is_same(a, b)).then(|| {
+                disjoint_set.union(a, b);
+                c
+            })
+        })
+        .sum();
+
+    println!("{min_weight}");
+}
