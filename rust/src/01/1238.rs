613ab782a6e09ca519ef3430344cc6d9598ba53e
rosenrose <crazytempler@gmail.com>
2022-12-30 16:31:06 +0900

    1238 파티

---
+++ rust/src/01/1238.rs
@@ -0,0 +1,63 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    let (n, m, x) = (input(), input(), input());
+    let mut adjacency_list = vec![Vec::new(); n + 1];
+    let mut adjacency_list_reverse = vec![Vec::new(); n + 1];
+
+    for _ in 0..m {
+        let (start, end, t) = (input(), input(), input() as i32);
+
+        adjacency_list[start].push((end, t));
+        adjacency_list_reverse[end].push((start, t));
+    }
+
+    let dists_reverse = dijkstra(&adjacency_list_reverse, x);
+    let dists = dijkstra(&adjacency_list, x);
+    let total_dists = dists_reverse
+        .iter()
+        .skip(1)
+        .zip(dists.iter().skip(1))
+        .map(|(a, b)| a + b);
+
+    println!("{}", total_dists.max().unwrap());
+}
+
+fn dijkstra(graph: &Vec<Vec<(usize, i32)>>, start: usize) -> Vec<i32> {
+    let mut distances = vec![i32::MAX; graph.len()];
+    distances[start] = 0;
+
+    let mut queue = BinaryHeap::from([Reverse((0, start))]);
+
+    while let Some(Reverse((dist, node))) = queue.pop() {
+        let min_dist = distances[node];
+
+        if dist > min_dist {
+            continue;
+        }
+
+        for &(neighbor, weight) in graph[node].iter() {
+            let neighbor_min_dist = distances[neighbor];
+            let new_dist = min_dist + weight;
+
+            if new_dist >= neighbor_min_dist {
+                continue;
+            }
+
+            distances[neighbor] = new_dist;
+            queue.push(Reverse((new_dist, neighbor)));
+        }
+    }
+
+    distances
+}
