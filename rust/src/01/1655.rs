ebceccf98b8588024cabca7ea9e9f908b5818c8c
rosenrose <crazytempler@gmail.com>
2023-01-01 01:42:38 +0900

    1655 가운데를 말해요

---
+++ rust/src/01/1655.rs
@@ -0,0 +1,37 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf.lines().map(|s| s.parse::<i32>().unwrap());
+    input.next();
+    let mut output = String::new();
+
+    let (mut under, mut over) = (BinaryHeap::new(), BinaryHeap::new());
+
+    under.push(input.next().unwrap());
+    println!("{}", under.peek().unwrap());
+
+    for num in input {
+        if num < *under.peek().unwrap() {
+            under.push(num);
+        } else {
+            over.push(Reverse(num));
+        }
+        // #[rustfmt::skip]
+        // println!("{:?} {:?}", under.clone().into_sorted_vec(), over.clone().into_sorted_vec());
+        match (under.len(), over.len()) {
+            (a, b) if a < b => under.push(over.pop().unwrap().0),
+            (a, b) if a > b + 1 => over.push(Reverse(under.pop().unwrap())),
+            _ => (),
+        }
+
+        writeln!(output, "{}", under.peek().unwrap()).unwrap();
+    }
+
+    print!("{output}");
+}
