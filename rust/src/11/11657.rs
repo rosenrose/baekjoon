cf04c34b23c0e73d761b99cd8a5f855bee3d1c7c
rosenrose <crazytempler@gmail.com>
2023-01-01 19:01:18 +0900

    11657 수정

--- rust/src/11/11657.rs
+++ rust/src/11/11657.rs
@@ -12,15 +12,11 @@ fn main() {
     let mut output = String::new();
 
     let (n, m) = (input() as usize, input());
+    let edges: Vec<_> = (0..m)
+        .map(|_| (input() as usize, input() as usize, input()))
+        .collect();
 
-    let adjacency_list = (0..m).fold(vec![Vec::new(); n + 1], |mut acc, _| {
-        let (a, b, c) = (input() as usize, input() as usize, input());
-        acc[a].push((b, c));
-
-        acc
-    });
-
-    let (distances, is_negative_cycle) = bellman_ford(&adjacency_list, 1);
+    let (distances, is_negative_cycle) = bellman_ford(n, &edges, 1);
 
     if is_negative_cycle {
         println!("-1");
@@ -34,29 +30,31 @@ fn main() {
     print!("{output}");
 }
 
-fn bellman_ford(graph: &Vec<Vec<(usize, i64)>>, start: usize) -> (Vec<i64>, bool) {
-    let mut distances = vec![i64::MAX; graph.len()];
+fn bellman_ford(
+    node_num: usize,
+    edges: &Vec<(usize, usize, i64)>,
+    start: usize,
+) -> (Vec<i64>, bool) {
+    let mut distances = vec![i64::MAX; node_num + 1];
     distances[start] = 0;
 
-    for count in 1..graph.len() {
-        for (node, edges) in graph.iter().enumerate().skip(1) {
-            if distances[node] == i64::MAX {
+    for count in 0..node_num {
+        for &(start, end, weight) in edges {
+            if distances[start] == i64::MAX {
                 continue;
             }
 
-            for &(neighbor, weight) in edges {
-                let new_dist = distances[node] + weight;
+            let new_dist = distances[start] + weight;
 
-                if distances[neighbor] <= new_dist {
-                    continue;
-                }
-
-                if count == graph.len() - 1 {
-                    return (distances, true);
-                }
-
-                distances[neighbor] = new_dist;
+            if distances[end] <= new_dist {
+                continue;
             }
+
+            if count == node_num - 1 {
+                return (distances, true);
+            }
+
+            distances[end] = new_dist;
         }
     }
 
