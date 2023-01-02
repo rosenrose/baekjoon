a90c33a93018ba8b8d047272a6123cfdaf1f7814
rosenrose <crazytempler@gmail.com>
2023-01-02 18:59:35 +0900

    2580 스도쿠

---
+++ rust/src/02/2580.rs
@@ -0,0 +1,65 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    let mut empty_cells = Vec::new();
+    let mut board: Vec<Vec<_>> = (0..9)
+        .map(|row| {
+            (0..9)
+                .map(|col| {
+                    let num = input.next().unwrap();
+
+                    if num == 0 {
+                        empty_cells.push((row, col));
+                    }
+
+                    num
+                })
+                .collect()
+        })
+        .collect();
+
+    sudoku(&mut board, &empty_cells, 0);
+
+    for row in board {
+        for num in row {
+            print!("{num} ");
+        }
+        println!("");
+    }
+}
+
+fn sudoku(board: &mut Vec<Vec<i32>>, empty_cells: &Vec<(usize, usize)>, idx: usize) -> bool {
+    if idx == empty_cells.len() {
+        return true;
+    }
+
+    let (row, col) = empty_cells[idx];
+
+    for num in 1..=9 {
+        if (0..9).any(|i| board[row][i] == num || board[i][col] == num) {
+            continue;
+        }
+        if (0..9).any(|i| board[(row / 3) * 3 + (i / 3)][(col / 3) * 3 + (i % 3)] == num) {
+            continue;
+        }
+
+        board[row][col] = num;
+
+        let is_finished = sudoku(board, empty_cells, idx + 1);
+
+        if is_finished {
+            return true;
+        }
+    }
+
+    board[row][col] = 0;
+
+    false
+}
