c8db1c176ec65582906033b6e26a26b776eb77f3
rosenrose <crazytempler@gmail.com>
2023-01-03 14:12:17 +0900

    11034 캥거루 세마리2

---
+++ rust/src/11/11034.rs
@@ -0,0 +1,16 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    while let (Some(a), Some(b), Some(c)) = (input.next(), input.next(), input.next()) {
+        let max_diff = (b - a).max(c - b);
+
+        println!("{}", max_diff - 1);
+    }
+}
