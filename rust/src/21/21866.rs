530d6b87573646b5e1e8627930c577234669235f
rosenrose <crazytempler@gmail.com>
2023-01-04 17:06:04 +0900

    21866 추첨을 통해 커피를 받자

---
+++ rust/src/21/21866.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let scores: Vec<i32> = buf.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    const MAX: [i32; 9] = [100, 100, 200, 200, 300, 300, 400, 400, 500];
+
+    if scores.iter().zip(MAX).any(|(&score, max)| score > max) {
+        println!("hacker");
+        return;
+    }
+
+    println!(
+        "{}",
+        if scores.iter().sum::<i32>() < 100 {
+            "none"
+        } else {
+            "draw"
+        }
+    );
+}
