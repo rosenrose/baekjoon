fca3e1a919347e404a7d81f108a0bd396b8426df
rosenrose <crazytempler@gmail.com>
2023-01-05 17:48:45 +0900

    17174 전체 계산 횟수

---
+++ rust/src/17/17174.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, m] = parse_int_vec(&buf)[..] {
+        let (mut count, mut exp) = (0, 1);
+
+        while exp <= n {
+            count += n / exp;
+            exp *= m;
+        }
+
+        println!("{count}");
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
