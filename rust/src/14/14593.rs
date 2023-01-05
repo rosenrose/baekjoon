b349158be672de1155bcf8b2675686bae8033183
rosenrose <crazytempler@gmail.com>
2023-01-05 14:14:51 +0900

    14593 2017 아주대학교 프로그래밍 경시대회 (Large)

---
+++ rust/src/14/14593.rs
@@ -0,0 +1,25 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    let mut infos: Vec<_> = (1..=input())
+        .map(|i| (i, (input(), input(), input())))
+        .collect();
+
+    infos.sort_unstable_by(|(_, (s1, c1, t1)), (_, (s2, c2, t2))| {
+        if s1 == s2 {
+            (c1, t1).cmp(&(c2, t2))
+        } else {
+            s2.cmp(s1)
+        }
+    });
+
+    println!("{}", infos[0].0);
+}
