9665ad88b07d9278296ed37f82f1fb176c356cc9
rosenrose <crazytempler@gmail.com>
2023-01-05 14:36:30 +0900

    12840 창용이의 시계

---
+++ rust/src/12/12840.rs
@@ -0,0 +1,50 @@
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i64>().unwrap());
+    let mut input = || input.next().unwrap();
+    let mut output = String::new();
+
+    let (mut h, mut m, mut s) = (input(), input(), input());
+
+    for _ in 0..input() {
+        match input() {
+            1 => s += input(),
+            2 => s -= input(),
+            3 => {
+                while !matches!((h, m, s), (0..=23, 0..=59, 0..=59)) {
+                    m += s / 60;
+                    s %= 60;
+
+                    h += m / 60;
+                    m %= 60;
+
+                    h %= 24;
+
+                    if s < 0 {
+                        m += s.div_euclid(60);
+                        s = s.rem_euclid(60);
+                    }
+                    if m < 0 {
+                        h += m.div_euclid(60);
+                        m = m.rem_euclid(60);
+                    }
+                    if h < 0 {
+                        h = h.rem_euclid(24);
+                    }
+                }
+
+                writeln!(output, "{h} {m} {s}").unwrap();
+            }
+            _ => (),
+        }
+    }
+
+    print!("{output}");
+}
