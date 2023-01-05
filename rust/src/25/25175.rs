4b1465365d8ad52134d24160eac75b51ee0bf136
rosenrose <crazytempler@gmail.com>
2023-01-05 16:41:17 +0900

    25175 두~~부 두부 두부

---
+++ rust/src/25/25175.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [n, m, k] = parse_int_vec(&buf)[..] {
+        let diff = k - 3;
+        let next = if diff >= 0 {
+            m + diff
+        } else {
+            m + (n - diff.abs() % n)
+        } % n;
+
+        println!("{}", if next == 0 { n } else { next });
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
