a070b45af524bc499bc1d85a2fa5b5ddd92d4a6d
rosenrose <crazytempler@gmail.com>
2023-01-05 17:14:53 +0900

    15923 욱제는 건축왕이야!!

---
+++ rust/src/15/15923.rs
@@ -0,0 +1,28 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    let coords: Vec<_> = (0..input.next().unwrap())
+        .map(|_| (input.next().unwrap(), input.next().unwrap()))
+        .collect();
+
+    let len: u32 = (0..coords.len())
+        .map(|i| {
+            let (a, b) = if i == 0 {
+                (coords[0], *coords.last().unwrap())
+            } else {
+                (coords[i - 1], coords[i])
+            };
+
+            (a.0.abs_diff(b.0)).max(a.1.abs_diff(b.1))
+        })
+        .sum();
+
+    println!("{len}");
+}
