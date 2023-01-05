59faa4121e59286d7e85a24a74feff8c6c873756
rosenrose <crazytempler@gmail.com>
2023-01-05 17:28:50 +0900

    10419 수정

--- rust/src/10/10419.rs
+++ rust/src/10/10419.rs
@@ -1,3 +1,17 @@
+// use std::io::{stdin, Read};
+
+// fn main() {
+//     let mut buf = String::new();
+//     stdin().read_to_string(&mut buf).unwrap();
+
+//     let mut input = buf
+//         .split_ascii_whitespace()
+//         .map(|s| s.parse::<i32>().unwrap());
+
+//     let (n, x) = (input.next().unwrap(), input.next().unwrap());
+//     let cycle = input.map(|t| )
+// }
+
 use std::io::{stdin, Read};
 
 fn main() {
@@ -7,8 +21,15 @@ fn main() {
     let input = buf.lines().map(|s| s.parse::<i32>().unwrap());
 
     for d in input.skip(1) {
-        let t: Vec<_> = (0..).take_while(|t| t * t + t <= d).collect();
+        let mut t = 0;
+        let t = loop {
+            if t * t + t > d {
+                break t - 1;
+            }
 
-        println!("{}", t.last().unwrap());
+            t += 1;
+        };
+
+        println!("{t}");
     }
 }
