8da73ba54abdd0b896592d5301ce8a5932aa6e56
rosenrose <crazytempler@gmail.com>
2023-01-06 15:01:32 +0900

    16503 괄호 없는 사칙연산

---
+++ rust/src/16/16503.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut input = buf.split_ascii_whitespace();
+    let mut input = || input.next().unwrap();
+
+    let (k1, op1, k2, op2, k3) = (
+        parse_int(input()),
+        input(),
+        parse_int(input()),
+        input(),
+        parse_int(input()),
+    );
+
+    #[rustfmt::skip]
+    let (first, second) = (0..2).fold((k1, k2), |(f, s), i| {
+        (
+            match if i == 0 { op1 } else { op2 } {
+                "+" => f + if i == 0 { k2 } else { k3 },
+                "-" => f - if i == 0 { k2 } else { k3 },
+                "*" => f * if i == 0 { k2 } else { k3 },
+                "/" => f / if i == 0 { k2 } else { k3 },
+                _ => 0,
+            },
+            match if i == 0 { op2 } else { op1 }  {
+                "+" => if i == 0 { s + k3 } else { k1 + s },
+                "-" => if i == 0 { s - k3 } else { k1 - s },
+                "*" => if i == 0 { s * k3 } else { k1 * s },
+                "/" => if i == 0 { s / k3 } else { k1 / s },
+                _ => 0,
+            },
+        )
+    });
+
+    println!("{}\n{}", first.min(second), first.max(second));
+}
+
+fn parse_int(buf: &str) -> i32 {
+    buf.parse().unwrap()
+}
