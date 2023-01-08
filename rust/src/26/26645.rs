a89abde7fb3bd7afe8f287e1d2a6cf88e64a85b2
rosenrose <crazytempler@gmail.com>
2023-01-08 14:36:40 +0900

    26645 성장의 비약 선택권

---
+++ rust/src/26/26645.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+    let mut level_number: Vec<_> = [(209, 8), (219, 4), (229, 2), (239, 1)]
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &(limit, count))| {
+            (n <= limit).then(|| ((limit + 1).min(n + count), i + 1))
+        })
+        .collect();
+
+    level_number.sort();
+
+    println!("{}", level_number.last().unwrap().1);
+}
