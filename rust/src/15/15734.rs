d915f80ab1310bc73c364c3812c37e44d88a68fe
rosenrose <crazytempler@gmail.com>
2023-01-08 16:16:04 +0900

    15734 명장 남정훈

---
+++ rust/src/15/15734.rs
@@ -0,0 +1,28 @@
+use std::cmp::Ordering;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [mut left, mut right, a] = parse_int_vec(&buf)[..] {
+        let diff = left.abs_diff(right);
+        let both = a.saturating_sub(diff);
+
+        match left.cmp(&right) {
+            Ordering::Less => left += diff.min(a),
+            Ordering::Greater => right += diff.min(a),
+            _ => (),
+        }
+
+        if left == right {
+            left += both / 2;
+            right += both / 2;
+        }
+
+        println!("{}", left.min(right) * 2);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<u32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
