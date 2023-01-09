255ee60ef4443ba4dca27801737b4a868f2209c9
rosenrose <crazytempler@gmail.com>
2023-01-09 14:50:07 +0900

    9493 길면 기차, 기차는 빨라, 빠른 것은 비행기

---
+++ rust/src/09/9493.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    while let (Some(m), Some(a), Some(b)) = (input.next(), input.next(), input.next()) {
+        if (m, a, b) == (0, 0, 0) {
+            return;
+        }
+
+        let train_seconds = m as f64 * 3600.0 / a as f64;
+        let plane_seconds = m as f64 * 3600.0 / b as f64;
+        let diff = (train_seconds - plane_seconds).round() as i32;
+
+        let s = diff % 60;
+        let m = (diff / 60) % 60;
+        let h = (diff / 60) / 60;
+
+        println!("{h}:{m:02}:{s:02}");
+    }
+}
