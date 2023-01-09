4f5b6b8d7bc2b0bcaf6b51c12b38616aeb7cf683
rosenrose <crazytempler@gmail.com>
2023-01-09 17:05:52 +0900

    2954 창영이의 일기장

---
+++ rust/src/02/2954.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut input = buf.trim().to_string();
+    let patterns = ["a", "e", "i", "o", "u"]
+        .iter()
+        .map(|vowel| (vowel, format!("{vowel}p{vowel}")));
+
+    for (v, p) in patterns {
+        input = input.replace(&p, v);
+    }
+
+    println!("{input}");
+}
