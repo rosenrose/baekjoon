43e6ef75e53f5162ce0a12caec09ed71319f792e
rosenrose <crazytempler@gmail.com>
2023-01-09 15:40:42 +0900

    15792 A/B - 2

---
+++ rust/src/15/15792.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [mut a, b] = parse_int_vec(&buf)[..] {
+        print!("{}.", a / b);
+
+        for _ in 0..1000 {
+            a = a % b * 10;
+            print!("{}", a / b);
+        }
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
