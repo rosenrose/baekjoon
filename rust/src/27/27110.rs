2b084351cd8d27c6770f57b7c71e10f68f4e3a16
rosenrose <crazytempler@gmail.com>
2023-01-09 16:11:35 +0900

    27110 특식 배부

---
+++ rust/src/27/27110.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    let n = input.next().unwrap();
+    let count: i32 = input.map(|fav| fav.min(n)).sum();
+
+    println!("{count}");
+}
