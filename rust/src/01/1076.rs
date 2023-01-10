f73ee03786c007bd5ba0e3b1ea5c4e7e8451367e
rosenrose <crazytempler@gmail.com>
2023-01-10 19:01:44 +0900

    1076 저항

---
+++ rust/src/01/1076.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+
+    #[rustfmt::skip]
+    let values: Vec<i64> = buf
+        .lines()
+        .enumerate()
+        .map(|(i, s)| match s {
+            "black"  => if i < 2 { 0 } else { 1 },
+            "brown"  => if i < 2 { 1 } else { 10 },
+            "red"    => if i < 2 { 2 } else { 100 },
+            "orange" => if i < 2 { 3 } else { 1_000 },
+            "yellow" => if i < 2 { 4 } else { 10_000 },
+            "green"  => if i < 2 { 5 } else { 100_000 },
+            "blue"   => if i < 2 { 6 } else { 1_000_000 },
+            "violet" => if i < 2 { 7 } else { 10_000_000 },
+            "grey"   => if i < 2 { 8 } else { 100_000_000 },
+            "white"  => if i < 2 { 9 } else { 1_000_000_000 },
+            _ => 0,
+        })
+        .collect();
+
+    println!("{}", (values[0] * 10 + values[1]) * values[2]);
+}
