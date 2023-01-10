039d7a986ced865b3c6544287214b2935c3a9356
rosenrose <crazytempler@gmail.com>
2023-01-10 16:49:20 +0900

    6376 e 계산

---
+++ rust/src/06/6376.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("n e");
+    println!("- -----------");
+
+    for n in 0..=9 {
+        let e = 1.0
+            + (1..=n)
+                .map(|i| 1.0 / (1..=i).product::<i32>() as f64)
+                .sum::<f64>();
+
+        if e.to_string().len() > 11 {
+            println!("{n} {e:.9}");
+        } else {
+            println!("{n} {e}");
+        }
+    }
+}
