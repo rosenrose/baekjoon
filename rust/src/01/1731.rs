08f8d7b72403448b1475cf8a8ca205bf5eb07660
rosenrose <crazytempler@gmail.com>
2023-01-10 14:56:54 +0900

    1731 추론

---
+++ rust/src/01/1731.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.lines().map(|s| s.parse::<i32>().unwrap());
+
+    let nums: Vec<_> = input.skip(1).collect();
+
+    match (
+        nums[1] - nums[0],
+        nums[2] - nums[1],
+        nums[1] / nums[0],
+        nums[2] / nums[1],
+    ) {
+        (d1, d2, ..) if d1 == d2 => println!("{}", nums.last().unwrap() + d1),
+        (.., r1, r2) if r1 == r2 => println!("{}", nums.last().unwrap() * r1),
+        _ => (),
+    }
+}
