6f68804948d49238491f08529a4136055ebbc10d
rosenrose <crazytempler@gmail.com>
2023-01-10 18:04:09 +0900

    19572 가뭄(Small)

---
+++ rust/src/19/19572.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    if let [d1, d2, d3] = parse_float_vec(&buf)[..] {
+        let sum = (d1 + d2 + d3) / 2.0;
+        let (a, b, c) = (sum - d3, sum - d2, sum - d1);
+
+        if [a, b, c].iter().any(|&i| i <= 0.0) {
+            println!("-1");
+            return;
+        }
+
+        println!("1");
+        println!("{a:.1} {b:.1} {c:.1}");
+    }
+}
+
+fn parse_float_vec(buf: &String) -> Vec<f64> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
