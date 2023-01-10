db0ba3d106b895fb42b4a77b5c992b52fc4f7a77
rosenrose <crazytempler@gmail.com>
2023-01-10 20:35:16 +0900

    7785 회사에 있는 사람

---
+++ rust/src/07/7785.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n: i32 = input.next().unwrap().parse().unwrap();
+    let mut names = HashSet::new();
+
+    for (name, in_out) in (0..n).map(|_| (input.next().unwrap(), input.next().unwrap())) {
+        match in_out {
+            "enter" => {
+                names.insert(name);
+            }
+            "leave" => {
+                names.remove(name);
+            }
+            _ => (),
+        }
+    }
+
+    let mut names = Vec::from_iter(names);
+    names.sort_unstable();
+
+    for name in names.iter().rev() {
+        writeln!(output, "{name}").unwrap();
+    }
+
+    print!("{output}");
+}
