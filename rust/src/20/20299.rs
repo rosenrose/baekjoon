91bd9110c106916ca6cace1487fb2a76034f0b8f
rosenrose <crazytempler@gmail.com>
2023-01-10 18:19:48 +0900

    20299 3대 측정

---
+++ rust/src/20/20299.rs
@@ -0,0 +1,28 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut input = || input.next().unwrap();
+    let mut output = String::new();
+
+    let (n, team, personal) = (input(), input(), input());
+    let mut count = 0;
+
+    (0..n)
+        .filter_map(|_| {
+            let rating = [input(), input(), input()];
+
+            (rating.iter().sum::<i32>() >= team && rating.iter().all(|&r| r >= personal))
+                .then(|| rating)
+        })
+        .for_each(|[x1, x2, x3]| {
+            count += 1;
+            write!(output, "{x1} {x2} {x3} ").unwrap();
+        });
+
+    println!("{count}\n{output}");
+}
