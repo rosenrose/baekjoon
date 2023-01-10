9616fbf204f91f8f1ae3be845b8c7014cfc9d738
rosenrose <crazytempler@gmail.com>
2023-01-10 17:27:17 +0900

    14582 오늘도 졌다

---
+++ rust/src/14/14582.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+
+    let team1: Vec<_> = (0..9).map(|_| input.next().unwrap()).collect();
+    let team2: Vec<_> = (0..9).map(|_| input.next().unwrap()).collect();
+
+    let mut was_winning = false;
+    let (mut score1, mut score2) = (0, 0);
+
+    for i in 0..9 {
+        score1 += team1[i];
+
+        if !was_winning && (score1 > score2) {
+            was_winning = true;
+        }
+
+        score2 += team2[i];
+    }
+
+    println!("{}", if was_winning { "Yes" } else { "No" });
+}
