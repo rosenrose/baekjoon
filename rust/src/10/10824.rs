52b5e75c9e1cb33317ac6a650ad072c336adb024
rosenrose <crazytempler@gmail.com>
2023-01-11 21:46:34 +0900

    10824 수정

--- rust/src/10/10824.rs
+++ rust/src/10/10824.rs
@@ -2,16 +2,9 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
-    let mut input = buf.split_whitespace();
-    let mut input = || input.next().unwrap();
-
-    let (a, b, c, d) = (input(), input(), input(), input());
-    let ab = parse_int([a, b].concat());
-    let cd = parse_int([c, d].concat());
+    let [a, b, c, d] = buf.split_whitespace().collect::<Vec<_>>()[..] else { return };
+    let ab: i64 = [a, b].concat().parse().unwrap();
+    let cd: i64 = [c, d].concat().parse().unwrap();
 
     println!("{}", ab + cd);
-}
-
-fn parse_int(buf: String) -> i64 {
-    buf.parse().unwrap()
 }
