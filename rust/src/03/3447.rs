c78f28e6dcdc6e98a35f8273227f276954fb94b6
rosenrose <crazytempler@gmail.com>
2023-01-11 23:36:42 +0900

    3447 수정

--- rust/src/03/3447.rs
+++ rust/src/03/3447.rs
@@ -2,13 +2,12 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    buf.lines().for_each(|line| {
-        let mut line = line.to_string();
 
-        while line.contains("BUG") {
-            line = line.replace("BUG", "");
+    for mut input in buf.lines().map(|line| line.to_string()) {
+        while input.contains("BUG") {
+            input = input.replace("BUG", "");
         }
 
-        println!("{line}");
-    });
+        println!("{input}");
+    }
 }
