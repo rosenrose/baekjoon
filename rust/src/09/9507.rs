af56f076b79a61ed6dab0c07b70f1f6135b9ecbd
rosenrose <crazytempler@gmail.com>
2023-01-11 15:01:04 +0900

    9507 Generations of Tribbles

---
+++ rust/src/09/9507.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.lines().map(|s| s.parse::<i32>().unwrap());
+
+    for n in input.skip(1) {
+        match n {
+            0..=1 => println!("1"),
+            2 => println!("2"),
+            3 => println!("4"),
+            _ => {
+                let (mut a, mut b, mut c, mut d) = (1_i64, 1, 2, 4);
+
+                for _ in 0..n - 3 {
+                    (a, b, c, d) = (b, c, d, a + b + c + d);
+                }
+
+                println!("{d}");
+            }
+        }
+    }
+}
