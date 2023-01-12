f6a366c7fbbccff44923186b81343f81174115ab
rosenrose <crazytempler@gmail.com>
2023-01-12 20:56:30 +0900

    10384 팬그램

---
+++ rust/src/10/10384.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+
+    for (i, input) in buf.lines().enumerate().skip(1) {
+        let counts = input.to_lowercase().chars().fold([0; 26], |mut acc, c| {
+            if !c.is_alphabetic() {
+                return acc;
+            }
+
+            acc[c as usize - 'a' as usize] += 1;
+            acc
+        });
+
+        print!("Case {i}: ");
+
+        if counts.iter().all(|&c| c >= 3) {
+            println!("Triple pangram!!!");
+            continue;
+        }
+        if counts.iter().all(|&c| c >= 2) {
+            println!("Double pangram!!");
+            continue;
+        }
+        if counts.iter().all(|&c| c >= 1) {
+            println!("Pangram!");
+            continue;
+        }
+
+        println!("Not a pangram");
+    }
+}
