78ce8d75778255fc6dcfa4219336a114b359ee42
rosenrose <crazytempler@gmail.com>
2023-01-13 00:22:50 +0900

    10810 공 넣기

---
+++ rust/src/10/10810.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    let (n, m) = (input(), input());
+    let balls = (0..m).fold(vec![0; n], |mut acc, _| {
+        let (i, j, k) = (input() - 1, input() - 1, input());
+
+        for idx in i..=j {
+            acc[idx] = k;
+        }
+
+        acc
+    });
+
+    for b in balls {
+        print!("{b} ");
+    }
+}
