af03c5d3f0538e68d8f4fe6c145a34412282881b
rosenrose <crazytempler@gmail.com>
2023-01-13 00:28:23 +0900

    7523 Gauß

---
+++ rust/src/07/7523.rs
@@ -0,0 +1,20 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i64>().unwrap());
+    let mut input = || input.next().unwrap();
+    let mut output = String::new();
+
+    let sum = |num: i64| (num * (num + 1)) / 2;
+
+    for (i, (n, m)) in (1..=input()).map(|i| (i, (input(), input()))) {
+        writeln!(output, "Scenario #{i}:").unwrap();
+        writeln!(output, "{}\n", sum(m) - sum(n - 1)).unwrap();
+    }
+
+    print!("{output}");
+}
