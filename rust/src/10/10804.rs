a524f96def508978753c24aa509f23b06721e7f9
rosenrose <crazytempler@gmail.com>
2023-01-14 16:59:40 +0900

    10804 카드 역배치

---
+++ rust/src/10/10804.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap());
+
+    let mut cards: Vec<_> = (1..=20).collect();
+
+    for (mut a, mut b) in (0..10).map(|_| (input.next().unwrap() - 1, input.next().unwrap() - 1)) {
+        while a < b {
+            cards.swap(a, b);
+            a += 1;
+            b -= 1;
+        }
+    }
+
+    for card in cards {
+        print!("{card} ");
+    }
+}
