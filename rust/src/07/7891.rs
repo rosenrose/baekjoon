e47761ff1185fd8bd86ce47a4b85bb5771cbff7c
rosenrose <crazytempler@gmail.com>
2023-01-16 00:14:23 +0900

    7891 Can you add this?

---
+++ rust/src/07/7891.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = || input.next().unwrap();
+
+    for (x, y) in (0..input()).map(|_| (input(), input())) {
+        println!("{}", x + y);
+    }
+}
