14cf88cffd4606752ccc4a28e9f3ad4cefcf4b26
rosenrose <crazytempler@gmail.com>
2023-01-15 23:26:47 +0900

    4562 No Brainer

---
+++ rust/src/04/4562.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap());
+    let mut input = || input.next().unwrap();
+
+    for (x, y) in (0..input()).map(|_| (input(), input())) {
+        println!("{}", if x >= y { "MMM BRAINS" } else { "NO BRAINS" });
+    }
+}
