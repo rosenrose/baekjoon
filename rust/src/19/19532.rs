aefeee0b4af656a5386d2eca501a655059ef8f9e
rosenrose <crazytempler@gmail.com>
2023-01-15 23:21:09 +0900

    19532 수학은 비대면강의입니다

---
+++ rust/src/19/19532.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let [a, b, c, d, e, f] = parse_int_vec(&buf)[..] else { return };
+
+    let x = (c * e - f * b) / (a * e - d * b);
+    let y = (c * d - f * a) / (b * d - e * a);
+
+    println!("{x} {y}");
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
