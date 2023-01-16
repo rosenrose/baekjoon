52231e8662de7dae8c61aecee845ff8b2d64d397
rosenrose <crazytempler@gmail.com>
2023-01-16 18:15:03 +0900

    1021 회전하는 큐

---
+++ rust/src/01/1021.rs
@@ -0,0 +1,32 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let (n, _) = (input.next().unwrap(), input.next());
+    let mut queue: VecDeque<_> = (1..=n).collect();
+    let mut count = 0;
+
+    for i in input {
+        let pos = queue.iter().position(|&num| num == i).unwrap();
+
+        while let Some(&num) = queue.front() {
+            if num == i {
+                queue.pop_front();
+                break;
+            }
+
+            if pos <= queue.len() / 2 {
+                queue.rotate_left(1);
+            } else {
+                queue.rotate_right(1);
+            }
+
+            count += 1;
+        }
+    }
+
+    println!("{count}");
+}
