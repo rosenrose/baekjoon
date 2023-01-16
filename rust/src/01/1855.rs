bbe0ce6e0162d1af2cfe5c77606f821ab6e2cfb8
rosenrose <crazytempler@gmail.com>
2023-01-17 00:47:57 +0900

    1855 암호

---
+++ rust/src/01/1855.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.lines();
+
+    let k: usize = input.next().unwrap().parse().unwrap();
+    let table: Vec<_> = input
+        .next()
+        .unwrap()
+        .as_bytes()
+        .chunks(k)
+        .enumerate()
+        .map(|(i, chunk)| {
+            let mut chunk = chunk.to_vec();
+
+            if i % 2 == 1 {
+                chunk.reverse();
+            }
+
+            chunk
+        })
+        .collect();
+
+    for col in 0..k {
+        for row in table.iter() {
+            print!("{}", row[col] as char);
+        }
+    }
+}
