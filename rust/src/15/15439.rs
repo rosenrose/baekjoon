c00e40c5a2db12e3c354763798d16befa281de93
rosenrose <crazytempler@gmail.com>
2023-01-16 15:54:08 +0900

    15439 Vera and Outfits

---
+++ rust/src/15/15439.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    println!("{}", n * (n - 1));
+}
