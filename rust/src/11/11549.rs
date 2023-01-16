9196d77dec4414e521c0eda8cc0b6cf35b3f848b
rosenrose <crazytempler@gmail.com>
2023-01-16 15:36:59 +0900

    11549 Identifying tea

---
+++ rust/src/11/11549.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let n = input.next().unwrap();
+
+    println!("{}", input.filter(|&num| num == n).count());
+}
