03b5af1e38914719527f7d919d930099d1d6a35e
rosenrose <crazytempler@gmail.com>
2023-01-17 17:31:38 +0900

    5671 호텔 방 번호

---
+++ rust/src/05/5671.rs
@@ -0,0 +1,32 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut output = String::new();
+
+    while let (Some(n), Some(m)) = (input.next(), input.next()) {
+        let count = (n..=m)
+            .filter(|num| {
+                let mut num = *num;
+                let mut visited = [false; 10];
+
+                while num > 0 {
+                    if visited[num % 10] {
+                        return false;
+                    }
+
+                    visited[num % 10] = true;
+                    num /= 10;
+                }
+
+                true
+            })
+            .count();
+
+        writeln!(output, "{count}").unwrap();
+    }
+
+    print!("{output}");
+}
