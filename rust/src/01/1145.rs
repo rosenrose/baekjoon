dd3600d376e1a7a6bcbaea34d9dd8f8776850122
rosenrose <crazytempler@gmail.com>
2023-01-18 17:12:01 +0900

    1145 적어도 대부분의 배수

---
+++ rust/src/01/1145.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let nums = parse_int_vec(&buf);
+    let mut min_lcm = i32::MAX;
+
+    for i in 0..3 {
+        for j in i + 1..4 {
+            for k in j + 1..5 {
+                let lcm = get_lcm(nums[i], nums[j]);
+                let lcm = get_lcm(lcm, nums[k]);
+
+                min_lcm = lcm.min(min_lcm);
+            }
+        }
+    }
+
+    println!("{min_lcm}");
+}
+
+fn get_lcm(a: i32, b: i32) -> i32 {
+    a / get_gcd(a, b) * b
+}
+
+fn get_gcd(mut a: i32, mut b: i32) -> i32 {
+    loop {
+        if b == 0 {
+            return a;
+        }
+
+        (a, b) = (b, a % b);
+    }
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().flat_map(str::parse).collect()
+}
