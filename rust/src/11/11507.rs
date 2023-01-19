aa114348c0a6e03db7117b0e1a556960e17a1f55
rosenrose <crazytempler@gmail.com>
2023-01-19 17:17:28 +0900

    11507 카드셋트

---
+++ rust/src/11/11507.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut cards = [[false; 13]; 4];
+
+    for card in buf.trim().as_bytes().chunks(3) {
+        let shape = match card[0] as char {
+            'P' => 0_usize,
+            'K' => 1,
+            'H' => 2,
+            'T' => 3,
+            _ => Default::default(),
+        };
+        let number = ((card[1] - '0' as u8) * 10 + card[2] - '0' as u8 - 1) as usize;
+
+        if cards[shape][number] {
+            println!("GRESKA");
+            return;
+        }
+
+        cards[shape][number] = true;
+    }
+
+    for card in cards {
+        print!("{} ", card.iter().filter(|&b| !b).count());
+    }
+}
