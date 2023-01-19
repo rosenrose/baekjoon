aad2e7a5f4132819ba0d61468724205edacbc84a
rosenrose <crazytempler@gmail.com>
2023-01-19 15:29:35 +0900

    2526 싸이클

---
+++ rust/src/02/2526.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let [n, p] = parse_int_vec(&buf)[..] else { return };
+    let mut nums = vec![n];
+    let mut next = n;
+
+    let pos = loop {
+        next = (next * n) % p;
+
+        if let Some(p) = nums.iter().position(|&num| num == next) {
+            break p;
+        }
+
+        nums.push(next);
+    };
+    // println!("{nums:?}");
+    println!("{}", &nums[pos..].len());
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().flat_map(str::parse).collect()
+}
