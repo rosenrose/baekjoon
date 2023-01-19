b51489143fbfe5100a2263547091f868d794d8d1
rosenrose <crazytempler@gmail.com>
2023-01-19 17:27:30 +0900

    6443 수정

--- rust/src/06/6443.rs
+++ rust/src/06/6443.rs
@@ -11,16 +11,17 @@ fn main() {
 
         let len = word.len();
 
-        while let Some(i) = (1..len).rfind(|&i| word[i - 1] < word[i]) {
+        loop {
             writeln!(output, "{}", String::from_utf8(word.clone()).unwrap()).unwrap();
 
+            let Some(i) = (1..len).rfind(|&i| word[i - 1] < word[i]) else {
+                break;
+            };
             let j = (i..len).rfind(|&j| word[j] > word[i - 1]).unwrap();
 
             word.swap(i - 1, j);
             quick_sort(&mut word[i..], len - i);
         }
-
-        writeln!(output, "{}", String::from_utf8(word).unwrap()).unwrap();
     }
 
     print!("{output}");
