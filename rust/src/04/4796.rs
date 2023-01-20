4f5df9237389e97d1b8d88465645be9d7e39764c
rosenrose <crazytempler@gmail.com>
2023-01-20 18:09:56 +0900

    4796 캠핑

---
+++ rust/src/04/4796.rs
@@ -0,0 +1,21 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = || input.next().unwrap();
+    let mut output = String::new();
+
+    for (i, (camping, max, vacation)) in (1..).map(|i| (i, (input(), input(), input()))) {
+        if (camping, max, vacation) == (0, 0, 0) {
+            break;
+        }
+
+        let days = (vacation / max) * camping + (vacation % max).min(camping);
+
+        writeln!(output, "Case {i}: {days}").unwrap();
+    }
+
+    print!("{output}");
+}
