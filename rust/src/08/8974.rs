f4d6571bba095bb4e1befeae0aa4220be2f6cbdb
rosenrose <crazytempler@gmail.com>
2023-01-23 12:02:13 +0900

    1292 8974 수정

--- rust/src/08/8974.rs
+++ rust/src/08/8974.rs
@@ -1,35 +1,18 @@
-struct Sequence {
-    num: i32,
-    counter: i32,
-}
-
-impl Sequence {
-    fn new() -> Self {
-        Self { num: 1, counter: 1 }
-    }
-}
-
-impl Iterator for Sequence {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.counter == 0 {
-            self.num += 1;
-            self.counter = self.num;
-        }
-
-        self.counter -= 1;
-
-        Some(self.num)
-    }
-}
-
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
     let [a, b] = parse_int_vec(&buf)[..] else { return };
-    let sequence = Sequence::new();
+    let sequence = (0..).scan((0, 1), |(num, counter), _| {
+        if *counter == 1 {
+            *num += 1;
+            *counter = *num;
+        } else {
+            *counter -= 1;
+        }
+
+        Some(*num)
+    });
 
     println!("{}", sequence.skip(a - 1).take(b - a + 1).sum::<i32>());
 }
