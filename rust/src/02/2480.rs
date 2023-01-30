1d1dbebf45646d3469489801d9e95c9c7f9e9e96
rosenrose <crazytempler@gmail.com>
2023-01-30 22:38:30 +0900

    2480 수정

--- rust/src/02/2480.rs
+++ rust/src/02/2480.rs
@@ -3,12 +3,11 @@ fn main() {
     std::io::stdin().read_line(&mut buf).unwrap();
 
     let nums = parse_int_vec(&buf);
-
     let prize = match nums[..] {
         [a, b, c] if a == b && b == c => 10000 + a * 1000,
         [a, b, _] if a == b => 1000 + a * 100,
-        [a, _, b] if a == b => 1000 + a * 100,
-        [_, a, b] if a == b => 1000 + a * 100,
+        [_, b, c] if b == c => 1000 + b * 100,
+        [a, _, c] if c == a => 1000 + c * 100,
         _ => nums.iter().max().unwrap() * 100,
     };
 
