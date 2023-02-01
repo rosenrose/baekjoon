15703c0e1d7c4c02871fb869aa1011f1697d172c
rosenrose <crazytempler@gmail.com>
2023-02-02 07:44:26 +0900

    1225 수정

--- rust/src/01/1225.rs
+++ rust/src/01/1225.rs
@@ -2,10 +2,10 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
-    let mut input = buf.split_ascii_whitespace();
+    let mut input = buf.split_whitespace();
     let (a, b) = (input.next().unwrap(), input.next().unwrap());
 
-    let digit_sum = |s: &str| s.chars().map(|c| c as i64 - '0' as i64).digit_sum::<i64>();
+    let digit_sum = |s: &str| s.chars().map(|c| c as i64 - '0' as i64).sum::<i64>();
 
     println!("{}", digit_sum(a) * digit_sum(b));
 }
