80c945b2b649c5d7a82b1fac18b15eb5d58001e2
rosenrose <crazytempler@gmail.com>
2023-02-03 21:45:16 +0900

    1120 문자열

---
+++ rust/src/01/1120.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut input = buf.split_whitespace().map(str::as_bytes);
+    let (a, b) = (input.next().unwrap(), input.next().unwrap());
+
+    let min_diff = b
+        .windows(a.len())
+        .map(|window| a.iter().zip(window).filter(|(a, b)| a != b).count())
+        .min()
+        .unwrap();
+
+    println!("{min_diff}");
+}
