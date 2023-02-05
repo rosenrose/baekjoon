44eb774e5424aad9e8e8bc3ebe322a9bb48fe5dd
rosenrose <crazytempler@gmail.com>
2023-02-05 14:34:14 +0900

    4811 수정

--- rust/src/04/4811.rs
+++ rust/src/04/4811.rs
@@ -9,13 +9,7 @@ fn main() {
     let mut cache = vec![1, 1, 2];
 
     for i in 3..=30 {
-        let mut next = (0..i / 2).map(|j| cache[j] * cache[i - 1 - j]).sum::<i64>() * 2;
-
-        if i % 2 == 1 {
-            next += cache[i / 2] * cache[i / 2];
-        }
-
-        cache.push(next);
+        cache.push((0..i).map(|j| cache[j] * cache[i - 1 - j]).sum::<i64>());
     }
     // println!("{cache:?}");
     for n in input.take_while(|&n| n != 0) {
