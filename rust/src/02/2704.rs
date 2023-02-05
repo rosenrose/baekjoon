4fa1d8c08f4a899af354a95aee32f083b747832a
rosenrose <crazytempler@gmail.com>
2023-02-05 16:42:31 +0900

    2704 이진법 시계

---
+++ rust/src/02/2704.rs
@@ -0,0 +1,26 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.lines().map(parse_time);
+    let mut output = String::new();
+
+    for bin_time in input.skip(1) {
+        let vertical = (0..bin_time[0].len())
+            .map(|i| ((0..3).map(|j| bin_time[j].chars().nth(i).unwrap())).collect())
+            .collect::<Vec<String>>()
+            .join("");
+        let horizontal = bin_time.join("");
+
+        writeln!(output, "{vertical} {horizontal}").unwrap();
+    }
+
+    print!("{output}");
+}
+
+fn parse_time(s: &str) -> Vec<String> {
+    s.split(':')
+        .map(|s| format!("{:06b}", s.parse::<i32>().unwrap()))
+        .collect()
+}
