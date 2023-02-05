1a07b9af69a660918f14b1078480758f2235f0cd
rosenrose <crazytempler@gmail.com>
2023-02-05 15:24:50 +0900

    1748 수정

--- rust/src/01/1748.rs
+++ rust/src/01/1748.rs
@@ -2,8 +2,8 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
-    let length = buf.trim().len() as i64;
-    let n: i64 = buf.trim().parse().unwrap();
+    let n: u32 = buf.trim().parse().unwrap();
+    let length = n.ilog10() + 1;
 
     let (mut digits, mut count) = (0, 9);
 
