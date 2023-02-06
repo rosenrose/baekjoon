dcffdc575425c041af0ac86ce11449cba00fe545
rosenrose <crazytempler@gmail.com>
2023-02-06 19:04:11 +0900

    1904 01타일

---
+++ rust/src/01/1904.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+    const M: i32 = 15746;
+
+    if n <= 2 {
+        println!("{n}");
+        return;
+    }
+
+    let (mut a, mut b) = (1, 2);
+
+    for _ in 0..n - 2 {
+        (a, b) = (b, (a + b) % M);
+    }
+
+    println!("{b}");
+}
