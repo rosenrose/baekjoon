fce73a916e31f1778ea7823ae102f1b499fa7090
rosenrose <crazytempler@gmail.com>
2023-02-06 13:33:07 +0900

    2108 2145 수정

--- rust/src/02/2108.rs
+++ rust/src/02/2108.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = buf.lines().flat_map(str::parse::<i32>);
 
     let len = input.next().unwrap() as usize;
     let mut sum = 0;
