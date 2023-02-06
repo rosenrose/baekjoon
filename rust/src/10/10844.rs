9de1ea55787a960bacd539a6a0acef32f818091f
rosenrose <crazytempler@gmail.com>
2023-02-06 18:02:49 +0900

    10844 쉬운 계단 수

---
+++ rust/src/10/10844.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+    const M: i32 = 1_000_000_000;
+
+    match n {
+        1 => println!("9"),
+        2 => println!("17"),
+        _ => {
+            let mut digit_counts = vec![1, 1, 2, 2, 2, 2, 2, 2, 2, 1];
+
+            for _ in 0..n - 2 {
+                digit_counts = (0..10)
+                    .map(|i| match i {
+                        0 => digit_counts[1],
+                        9 => digit_counts[8],
+                        _ => (digit_counts[i - 1] + digit_counts[i + 1]) % M,
+                    })
+                    .collect();
+            }
+            // println!("{digit_counts:?}");
+            let stair_nums = digit_counts.into_iter().reduce(|a, b| (a + b) % M).unwrap();
+
+            println!("{stair_nums}");
+        }
+    }
+}
