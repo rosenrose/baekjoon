145ceed1fa812c5d945b02366ea4b9da49d466b4
rosenrose <crazytempler@gmail.com>
2023-02-06 18:29:18 +0900

    1942 수정

--- rust/src/01/1942.rs
+++ rust/src/01/1942.rs
@@ -1,31 +1,38 @@
+enum Time {
+    H,
+    M,
+    S,
+}
+
 use std::io;
+use Time::{H, M, S};
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().map(parse_time);
+    let (h, m, s) = (H as usize, M as usize, S as usize);
 
-    for (start, end) in (0..3).map(|_| (input.next().unwrap(), input.next().unwrap())) {
-        let [mut h, mut m, mut s] = start[..] else { return };
+    for (mut start, end) in (0..3).map(|_| (input.next().unwrap(), input.next().unwrap())) {
         let mut count = 0;
 
-        while [h, m, s] != end[..] {
-            if (h + m + s) % 3 == 0 {
+        loop {
+            if start.iter().sum::<i32>() % 3 == 0 {
                 count += 1;
             }
-            // println!("{h}:{m}:{s}");
-            s += 1;
 
-            m += s / 60;
-            s %= 60;
+            if start == end {
+                break;
+            }
+            // println!("{start:?}");
+            start[s] += 1;
 
-            h += m / 60;
-            m %= 60;
+            start[m] += start[s] / 60;
+            start[s] %= 60;
 
-            h %= 24;
-        }
+            start[h] += start[m] / 60;
+            start[m] %= 60;
 
-        if (h + m + s) % 3 == 0 {
-            count += 1;
+            start[h] %= 24;
         }
 
         println!("{count}");
