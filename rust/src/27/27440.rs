c0c22f5d58896db666bafbd50d539dd484ae6bd2
rosenrose <crazytempler@gmail.com>
2023-02-07 16:25:11 +0900

    27440 1로 만들기 3

---
+++ rust/src/27/27440.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i64 = buf.trim().parse().unwrap();
+    let mut cache = HashMap::from([(1, 0), (2, 1), (3, 1)]);
+
+    println!("{}", make_1_count(n, &mut cache));
+}
+
+fn make_1_count(n: i64, cache: &mut HashMap<i64, i32>) -> i32 {
+    if let Some(count) = cache.get(&n) {
+        return *count;
+    }
+
+    let mut list = Vec::new();
+
+    if n % 3 == 0 {
+        list.push(make_1_count(n / 3, cache) + 1);
+    }
+    if n % 2 == 0 {
+        list.push(make_1_count(n / 2, cache) + 1);
+    }
+    if (n - 1) % 3 == 0 || (n - 1) % 2 == 0 {
+        list.push(make_1_count(n - 1, cache) + 1);
+    }
+    if (n - 2) % 3 == 0 {
+        list.push(make_1_count(n - 2, cache) + 2);
+    }
+
+    let count = *list.iter().min().unwrap();
+    cache.insert(n, count);
+
+    count
+}
