622e08b6e3d039afc39f75aeaf34130f9ef98d91
rosenrose <crazytempler@gmail.com>
2023-02-07 16:27:37 +0900

    12348 분해합 2

---
+++ rust/src/12/12348.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let input = buf.trim();
+    let n: i64 = input.parse().unwrap();
+
+    let min = (n - (9 * input.len() as i64)).max(1);
+    let max = n - 1;
+
+    for i in min..=max {
+        if d(i) == n {
+            println!("{i}");
+            return;
+        }
+    }
+
+    println!("0");
+}
+
+fn d(n: i64) -> i64 {
+    let digit_sum: i64 = n.to_string().chars().map(|c| c as i64 - '0' as i64).sum();
+    n + digit_sum
+}
