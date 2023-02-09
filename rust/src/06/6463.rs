c95c30f906c616fc38807e1e0f364ae273c43554
rosenrose <crazytempler@gmail.com>
2023-02-09 16:11:44 +0900

    6463 팩트

---
+++ rust/src/06/6463.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+const DIGITS: usize = 18;
+const EXP: i128 = 10_i128.pow(DIGITS as u32);
+
+struct BigInt(Vec<i64>);
+
+impl BigInt {
+    fn mul(&self, other: i64) -> Self {
+        let mut carry = 0;
+        let mut result: Vec<_> = self
+            .0
+            .iter()
+            .map(|&num| {
+                let temp = carry + num as i128 * other as i128;
+                carry = temp / EXP;
+
+                (temp % EXP) as i64
+            })
+            .collect();
+
+        if carry > 0 {
+            result.push(carry as i64);
+        }
+
+        Self(result)
+    }
+}
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.lines().flat_map(str::parse::<usize>);
+
+    let mut cache = vec![BigInt(vec![1])];
+
+    for i in 1..=9999 {
+        let mut factorial = cache.last().unwrap().mul(i);
+
+        while factorial.0[0] == 0 {
+            factorial.0.remove(0);
+        }
+
+        let next = factorial.0.into_iter().take(2).collect();
+
+        cache.push(BigInt(next));
+    }
+
+    for n in input {
+        let non_zero = cache[n].0[0]
+            .to_string()
+            .chars()
+            .rev()
+            .find(|&c| c != '0')
+            .unwrap();
+
+        println!("{n:>5} -> {non_zero}");
+    }
+}
