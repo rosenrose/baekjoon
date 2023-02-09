276eaf4dc39b49d5661f2c2155cd9fdc41cbb620
rosenrose <crazytempler@gmail.com>
2023-02-09 14:51:47 +0900

    7489 팩토리얼

---
+++ rust/src/07/7489.rs
@@ -0,0 +1,54 @@
+use std::io;
+
+const DIGITS: usize = 18;
+const EXP: i128 = 10_i128.pow(DIGITS as u32);
+
+struct BigInt(Vec<i64>);
+
+impl BigInt {
+    fn mul(&self, other: i64) -> Self {
+        let mut carry = 0;
+        let mut result: Vec<_> = self
+            .0
+            .iter()
+            .map(|&num| {
+                let temp = carry + num as i128 * other as i128;
+                carry = temp / EXP;
+
+                (temp % EXP) as i64
+            })
+            .collect();
+
+        if carry > 0 {
+            result.push(carry as i64);
+        }
+
+        Self(result)
+    }
+}
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.lines().flat_map(str::parse::<i64>);
+
+    for n in input.skip(1) {
+        let factorial = (1..=n).fold(BigInt(vec![1]), |mut acc, num| {
+            acc = acc.mul(num);
+
+            while acc.0[0] == 0 {
+                acc.0.remove(0);
+            }
+
+            acc
+        });
+
+        let non_zero = factorial.0[0]
+            .to_string()
+            .chars()
+            .rev()
+            .find(|&c| c != '0')
+            .unwrap();
+
+        println!("{non_zero}");
+    }
+}
