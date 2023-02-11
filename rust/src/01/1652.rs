891130f1292c4a61fdce2e840080c97b81ec335e
rosenrose <crazytempler@gmail.com>
2023-02-12 00:45:09 +0900

    1652 수정

--- rust/src/01/1652.rs
+++ rust/src/01/1652.rs
@@ -2,16 +2,8 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
+
     let room: Vec<_> = buf.lines().skip(1).map(str::to_owned).collect();
-
-    let count_rest = |row: &String| {
-        row.split('X')
-            .filter(|s| !s.is_empty() && s.contains(".."))
-            .count()
-    };
-
-    let horizontal_count: usize = room.iter().map(count_rest).sum();
-
     let room_inversed: Vec<String> = (0..room[0].len())
         .map(|i| {
             (0..room.len())
@@ -20,7 +12,10 @@ fn main() {
         })
         .collect();
     // println!("{room_inversed:?}");
-    let vertical_count: usize = room_inversed.iter().map(count_rest).sum();
+    let count_rest = |row: &String| row.split('X').filter(|s| s.contains("..")).count();
 
-    println!("{horizontal_count} {vertical_count}");
+    let horizontal: usize = room.iter().map(count_rest).sum();
+    let vertical: usize = room_inversed.iter().map(count_rest).sum();
+
+    println!("{horizontal} {vertical}");
 }
