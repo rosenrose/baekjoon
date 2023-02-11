f428d5694df9c21ba1b74939a5d180f8dc66c291
rosenrose <crazytempler@gmail.com>
2023-02-12 01:05:30 +0900

    4821 페이지 세기

---
+++ rust/src/04/4821.rs
@@ -0,0 +1,35 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.lines();
+    let mut output = String::new();
+
+    while let Some(pages) = input.next() {
+        if pages == "0" {
+            break;
+        }
+
+        let pages = parse_int(pages);
+        let mut is_print = vec![false; pages + 1];
+
+        for range in input.next().unwrap().split(',') {
+            let mut range = range.split('-').map(parse_int);
+            let low = range.next().unwrap();
+            let high = range.next().unwrap_or(low);
+
+            for i in low..=high.min(pages) {
+                is_print[i] = true;
+            }
+        }
+
+        writeln!(output, "{}", is_print.iter().filter(|&&p| p).count()).unwrap();
+    }
+
+    print!("{output}");
+}
+
+fn parse_int(buf: &str) -> usize {
+    buf.parse().unwrap()
+}
