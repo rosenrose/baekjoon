d666e9c535e68da58bd3897d17ef709fa2eb48c1
rosenrose <crazytempler@gmail.com>
2023-02-12 10:36:33 +0900

    3568 iSharp

---
+++ rust/src/03/3568.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut input = buf.split_ascii_whitespace();
+    let type_ = input.next().unwrap();
+
+    for var in input.flat_map(|s| s.strip_suffix([',', ';'])) {
+        let (name, extra) = if let Some(i) = var.find(['[', ']', '&', '*']) {
+            var.split_at(i)
+        } else {
+            (var, "")
+        };
+
+        let extra = extra.chars().rev().collect::<String>().replace("][", "[]");
+
+        println!("{type_}{extra} {name};");
+    }
+}
