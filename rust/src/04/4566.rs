91126e907aaf7e8f94da786392c8651a6cc599ba
rosenrose <crazytempler@gmail.com>
2023-02-14 02:27:08 +0900

    4566 수정

--- rust/src/04/4566.rs
+++ rust/src/04/4566.rs
@@ -2,43 +2,53 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    const NOT: &str = "not a quine";
+    let regex = Regex::new(r#"^"([A-Z ]+)" \1$"#, false);
 
     for input in buf.lines().take_while(|&input| input != "END") {
-        let Some(start) = input.find('"') else {
-            println!("{NOT}");
-            continue;
-        };
-        let Some(end) = input.rfind('"') else {
-            println!("{NOT}");
-            continue;
-        };
-
-        if start != 0 {
-            println!("{NOT}");
-            continue;
+        if regex.find(input).is_some() {
+            println!("Quine({})", &input[input.rfind('"').unwrap() + 2..]);
+        } else {
+            println!("not a quine");
         }
-        if end - start <= 1 {
-            println!("{NOT}");
-            continue;
-        }
-
-        let phrase = &input[start + 1..end];
-
-        if !phrase.chars().all(|ch| matches!(ch, 'A'..='Z' | ' ')) {
-            println!("{NOT}");
-            continue;
-        }
-
-        if input.chars().nth(end + 1) != Some(' ') {
-            println!("{NOT}");
-            continue;
-        }
-        if &input[(end + 2).min(input.len() - 1)..] != phrase {
-            println!("{NOT}");
-            continue;
-        }
-
-        println!("Quine({phrase})");
     }
+
+    // const NOT: &str = "not a quine";
+
+    // for input in buf.lines().take_while(|&input| input != "END") {
+    //     let Some(start) = input.find('"') else {
+    //         println!("{NOT}");
+    //         continue;
+    //     };
+    //     let Some(end) = input.rfind('"') else {
+    //         println!("{NOT}");
+    //         continue;
+    //     };
+
+    //     if start != 0 {
+    //         println!("{NOT}");
+    //         continue;
+    //     }
+    //     if end - start <= 1 {
+    //         println!("{NOT}");
+    //         continue;
+    //     }
+
+    //     let phrase = &input[start + 1..end];
+
+    //     if !phrase.chars().all(|ch| matches!(ch, 'A'..='Z' | ' ')) {
+    //         println!("{NOT}");
+    //         continue;
+    //     }
+
+    //     if input.chars().nth(end + 1) != Some(' ') {
+    //         println!("{NOT}");
+    //         continue;
+    //     }
+    //     if &input[(end + 2).min(input.len() - 1)..] != phrase {
+    //         println!("{NOT}");
+    //         continue;
+    //     }
+
+    //     println!("Quine({phrase})");
+    // }
 }
