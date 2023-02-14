cf09e327f7ed20540dd7d9514197d00646281d25
rosenrose <crazytempler@gmail.com>
2023-02-14 21:02:16 +0900

    3518 공백왕 빈-칸

---
+++ rust/src/03/3518.rs
@@ -0,0 +1,46 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut output = String::new();
+    let mut max_widths = Vec::new();
+
+    let document: Vec<Vec<_>> = buf
+        .lines()
+        .map(|input| {
+            input
+                .split(' ')
+                .filter(|s| !s.is_empty())
+                .enumerate()
+                .map(|(i, s)| {
+                    if max_widths.get(i).is_none() {
+                        max_widths.push(s.len());
+                    } else {
+                        max_widths[i] = max_widths[i].max(s.len());
+                    }
+
+                    s
+                })
+                .collect()
+        })
+        .collect();
+    // println!("{document:?}\n{max_widths:?}");
+    for row in document {
+        for (i, word) in row.iter().enumerate() {
+            if i > 0 {
+                write!(output, " ").unwrap();
+            }
+
+            if i == row.len() - 1 {
+                write!(output, "{word}").unwrap();
+            } else {
+                write!(output, "{word:width$}", width = max_widths[i]).unwrap();
+            }
+        }
+
+        writeln!(output, "").unwrap();
+    }
+
+    print!("{output}");
+}
