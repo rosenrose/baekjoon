bc2ad8c682dbbd6050ac5a49276fef2f9d5d029c
rosenrose <crazytempler@gmail.com>
2023-02-16 02:32:36 +0900

    17949 Drop The Byte!

---
+++ rust/src/17/17949.rs
@@ -0,0 +1,30 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let mut encrypted = input.next().unwrap().as_bytes().chunks(2);
+
+    for type_ in input.skip(1) {
+        let bytes: Vec<_> = encrypted
+            .by_ref()
+            .take(match type_ {
+                "char" => 1,
+                "int" => 4,
+                "long_long" => 8,
+                _ => Default::default(),
+            })
+            .flatten()
+            .copied()
+            .collect();
+        let chunk = String::from_utf8(bytes).unwrap();
+        let decrypted = u64::from_str_radix(&chunk, 16).unwrap();
+
+        write!(output, "{decrypted} ").unwrap();
+    }
+
+    print!("{output}");
+}
