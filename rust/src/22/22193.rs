c0fef8ed5085871039eb4009d651592dd3bcf159
rosenrose <crazytempler@gmail.com>
2023-02-15 17:17:04 +0900

    22193 수정

--- rust/src/22/22193.rs
+++ rust/src/22/22193.rs
@@ -1,19 +1,54 @@
-use std::collections::VecDeque;
-use std::fmt;
+use std::f64::consts::PI;
+use std::fmt::{self, Write};
 use std::io;
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Mul, MulAssign, Sub};
 
-const DIGITS: usize = 18;
-const EXP: i128 = 10_i128.pow(DIGITS as u32);
+const DIGITS: usize = 3;
+const EXP: i64 = 10_i64.pow(DIGITS as u32);
 
-#[derive(PartialEq)]
-struct BigInt(VecDeque<i64>);
+#[derive(Clone, Copy, Debug)]
+struct Complex(f64, f64);
+
+impl Complex {
+    fn div(&mut self, num: f64) {
+        self.0 /= num;
+        self.1 /= num;
+    }
+}
+
+impl Add for Complex {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+impl Sub for Complex {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1)
+    }
+}
+impl Mul for Complex {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self(
+            self.0 * other.0 - self.1 * other.1,
+            self.0 * other.1 + self.1 * other.0,
+        )
+    }
+}
+impl MulAssign for Complex {
+    fn mul_assign(&mut self, other: Self) {
+        *self = *self * other;
+    }
+}
+
+struct BigInt(Vec<i64>);
 
 impl BigInt {
-    fn new() -> Self {
-        Self(VecDeque::from([0]))
-    }
-
     fn parse(input: &str) -> Self {
         Self(
             input
@@ -33,148 +68,62 @@ impl BigInt {
         )
     }
 
-    fn is_zero(&self) -> bool {
-        self.0.iter().all(|&i| i == 0)
-    }
-
     fn zero_justify(&mut self) {
-        while self.0.len() > 1 && self.0.back() == Some(&0) {
-            self.0.pop_back();
+        while self.0.len() > 1 && self.0.last() == Some(&0) {
+            self.0.pop();
         }
-    }
-
-    fn mul_int(&self, other: i64) -> Self {
-        let mut carry = 0;
-        let mut result: VecDeque<_> = self
-            .0
-            .iter()
-            .map(|&num| {
-                let temp = carry + num as i128 * other.abs() as i128;
-                carry = temp / EXP;
-
-                (temp % EXP) as i64
-            })
-            .collect();
-
-        if carry > 0 {
-            result.push_back(carry as i64);
-        }
-
-        Self(result)
     }
 }
 
-impl Add for &BigInt {
-    type Output = BigInt;
+impl Mul for BigInt {
+    type Output = Self;
 
-    fn add(self, other: Self) -> Self::Output {
-        let mut carry = 0;
-        let mut sum: VecDeque<_> = (0..self.0.len().max(other.0.len()))
-            .map(|i| {
-                let temp = carry
-                    + *self.0.get(i).unwrap_or(&0) as i128
-                    + *other.0.get(i).unwrap_or(&0) as i128;
-                carry = temp / EXP;
+    fn mul(self, other: Self) -> Self {
+        let mut len = 2;
 
-                (temp % EXP) as i64
-            })
-            .collect();
-
-        if carry > 0 {
-            sum.push_back(carry as i64);
+        while len < self.0.len() + other.0.len() {
+            len *= 2;
         }
 
-        BigInt(sum)
-    }
-}
-impl Sub for &BigInt {
-    type Output = BigInt;
+        let mut a = vec![Complex(0.0, 0.0); len];
+        let mut b = vec![Complex(0.0, 0.0); len];
 
-    fn sub(self, other: Self) -> Self::Output {
-        let mut carry = 0;
-        let diff: VecDeque<_> = (0..self.0.len().max(other.0.len()))
-            .map(|i| {
-                let temp = carry + self.0.get(i).unwrap_or(&0) - other.0.get(i).unwrap_or(&0);
+        for i in 0..self.0.len().max(other.0.len()) {
+            if let Some(&num) = self.0.get(i) {
+                a[i] = Complex(num as f64, 0.0);
+            }
+            if let Some(&num) = other.0.get(i) {
+                b[i] = Complex(num as f64, 0.0);
+            }
+        }
 
-                if temp < 0 {
-                    carry = -1;
-                    temp + EXP as i64
-                } else {
-                    carry = 0;
-                    temp
-                }
-            })
-            .collect();
+        fast_fourier_transform(&mut a, false);
+        fast_fourier_transform(&mut b, false);
 
-        let mut result = BigInt(diff);
+        let mut mul: Vec<_> = a.iter().zip(b.iter()).map(|(&a, &b)| a * b).collect();
+
+        fast_fourier_transform(&mut mul, true);
+
+        let mut result = Self(normalize(mul));
 
         result.zero_justify();
         result
     }
 }
-impl Mul for &BigInt {
-    type Output = BigInt;
-
-    fn mul(self, other: Self) -> Self::Output {
-        if self.is_zero() || other.is_zero() {
-            return BigInt::new();
-        }
-        if self.0.len() == 1 {
-            return other.mul_int(self.0[0]);
-        }
-        if other.0.len() == 1 {
-            return self.mul_int(other.0[0]);
-        }
-
-        let m = self.0.len().max(other.0.len()) / 2;
-        let mut half = m.min(self.0.len());
-
-        let y = BigInt(self.0.range(0..half).copied().collect());
-        let x = if half < self.0.len() {
-            BigInt(self.0.range(half..).copied().collect())
-        } else {
-            BigInt::new()
-        };
-
-        half = m.min(other.0.len());
-
-        let z = BigInt(other.0.range(0..half).copied().collect());
-        let w = if half < other.0.len() {
-            BigInt(other.0.range(half..).copied().collect())
-        } else {
-            BigInt::new()
-        };
-
-        let (mut xw, yz) = (&x * &w, &y * &z);
-        let r = &(&x + &y) * &(&w + &z);
-        let mut xz_plus_yw = &r - &(&xw + &yz);
-
-        if !xw.is_zero() {
-            for _ in 0..2 * m {
-                xw.0.push_front(0);
-            }
-        }
-        if !xz_plus_yw.is_zero() {
-            for _ in 0..m {
-                xz_plus_yw.0.push_front(0);
-            }
-        }
-
-        &xw + &(&xz_plus_yw + &yz)
-    }
-}
 
 impl fmt::Display for BigInt {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.iter().rev().enumerate().for_each(|(i, num)| {
-            if i == 0 {
-                write!(f, "{num}").unwrap();
-            } else {
-                write!(f, "{num:0DIGITS$}").unwrap();
-            }
-        });
+        let mut output = String::new();
 
-        write!(f, "")
+        for (i, num) in self.0.iter().rev().enumerate() {
+            if i == 0 {
+                write!(output, "{num}").unwrap();
+            } else {
+                write!(output, "{num:0DIGITS$}").unwrap();
+            }
+        }
+
+        write!(f, "{output}")
     }
 }
 
@@ -184,5 +133,78 @@ fn main() {
 
     let (a, b) = (input.next().unwrap(), input.next().unwrap());
 
-    println!("{}", &a * &b);
+    println!("{}", a * b);
+}
+
+fn fast_fourier_transform(v: &mut Vec<Complex>, is_inverse: bool) {
+    bit_reversal(v);
+
+    let mut len = 2;
+    let direction = if is_inverse { -1.0 } else { 1.0 };
+
+    while len <= v.len() {
+        let angle = -2.0 * PI * direction / len as f64;
+        let wlen = Complex(angle.cos(), angle.sin());
+
+        for i in (0..v.len()).step_by(len) {
+            let mut w = Complex(1.0, 0.0);
+
+            for j in 0..len / 2 {
+                let (even, odd) = (v[i + j], v[i + j + len / 2]);
+
+                v[i + j] = even + odd * w;
+                v[i + j + len / 2] = even - odd * w;
+
+                w *= wlen;
+            }
+        }
+
+        len *= 2;
+    }
+
+    if is_inverse {
+        len = v.len();
+
+        for num in v.iter_mut() {
+            (*num).div(len as f64);
+        }
+    }
+}
+
+fn bit_reversal(v: &mut Vec<Complex>) {
+    let mut rev = 0;
+
+    for i in 1..v.len() {
+        let mut bit = v.len() / 2;
+
+        while rev >= bit {
+            rev -= bit;
+            bit /= 2;
+        }
+
+        rev += bit;
+
+        if i < rev {
+            v.swap(i, rev);
+        }
+    }
+}
+
+fn normalize(v: Vec<Complex>) -> Vec<i64> {
+    let mut carry = 0;
+    let mut result: Vec<_> = v
+        .iter()
+        .map(|complex| {
+            let temp = carry + complex.0.round() as i64;
+            carry = temp / EXP;
+
+            temp % EXP
+        })
+        .collect();
+
+    if carry > 0 {
+        result.push(carry);
+    }
+
+    result
 }
