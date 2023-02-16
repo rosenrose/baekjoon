6905df6a308ea7622d732dc0c1ecc7e3851d0292
rosenrose <crazytempler@gmail.com>
2023-02-16 12:01:26 +0900

    11319 수정

--- rust/src/11/11319.rs
+++ rust/src/11/11319.rs
@@ -2,13 +2,13 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
 
-    for input in buf.lines().skip(1).map(|input| input.to_lowercase()) {
+    for input in buf.lines().skip(1).map(str::to_lowercase) {
         let c = input
-            .matches(|ch| ch != ' ' && !vowels.contains(&ch))
+            .matches(|ch| ch != ' ' && !VOWELS.contains(&ch))
             .count();
-        let v = input.matches(vowels).count();
+        let v = input.matches(VOWELS).count();
 
         println!("{c} {v}");
     }
