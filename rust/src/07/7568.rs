78810d25fe1fdddeca6f6a2fa35036fb6a5ce64f
rosenrose <crazytempler@gmail.com>
2023-02-21 15:13:43 +0900

    7568 수정

--- rust/src/07/7568.rs
+++ rust/src/07/7568.rs
@@ -6,25 +6,24 @@ fn main() {
     let mut input = || input.next().unwrap();
 
     let n = input();
-    let mut volumes: Vec<_> = (0..n).map(|_| ((input(), input()), 1)).collect();
+    let mut volume_ranks: Vec<_> = (0..n).map(|_| ((input(), input()), 1)).collect();
 
-    for i in 0..n {
+    for i in 0..n - 1 {
+        let ((height1, weight1), _) = volume_ranks[i];
+
         for j in i + 1..n {
-            let ((height1, weight1), _) = volumes[i];
-            let ((height2, weight2), _) = volumes[j];
+            let ((height2, weight2), _) = volume_ranks[j];
 
             if height1 > height2 && weight1 > weight2 {
-                let (_, order2) = &mut volumes[j];
-                *order2 += 1;
+                volume_ranks[j].1 += 1;
             }
             if height1 < height2 && weight1 < weight2 {
-                let (_, order1) = &mut volumes[i];
-                *order1 += 1;
+                volume_ranks[i].1 += 1;
             }
         }
     }
 
-    for (_, rank) in volumes {
+    for (_, rank) in volume_ranks {
         print!("{rank} ");
     }
 }
