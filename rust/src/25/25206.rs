2262b512f18e611e51a4b0b1c2d2468f6af7dfe0
rosenrose <crazytempler@gmail.com>
2023-02-22 15:50:28 +0900

    25206 너의 평점은

---
+++ rust/src/25/25206.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut input = || input.next().unwrap();
+
+    let mut credit_sum = 0.0;
+    let sum: f32 = (0..20)
+        .filter_map(|_| {
+            let (_, credit, grade) = (input(), input().parse::<f32>().unwrap(), input());
+
+            (grade != "P").then(|| {
+                credit_sum += credit;
+
+                credit
+                    * match grade {
+                        "A+" => 4.5,
+                        "A0" => 4.0,
+                        "B+" => 3.5,
+                        "B0" => 3.0,
+                        "C+" => 2.5,
+                        "C0" => 2.0,
+                        "D+" => 1.5,
+                        "D0" => 1.0,
+                        "F" => 0.0,
+                        _ => Default::default(),
+                    }
+            })
+        })
+        .sum();
+
+    println!("{}", sum / credit_sum);
+}
