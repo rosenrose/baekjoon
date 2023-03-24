fffd12f9cc86e9ae79259373b25409650d4c1429
rosenrose <crazytempler@gmail.com>
2023-03-24 18:16:55 +0900

    4999 6359 수정

--- rust/src/04/4999.rs
+++ rust/src/04/4999.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let mut input = buf.split_ascii_whitespace();
+    let mut input = buf.lines();
     let (ah, require) = (input.next().unwrap(), input.next().unwrap());
 
     println!(
--- rust/src/06/6359.rs
+++ rust/src/06/6359.rs
@@ -2,17 +2,9 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let input = buf.lines().flat_map(str::parse::<usize>);
+    let input = buf.lines().flat_map(str::parse::<i32>);
 
     for n in input.skip(1) {
-        let mut rooms = vec![false; n];
-
-        for k in 1..=n {
-            for i in (k..=n).step_by(k) {
-                rooms[i - 1] = !rooms[i - 1];
-            }
-        }
-
-        println!("{}", rooms.iter().filter(|&&is_open| is_open).count());
+        println!("{}", (1..).take_while(|i| i * i <= n).count());
     }
 }
