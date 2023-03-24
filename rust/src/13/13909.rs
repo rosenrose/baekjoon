7ddc0794c66153f082658e5786b9d9c6a0063821
rosenrose <crazytempler@gmail.com>
2023-03-24 18:15:10 +0900

    13909 창문 닫기

---
+++ rust/src/13/13909.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: i32 = buf.trim().parse().unwrap();
+
+    println!("{}", (1..).take_while(|i| i * i <= n).count());
+}
