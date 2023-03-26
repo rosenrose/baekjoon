8f8eb3418966f9a38e57331ab63de336bcbfdfaa
rosenrose <crazytempler@gmail.com>
2023-03-26 15:38:20 +0900

    2133 수정

--- rust/src/02/2133.rs
+++ rust/src/02/2133.rs
@@ -10,10 +10,10 @@ fn main() {
     }
 
     let mut cache = vec![0; n / 2 + 1];
-    cache[1] = 3;
+    (cache[0], cache[1]) = (1, 3);
 
     for i in 2..=n / 2 {
-        cache[i] = cache[i - 1] * 3 + (cache[1..=i - 2].iter().sum::<i32>() + 1) * 2;
+        cache[i] = cache[i - 1] * 3 + (cache[0..=i - 2].iter().sum::<i32>()) * 2;
     }
     // println!("{cache:?}");
     println!("{}", cache[n / 2]);
