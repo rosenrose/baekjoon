4df4748941a824cce69778d373fc658697091fdc
rosenrose <crazytempler@gmail.com>
2023-03-26 18:35:07 +0900

    13976 타일 채우기 2

--- rust/src/13/13976.rs
+++ rust/src/13/13976.rs
@@ -1,15 +1,13 @@
-use std::fmt;
-use std::io;
 use std::ops::Mul;
 
-struct Matrix(Vec<Vec<i32>>);
+struct Matrix(Vec<Vec<i64>>);
 
 impl Matrix {
-    fn rem(&self, m: i32) -> Self {
+    fn rem(&self, m: i64) -> Self {
         Self(
             self.0
                 .iter()
-                .map(|row| row.iter().map(|num| num % m).collect())
+                .map(|row| row.iter().map(|num| (num + m) % m).collect())
                 .collect(),
         )
     }
@@ -37,34 +35,34 @@ impl Mul for &Matrix {
     }
 }
 
-impl fmt::Display for Matrix {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for row in self.0.iter() {
-            for cell in row {
-                write!(f, "{cell} ").unwrap();
-            }
-            writeln!(f, "").unwrap();
-        }
-
-        write!(f, "")
-    }
-}
-
-const M: i32 = 1_000;
+const M: i64 = 1_000_000_007;
 
 fn main() {
-    let buf = io::read_to_string(io::stdin()).unwrap();
-    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i64>);
-    let mut input = || input.next().unwrap();
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
 
-    let (n, b) = (input(), input());
-    let a = Matrix(
-        (0..n)
-            .map(|_| (0..n).map(|_| input() as i32).collect())
-            .collect(),
-    );
+    let n: i64 = buf.trim().parse().unwrap();
 
-    print!("{}", pow_rem(&a, b));
+    if n % 2 == 1 {
+        println!("0");
+        return;
+    }
+
+    println!("{}", matrix_pow_rem(n / 2));
+}
+
+fn matrix_pow_rem(n: i64) -> i64 {
+    if n == 1 {
+        return 3;
+    }
+    if n == 2 {
+        return 11;
+    }
+
+    let mut matrix = Matrix(vec![vec![4, -1], vec![1, 0]]);
+    matrix = pow_rem(&mut matrix, n - 2);
+    // println!("{:?}", matrix.0);
+    ((matrix.0[0][0] * 11) % M + (matrix.0[0][1] * 3) % M) % M
 }
 
 fn pow_rem(base: &Matrix, exp: i64) -> Matrix {
