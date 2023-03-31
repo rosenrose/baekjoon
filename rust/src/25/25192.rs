426357b6f3f2e6b1fe70fbd31d31d1e4508f8666
rosenrose <crazytempler@gmail.com>
2023-03-31 15:08:34 +0900

    25192 인사성 밝은 곰곰이

---
+++ rust/src/25/25192.rs
@@ -0,0 +1,14 @@
+use std::collections::HashSet;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input: Vec<_> = buf.lines().skip(1).collect();
+
+    let count: usize = input
+        .split(|&s| s == "ENTER")
+        .map(|names| HashSet::<&&str>::from_iter(names).len())
+        .sum();
+
+    println!("{count}");
+}
