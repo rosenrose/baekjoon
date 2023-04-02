427ebf76dd026c056fa9da7f928ed4f7b747f14c
rosenrose <crazytempler@gmail.com>
2023-04-02 13:17:12 +0900

    25372 수정

--- rust/src/25/25372.rs
+++ rust/src/25/25372.rs
@@ -3,14 +3,13 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let input = buf.split_ascii_whitespace();
     let mut output = String::new();
 
-    for password in input.skip(1) {
+    for input in buf.lines().skip(1) {
         writeln!(
             output,
             "{}",
-            if matches!(password.len(), 6..=9) {
+            if matches!(input.len(), 6..=9) {
                 "yes"
             } else {
                 "no"
