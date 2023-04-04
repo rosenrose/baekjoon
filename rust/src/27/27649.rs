cb47d44d27aa591098d76399be69802069158ac5
rosenrose <crazytempler@gmail.com>
2023-04-04 20:16:02 +0900

    27649 수정

--- rust/src/27/27649.rs
+++ rust/src/27/27649.rs
@@ -4,42 +4,60 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
-    let mut input = buf.trim().to_owned();
+    let input = buf.trim();
     let mut output = String::new();
-
-    while input.contains("  ") {
-        input = input.replace("  ", " ");
-    }
-
-    let mut delim_indices: Vec<_> = input
-        .match_indices(['<', '>', '(', ')'])
-        .chain(
-            ["&&", "||"]
-                .iter()
-                .flat_map(|delim| input.match_indices(delim)),
-        )
-        .collect();
-    delim_indices.sort();
-    // println!("{input}\n{delim_indices:?}");
     let mut cursor = 0;
 
-    for (delim_start, delim) in delim_indices {
-        let token = input[cursor..delim_start].trim();
-        let delim_end = delim_start + delim.len();
+    while let Some((start, end)) = find_delims(input, cursor) {
+        let token = &input[cursor..start];
+        let delim = &input[start..end];
 
-        (if token.is_empty() {
-            write!(output, "{delim} ")
-        } else {
-            write!(output, "{token} {delim} ")
-        })
+        match (token.is_empty(), delim.starts_with(' ')) {
+            (true, true) => write!(output, ""),
+            (true, false) => write!(output, "{delim} "),
+            (false, true) => write!(output, "{token} "),
+            (false, false) => write!(output, "{token} {delim} "),
+        }
         .unwrap();
 
-        cursor = delim_end;
+        cursor = end;
     }
 
-    if cursor < input.len() {
-        write!(output, "{}", &input[cursor..].trim()).unwrap();
+    write!(output, "{}", buf[cursor..].trim()).unwrap();
+    print!("{output}");
+}
+
+fn find_delims(input: &str, start: usize) -> Option<(usize, usize)> {
+    let input = &input[start..];
+    let mut i = 0;
+
+    while i < input.len() {
+        let ch = &input[i..i + 1];
+
+        match ch {
+            "<" | ">" | "(" | ")" => {
+                return Some((start + i, start + i + 1));
+            }
+            "&" | "|" => {
+                if i + 1 == input.len() {
+                    return None;
+                }
+
+                let next_ch = &input[i + 1..i + 2];
+
+                if ch == next_ch {
+                    return Some((start + i, start + i + 2));
+                }
+            }
+            " " => {
+                let end = input[i..].find(|c| c != ' ')?;
+                return Some((start + i, start + i + end));
+            }
+            _ => (),
+        }
+
+        i += 1;
     }
 
-    print!("{}", output.trim());
+    None
 }
