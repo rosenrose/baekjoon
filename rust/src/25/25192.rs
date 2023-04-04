6ac6901f1bdb6bc999e18a678c432ed412af8931
rosenrose <crazytempler@gmail.com>
2023-04-04 12:41:42 +0900

    25192 수정

--- rust/src/25/25192.rs
+++ rust/src/25/25192.rs
@@ -3,7 +3,7 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let input: Vec<_> = buf.lines().skip(1).collect();
+    let input: Vec<_> = buf.lines().skip(2).collect();
 
     let count: usize = input
         .split(|&s| s == "ENTER")
