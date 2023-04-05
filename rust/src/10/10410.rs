b45f6b9abf493304296f5e55381788d11ed2ac86
rosenrose <crazytempler@gmail.com>
2023-04-05 21:04:07 +0900

    10410 Eligibility

---
+++ rust/src/10/10410.rs
@@ -0,0 +1,35 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut input = || input.next().unwrap();
+    let mut output = String::new();
+
+    for _ in 0..parse_int(input()) {
+        let (name, study_date, birth_date, courses) =
+            (input(), input(), input(), parse_int(input()));
+
+        let study_year = parse_int(study_date.split_once('/').unwrap().0);
+        let birth_year = parse_int(birth_date.split_once('/').unwrap().0);
+
+        if study_year >= 2010 || birth_year >= 1991 {
+            writeln!(output, "{name} eligible").unwrap();
+            continue;
+        }
+
+        if courses > 8 * 5 {
+            writeln!(output, "{name} ineligible").unwrap();
+            continue;
+        }
+
+        writeln!(output, "{name} coach petitions").unwrap();
+    }
+
+    print!("{output}");
+}
+
+fn parse_int(buf: &str) -> i32 {
+    buf.parse().unwrap()
+}
