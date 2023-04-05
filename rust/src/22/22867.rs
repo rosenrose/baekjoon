2cff0185030864c41805fed7f186c9f9f71cd4e3
rosenrose <crazytempler@gmail.com>
2023-04-05 20:24:50 +0900

    22867 종점

---
+++ rust/src/22/22867.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut input = || input.next().unwrap();
+
+    let mut times: Vec<_> = (0..parse_int(input()))
+        .flat_map(|_| {
+            let (enter, leave) = (parse_time(input()), parse_time(input()));
+            [(enter, true), (leave, false)]
+        })
+        .collect();
+    times.sort_unstable();
+    // println!("{times:?}");
+    let (mut count, mut max_count) = (0, 1);
+
+    for (_, is_enter) in times {
+        if is_enter {
+            count += 1;
+        } else {
+            count -= 1;
+        }
+
+        max_count = count.max(max_count);
+    }
+
+    println!("{max_count}");
+}
+
+fn parse_time(time: &str) -> i32 {
+    let mut tokens = time.split([':', '.']).map(parse_int);
+    let mut token = || tokens.next().unwrap();
+
+    let (hour, minute, second, millisecond) = (token(), token(), token(), token());
+    let seconds = hour * (60 * 60) + minute * 60 + second;
+
+    seconds * 1000 + millisecond
+}
+
+fn parse_int(buf: &str) -> i32 {
+    buf.parse().unwrap()
+}
