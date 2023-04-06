b9824bc49d320abeb7a8e098554417e0a130e2dd
rosenrose <crazytempler@gmail.com>
2023-04-07 00:11:35 +0900

    4752 HTML 에디터

---
+++ rust/src/04/4752.rs
@@ -0,0 +1,85 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+
+    for input in buf.lines().map(str::trim) {
+        let (b, right) = input.split_once(' ').unwrap();
+        let Some((e, text)) = right.split_once(' ') else {
+            return;
+        };
+
+        let (begin, end) = (parse_int(b), parse_int(e));
+        let selected = &text[begin..end];
+
+        let outer_tags = get_tags(&text[..begin]);
+        let inner_tags = get_tags(selected);
+        let mut output = String::new();
+        // println!("{outer_tags:?} {inner_tags:?}");
+        for out_tag in outer_tags.iter() {
+            write!(output, "<{out_tag}>").unwrap();
+        }
+
+        write!(output, "{selected}").unwrap();
+
+        for in_tag in inner_tags.iter().rev() {
+            write!(output, "</{in_tag}>").unwrap();
+        }
+
+        for out_tag in outer_tags.iter().rev() {
+            let closing = format!("</{out_tag}>");
+
+            if selected.contains(&closing) {
+                continue;
+            }
+
+            write!(output, "{closing}").unwrap();
+        }
+
+        println!("{output}");
+    }
+}
+
+fn get_tags(input: &str) -> Vec<String> {
+    let mut stack = Vec::new();
+    let mut tag = String::new();
+    let mut is_tag = false;
+    let mut is_closing = false;
+
+    for ch in input.chars() {
+        if ch == '<' {
+            is_tag = true;
+            continue;
+        }
+        if !is_tag {
+            continue;
+        }
+
+        match ch {
+            '/' => is_closing = true,
+            '>' => {
+                if is_closing {
+                    stack.pop();
+                    is_closing = false;
+                } else {
+                    stack.push(tag.clone());
+                }
+
+                tag.clear();
+                is_tag = false;
+            }
+            _ => {
+                if !is_closing {
+                    tag.push(ch);
+                }
+            }
+        }
+    }
+
+    stack
+}
+
+fn parse_int(buf: &str) -> usize {
+    buf.parse().unwrap()
+}
