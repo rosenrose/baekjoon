71d0ec7654aee536e021cb5771e4b0ad773b854b
rosenrose <crazytempler@gmail.com>
2023-04-06 14:52:37 +0900

    11816 수정

--- rust/src/11/11816.rs
+++ rust/src/11/11816.rs
@@ -2,10 +2,13 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
+    const HEX_PREFIX: &str = "0x";
+    const OCT_PREFIX: &str = "0";
+
     let dec: i32 = (match buf.trim() {
-        x if &x[..2] == "0x" => i32::from_str_radix(&x[2..], 16),
-        x if &x[..1] == "0" => i32::from_str_radix(&x[1..], 8),
-        _ => buf.trim().parse(),
+        x if x.starts_with(HEX_PREFIX) => i32::from_str_radix(&x[HEX_PREFIX.len()..], 16),
+        x if x.starts_with(OCT_PREFIX) => i32::from_str_radix(&x[OCT_PREFIX.len()..], 8),
+        x => x.parse(),
     })
     .unwrap();
 
