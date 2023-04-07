71e81f39a509de397f8cdc70af68a1b7e074f21b
rosenrose <crazytempler@gmail.com>
2023-04-07 16:52:56 +0900

    27918 탁구 경기

---
+++ rust/src/27/27918.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let (mut x, mut y) = (0_i32, 0);
+
+    for winner in buf.lines().skip(1) {
+        match winner {
+            "D" => x += 1,
+            "P" => y += 1,
+            _ => (),
+        }
+
+        if x.abs_diff(y) == 2 {
+            break;
+        }
+    }
+
+    println!("{x}:{y}");
+}
