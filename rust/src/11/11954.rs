0b7e29b7acdd4c3cee0fc9742bb2e2462be20f23
rosenrose <crazytempler@gmail.com>
2023-04-09 15:15:06 +0900

    11954 추가

--- rust/src/11/11954.rs
+++ rust/src/11/11954.rs
@@ -40,3 +40,73 @@ fn main() {
 
     print!("{output}");
 }
+
+// print!("{}", prettify_json(buf.trim(), 2, 0));
+fn prettify_json(input: &str, indent_size: usize, depth: usize) -> String {
+    let mut output = String::new();
+    let mut idx = 0;
+    let mut count = 0;
+    let mut is_item = false;
+
+    let indent = " ".repeat(indent_size * depth);
+
+    for (i, ch) in input.char_indices() {
+        match ch {
+            '{' => {
+                if count == 0 {
+                    idx = i;
+                    writeln!(output, "{indent}{ch}").unwrap();
+                }
+
+                count += 1;
+            }
+            '}' => {
+                count -= 1;
+
+                if count > 0 {
+                    continue;
+                }
+
+                write!(
+                    output,
+                    "{}",
+                    prettify_json(&input[idx + 1..i], indent_size, depth + 1)
+                )
+                .unwrap();
+                write!(output, "{indent}{ch}").unwrap();
+
+                if i == input.len() - 1 {
+                    writeln!(output, "").unwrap();
+                }
+            }
+            ',' => {
+                if count > 0 {
+                    continue;
+                }
+
+                if is_item {
+                    is_item = false;
+                    writeln!(output, "{indent}{}", &input[idx..i + 1]).unwrap();
+                } else {
+                    writeln!(output, "{ch}").unwrap();
+                }
+            }
+            _ => {
+                if count > 0 {
+                    continue;
+                }
+
+                if !is_item {
+                    is_item = true;
+                    idx = i;
+                }
+            }
+        }
+    }
+
+    if is_item {
+        writeln!(output, "{indent}{}", &input[idx..]).unwrap();
+    }
+
+    output
+}
