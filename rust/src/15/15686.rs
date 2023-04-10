2382a050fd970552340553f964287c575ad787a3
rosenrose <crazytempler@gmail.com>
2023-04-10 16:31:27 +0900

    15686 치킨 배달

---
+++ rust/src/15/15686.rs
@@ -0,0 +1,69 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+
+    let (n, m) = (input.next().unwrap(), input.next().unwrap());
+    let (mut houses, mut chickens) = (Vec::new(), Vec::new());
+
+    for r in 0..n {
+        for c in 0..n {
+            match input.next() {
+                Some(1) => houses.push((r, c)),
+                Some(2) => chickens.push((r, c)),
+                _ => (),
+            }
+        }
+    }
+
+    let min_dist = (1..=m)
+        .map(|i| combinations(0, 0, &mut vec![(0, 0); i], &chickens, &houses))
+        .min()
+        .unwrap();
+
+    println!("{min_dist}");
+}
+
+fn combinations(
+    depth: usize,
+    start: usize,
+    selected: &mut Vec<(usize, usize)>,
+    chickens: &Vec<(usize, usize)>,
+    houses: &Vec<(usize, usize)>,
+) -> usize {
+    if depth == selected.len() {
+        let chicken_dist = houses
+            .iter()
+            .map(|&(house_r, house_c)| {
+                selected
+                    .iter()
+                    .map(|(chicken_r, chicken_c)| {
+                        chicken_r.abs_diff(house_r) + chicken_c.abs_diff(house_c)
+                    })
+                    .min()
+                    .unwrap()
+            })
+            .sum();
+
+        return chicken_dist;
+    }
+
+    let takes = chickens.len() - selected.len() + 1;
+
+    chickens
+        .iter()
+        .enumerate()
+        .skip(start)
+        .take(takes)
+        .map(|(i, &chicken)| {
+            if selected[..depth].contains(&chicken) {
+                return 0;
+            }
+
+            selected[depth] = chicken;
+            combinations(depth + 1, i + 1, selected, chickens, houses)
+        })
+        .min()
+        .unwrap_or(usize::MAX)
+}
