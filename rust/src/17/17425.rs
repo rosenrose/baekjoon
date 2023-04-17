e52ae3f1d31caa03d7574df93a38ff1438d26309
rosenrose <crazytempler@gmail.com>
2023-04-17 17:28:36 +0900

    17425 약수의 합

---
+++ rust/src/17/17425.rs
@@ -0,0 +1,25 @@
+use std::fmt::Write;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.lines().flat_map(str::parse::<usize>);
+    let mut output = String::new();
+
+    const MAX: usize = 1_000_000;
+    let mut divisor_sums = vec![0; MAX + 1];
+
+    for i in 1..=MAX {
+        for j in (i..=MAX).step_by(i) {
+            divisor_sums[j] += i;
+        }
+
+        divisor_sums[i] += divisor_sums[i - 1];
+    }
+    // println!("{divisor_sums:?}");
+    for n in input.skip(1) {
+        writeln!(output, "{}", divisor_sums[n]).unwrap();
+    }
+
+    print!("{output}");
+}
