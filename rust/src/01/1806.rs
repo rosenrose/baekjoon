bc38639027009c09dd249502cfe841fa8e5f96f5
rosenrose <crazytempler@gmail.com>
2023-04-17 17:36:50 +0900

    1806 부분합

---
+++ rust/src/01/1806.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let (_, m) = (input.next(), input.next().unwrap());
+    let nums: Vec<_> = input.collect();
+
+    let (mut start, mut end) = (0, 0);
+    let mut sum = nums[start];
+    let mut min_len = usize::MAX;
+
+    loop {
+        if sum < m {
+            end += 1;
+
+            if end == nums.len() {
+                break;
+            }
+
+            sum += nums[end];
+        } else {
+            min_len = min_len.min(end - start + 1);
+            sum -= nums[start];
+            start += 1;
+        }
+    }
+
+    println!("{}", if min_len == usize::MAX { 0 } else { min_len });
+}
