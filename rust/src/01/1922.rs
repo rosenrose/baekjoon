1bb9004ae574836ac472c96af601d6b3bee54950
rosenrose <crazytempler@gmail.com>
2023-04-19 01:11:44 +0900

    1922 네트워크 연결

---
+++ rust/src/01/1922.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+struct DisjointSet(Vec<usize>);
+
+impl DisjointSet {
+    fn make(n: usize) -> Self {
+        Self((0..=n).collect())
+    }
+
+    fn find(&mut self, a: usize) -> usize {
+        if self.0[a] != a {
+            self.0[a] = self.find(self.0[a]);
+        }
+
+        self.0[a]
+    }
+
+    fn union(&mut self, a: usize, b: usize) {
+        let (a, b) = (self.find(a), self.find(b));
+
+        if a == b {
+            return;
+        }
+
+        self.0[b] = a;
+    }
+
+    fn is_same(&mut self, a: usize, b: usize) -> bool {
+        self.find(a) == self.find(b)
+    }
+}
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = || input.next().unwrap();
+
+    let (n, m) = (input() as usize, input());
+
+    let mut disjoint_set = DisjointSet::make(n);
+    let mut edges: Vec<_> = (0..m)
+        .map(|_| (input() as usize, input() as usize, input()))
+        .collect();
+
+    edges.sort_unstable_by_key(|&(_, _, weight)| weight);
+
+    let min_weight: i32 = edges
+        .iter()
+        .filter_map(|&(a, b, c)| {
+            (!disjoint_set.is_same(a, b)).then(|| {
+                disjoint_set.union(a, b);
+                c
+            })
+        })
+        .sum();
+
+    println!("{min_weight}");
+}
