64c96a6cabb135ebcc76af07f1a7cf08b8ca7379
rosenrose <crazytempler@gmail.com>
2023-04-20 18:40:43 +0900

    13900 수정

--- rust/src/13/13900.rs
+++ rust/src/13/13900.rs
@@ -2,20 +2,20 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i64>);
+    let input = buf.split_ascii_whitespace().flat_map(str::parse::<i64>);
 
-    let n = input.next().unwrap() as usize;
-    let mut sum_accum = vec![0; n];
+    let mut sum = 0;
+    let mut product_sum = 0;
 
-    let nums: Vec<_> = input
-        .enumerate()
-        .map(|(i, num)| {
-            sum_accum[i] = num + if i == 0 { 0 } else { sum_accum[i - 1] };
-            num
-        })
-        .collect();
-    // println!("{nums:?} {sum_accum:?}");
-    let product_sum: i64 = (1..n).map(|i| nums[i] * sum_accum[i - 1]).sum();
+    for (i, num) in input.skip(1).enumerate() {
+        if i == 0 {
+            sum = num;
+            continue;
+        }
+
+        product_sum += num * sum;
+        sum += num;
+    }
 
     println!("{product_sum}");
 }
