6ba3f59443d20efd27b7d558c6da6abaa0ecb854
rosenrose <crazytempler@gmail.com>
2023-04-21 15:18:08 +0900

    5568 카드 놓기

---
+++ rust/src/05/5568.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.lines();
+
+    let (_, k) = (
+        input.next(),
+        input.next().unwrap().parse::<usize>().unwrap(),
+    );
+    let nums: Vec<_> = input.collect();
+    let mut results = Vec::new();
+
+    permutations(0, &mut vec![0; k], &nums, &mut results);
+
+    results.sort();
+    results.dedup();
+
+    println!("{}", results.len());
+}
+
+fn permutations(
+    depth: usize,
+    selected: &mut Vec<usize>,
+    nums: &Vec<&str>,
+    results: &mut Vec<String>,
+) {
+    if depth == selected.len() {
+        let num: Vec<_> = selected.iter().map(|&i| nums[i]).collect();
+        results.push(num.join(""));
+
+        return;
+    }
+
+    for i in 0..nums.len() {
+        if selected[..depth].contains(&i) {
+            continue;
+        }
+
+        selected[depth] = i;
+        permutations(depth + 1, selected, nums, results);
+    }
+}
