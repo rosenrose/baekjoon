1bd18d38a696acfd8414ef170e4ac606c869a825
rosenrose <crazytempler@gmail.com>
2023-04-22 19:54:44 +0900

    10211 수정

--- rust/src/10/10211.rs
+++ rust/src/10/10211.rs
@@ -4,26 +4,15 @@ fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
 
-    let mut sums = Vec::<i32>::new();
-
     for _ in 0..input.next().unwrap() {
         let n = input.next().unwrap() as usize;
-        sums.reserve(n * (n + 1) / 2);
-
+        let mut memo = vec![0; n + 1];
         let mut max_sum = i32::MIN;
 
         for (i, num) in input.by_ref().take(n).enumerate() {
-            for j in sums.len() - i..sums.len() {
-                let sum = sums[j] + num;
-
-                max_sum = sum.max(max_sum);
-                sums.push(sum);
-            }
-
-            max_sum = num.max(max_sum);
-            sums.push(num);
+            memo[i + 1] = num.max(memo[i] + num);
+            max_sum = memo[i + 1].max(max_sum);
         }
-        sums.clear();
 
         println!("{max_sum}");
     }
