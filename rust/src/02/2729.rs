b0f3ab8f2dfef1b103af50cf770aa9a1d123e11f
rosenrose <crazytempler@gmail.com>
2023-04-22 15:32:50 +0900

    2729 수정

--- rust/src/02/2729.rs
+++ rust/src/02/2729.rs
@@ -3,16 +3,11 @@ use std::io;
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace();
-    let mut input = || input.next().unwrap();
 
-    let n: i32 = input().parse().unwrap();
+    let n: i32 = input.next().unwrap().parse().unwrap();
+    let mut input = input.flat_map(|s| i128::from_str_radix(s, 2));
 
-    for _ in 0..n {
-        let (a, b) = (
-            i128::from_str_radix(input(), 2).unwrap(),
-            i128::from_str_radix(input(), 2).unwrap(),
-        );
-
+    for (a, b) in (0..n).map(|_| (input.next().unwrap(), input.next().unwrap())) {
         println!("{:b}", a + b);
     }
 }
