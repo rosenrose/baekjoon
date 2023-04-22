acaed38e394562e70aa35ea91685e7413b70238a
rosenrose <crazytempler@gmail.com>
2023-04-22 17:01:43 +0900

    2015 수들의 합 4

---
+++ rust/src/02/2015.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let (n, k) = (input.next().unwrap() as usize, input.next().unwrap());
+    let mut sum_counts = HashMap::with_capacity(n >> 1);
+    sum_counts.insert(0, 1_i64);
+
+    let mut sum = 0;
+    let mut count = 0;
+
+    for num in input {
+        sum += num;
+        count += sum_counts.get(&(sum - k)).unwrap_or(&0);
+
+        sum_counts.entry(sum).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    println!("{count}");
+}
