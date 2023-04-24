15212253e5639c4ecd6bfe27ecdec83ad2d50d1a
rosenrose <crazytempler@gmail.com>
2023-04-24 16:32:54 +0900

    1865 웜홀

---
+++ rust/src/01/1865.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut input = || input.next().unwrap();
+
+    for _ in 0..input() {
+        let (n, m, w) = (input(), input(), input());
+        let mut edges = vec![(0, 0, 0); m * 2 + w];
+
+        for (i, (s, e, t)) in (0..m).map(|i| (i, (input(), input(), input() as i64))) {
+            edges[i * 2] = (s, e, t);
+            edges[i * 2 + 1] = (e, s, t);
+        }
+        for i in 0..w {
+            edges[i + m * 2] = (input(), input(), -(input() as i64))
+        }
+
+        println!("{}", if bellman_ford(n, &edges) { "YES" } else { "NO" });
+    }
+}
+
+fn bellman_ford(len: usize, edges: &Vec<(usize, usize, i64)>) -> bool {
+    let mut distances = vec![0; len + 1]; // 모든 정점에서 동시에 시작. https://www.acmicpc.net/board/view/72995
+
+    for i in 0..len {
+        for &(start, end, weight) in edges {
+            let new_dist = distances[start] + weight;
+
+            if distances[end] <= new_dist {
+                continue;
+            }
+
+            if i == len - 1 {
+                return true;
+            }
+
+            distances[end] = new_dist;
+        }
+    }
+
+    false
+}
