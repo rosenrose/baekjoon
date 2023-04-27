868fd5a546e45cf33ff7c6ebb115dc6b093a191f
rosenrose <crazytempler@gmail.com>
2023-04-27 16:10:20 +0900

    27959 초코바

---
+++ rust/src/27/27959.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let [n, m] = parse_int_vec(&buf)[..] else { return };
+
+    println!("{}", if n * 100 >= m { "Yes" } else { "No" });
+}
+
+fn parse_int_vec(buf: &String) -> Vec<i32> {
+    buf.split_whitespace().flat_map(str::parse).collect()
+}
