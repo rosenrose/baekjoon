51a4e12a624ccac31d48c92db2ba0eaf34b388a8
rosenrose <crazytempler@gmail.com>
2023-05-09 16:19:22 +0900

    12100 수정

--- rust/src/12/12100.rs
+++ rust/src/12/12100.rs
@@ -7,7 +7,6 @@ enum Dirs {
     Left,
 }
 
-use std::collections::HashSet;
 use std::io;
 use Dirs::*;
 
@@ -18,48 +17,48 @@ fn main() {
     let n = input.next().unwrap() as usize;
     let board: Vec<Vec<_>> = (0..n).map(|_| input.by_ref().take(n).collect()).collect();
 
-    let max_block = product(0, &mut [Default::default(); 5], &board, &n);
+    let max_block = product(0, &mut [Default::default(); 5], &board);
 
     println!("{max_block}");
 }
 
-fn product(depth: usize, selected: &mut [Dirs; 5], board: &[Vec<i32>], size: &usize) -> i32 {
+fn product(depth: usize, selected: &mut [Dirs; 5], board: &[Vec<i32>]) -> i32 {
     if depth == selected.len() {
-        return simulate(selected, board.to_owned(), size);
+        return simulate(selected, board.to_owned());
     }
 
     [Up, Right, Down, Left]
         .iter()
         .map(|&dir| {
             selected[depth] = dir;
-            product(depth + 1, selected, board, size)
+            product(depth + 1, selected, board)
         })
         .max()
         .unwrap()
 }
 
-fn simulate(selected: &mut [Dirs; 5], mut board: Vec<Vec<i32>>, &size: &usize) -> i32 {
-    let mut merged = HashSet::new();
+fn simulate(selected: &mut [Dirs; 5], mut board: Vec<Vec<i32>>) -> i32 {
+    let n = board.len();
+    let mut merged = vec![vec![false; n]; n];
 
     for dir in selected {
         match dir {
             Up => {
-                for r in 1..size {
-                    for c in 0..size {
+                for r in 1..n {
+                    for c in 0..n {
                         if board[r][c] == 0 {
                             continue;
                         }
-                        if board[0][c] == 0 {
+
+                        let Some(up) = (0..r).rfind(|&r| board[r][c] != 0) else {
                             (board[0][c], board[r][c]) = (board[r][c], board[0][c]);
                             continue;
-                        }
+                        };
 
-                        let up = (0..r).rfind(|&r| board[r][c] != 0).unwrap();
-
-                        if board[up][c] == board[r][c] && !merged.contains(&(up, c)) {
+                        if board[up][c] == board[r][c] && !merged[up][c] {
                             board[up][c] <<= 1;
                             board[r][c] = 0;
-                            merged.insert((up, c));
+                            merged[up][c] = true;
                         } else {
                             (board[up + 1][c], board[r][c]) = (board[r][c], board[up + 1][c]);
                         }
@@ -67,22 +66,21 @@ fn simulate(selected: &mut [Dirs; 5], mut board: Vec<Vec<i32>>, &size: &usize) -
                 }
             }
             Down => {
-                for r in (0..size - 1).rev() {
-                    for c in 0..size {
+                for r in (0..n - 1).rev() {
+                    for c in 0..n {
                         if board[r][c] == 0 {
                             continue;
                         }
-                        if board[size - 1][c] == 0 {
-                            (board[size - 1][c], board[r][c]) = (board[r][c], board[size - 1][c]);
+
+                        let Some(down) = (r + 1..n).find(|&r| board[r][c] != 0) else {
+                            (board[n - 1][c], board[r][c]) = (board[r][c], board[n - 1][c]);
                             continue;
-                        }
+                        };
 
-                        let down = (r + 1..size).find(|&r| board[r][c] != 0).unwrap();
-
-                        if board[down][c] == board[r][c] && !merged.contains(&(down, c)) {
+                        if board[down][c] == board[r][c] && !merged[down][c] {
                             board[down][c] <<= 1;
                             board[r][c] = 0;
-                            merged.insert((down, c));
+                            merged[down][c] = true;
                         } else {
                             (board[down - 1][c], board[r][c]) = (board[r][c], board[down - 1][c]);
                         }
@@ -90,54 +88,56 @@ fn simulate(selected: &mut [Dirs; 5], mut board: Vec<Vec<i32>>, &size: &usize) -
                 }
             }
             Right => {
-                for r in 0..size {
-                    for c in (0..size - 1).rev() {
+                for r in 0..n {
+                    for c in (0..n - 1).rev() {
                         if board[r][c] == 0 {
                             continue;
                         }
-                        if board[r][size - 1] == 0 {
-                            (board[r][size - 1], board[r][c]) = (board[r][c], board[r][size - 1]);
+
+                        let Some(right) = (c + 1..n).find(|&c| board[r][c] != 0) else {
+                            board[r].swap(n - 1, c);
                             continue;
-                        }
+                        };
 
-                        let right = (c + 1..size).find(|&c| board[r][c] != 0).unwrap();
-
-                        if board[r][right] == board[r][c] && !merged.contains(&(r, right)) {
+                        if board[r][right] == board[r][c] && !merged[r][right] {
                             board[r][right] <<= 1;
                             board[r][c] = 0;
-                            merged.insert((r, right));
+                            merged[r][right] = true;
                         } else {
-                            (board[r][right - 1], board[r][c]) = (board[r][c], board[r][right - 1]);
+                            board[r].swap(right - 1, c);
                         }
                     }
                 }
             }
             Left => {
-                for r in 0..size {
-                    for c in 1..size {
+                for r in 0..n {
+                    for c in 1..n {
                         if board[r][c] == 0 {
                             continue;
                         }
-                        if board[r][0] == 0 {
-                            (board[r][0], board[r][c]) = (board[r][c], board[r][0]);
+
+                        let Some(left) = (0..c).rfind(|&c| board[r][c] != 0) else {
+                            board[r].swap(0, c);
                             continue;
-                        }
+                        };
 
-                        let left = (0..c).rfind(|&c| board[r][c] != 0).unwrap();
-
-                        if board[r][left] == board[r][c] && !merged.contains(&(r, left)) {
+                        if board[r][left] == board[r][c] && !merged[r][left] {
                             board[r][left] <<= 1;
                             board[r][c] = 0;
-                            merged.insert((r, left));
+                            merged[r][left] = true;
                         } else {
-                            (board[r][left + 1], board[r][c]) = (board[r][c], board[r][left + 1]);
+                            board[r].swap(left + 1, c);
                         }
                     }
                 }
             }
         }
 
-        merged.clear();
+        for r in 0..n {
+            for c in 0..n {
+                merged[r][c] = false;
+            }
+        }
     }
 
     *board.iter().flatten().max().unwrap()
