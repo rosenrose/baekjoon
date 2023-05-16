6f8bc3815cc61f206c34f99514d882ade7dff7c6
rosenrose <crazytempler@gmail.com>
2023-05-16 15:38:43 +0900

    13908 수정

--- rust/src/13/13908.rs
+++ rust/src/13/13908.rs
@@ -4,28 +4,26 @@ fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
 
-    let (n, _) = (input.next().unwrap() as u32, input.next());
+    let (n, _) = (input.next().unwrap(), input.next());
     let nums: Vec<_> = input.collect();
 
-    let max = 10_usize.pow(n);
-    let mut count = 0;
-
-    for mut num in 0..max {
-        let mut digits = [false; 10];
-
-        if num < max / 10 {
-            digits[0] = true;
-        }
-
-        while num > 0 {
-            digits[num % 10] = true;
-            num /= 10;
-        }
-
-        if nums.iter().all(|&num| digits[num]) {
-            count += 1;
-        }
-    }
+    let count = product(0, n, &mut [0; 10], &nums);
 
     println!("{count}");
+}
+
+fn product(depth: usize, n: usize, digits: &mut [i32], nums: &[usize]) -> i32 {
+    if depth == n {
+        return i32::from(nums.iter().all(|&num| digits[num] > 0));
+    }
+
+    (0..10)
+        .map(|digit| {
+            digits[digit] += 1;
+            let result = product(depth + 1, n, digits, nums);
+            digits[digit] -= 1;
+
+            result
+        })
+        .sum()
 }
