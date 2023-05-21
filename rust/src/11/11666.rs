e19e3c5f76e22305cf9de21bba0b4d91e24994f1
rosenrose <crazytempler@gmail.com>
2023-05-21 17:48:10 +0900

    11666 워크스테이션 배정

---
+++ rust/src/11/11666.rs
@@ -0,0 +1,42 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = || input.next().unwrap();
+
+    let (n, lock_time) = (input() as usize, input());
+    let mut times: Vec<_> = (0..n)
+        .map(|_| {
+            let start = input();
+            let end = start + input();
+            (start, end)
+        })
+        .collect();
+    times.sort_unstable();
+
+    let mut workstations = BinaryHeap::with_capacity(n >> 1);
+    let mut unlock_count = 0;
+
+    'outer: for (start, end) in times {
+        while let Some(&Reverse(occupied_end)) = workstations.peek() {
+            if start < occupied_end {
+                break;
+            }
+
+            workstations.pop();
+
+            if start <= occupied_end + lock_time {
+                workstations.push(Reverse(end));
+                continue 'outer;
+            }
+        }
+
+        unlock_count += 1;
+        workstations.push(Reverse(end));
+    }
+
+    println!("{}", n - unlock_count);
+}
