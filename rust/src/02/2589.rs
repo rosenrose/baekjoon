84124e90aeb203625cb373cba15781188c19686a
rosenrose <crazytempler@gmail.com>
2023-05-23 19:48:12 +0900

    2589 보물섬

---
+++ rust/src/02/2589.rs
@@ -0,0 +1,72 @@
+use std::collections::VecDeque;
+use std::io;
+
+#[derive(PartialEq)]
+enum Cells {
+    Land,
+    Sea,
+}
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+
+    let (height, width) = (
+        parse_int(input.next().unwrap()),
+        parse_int(input.next().unwrap()),
+    );
+    let map: Vec<Vec<_>> = input
+        .map(|row| {
+            row.chars()
+                .map(|ch| match ch {
+                    'L' => Cells::Land,
+                    'W' => Cells::Sea,
+                    _ => unreachable!(),
+                })
+                .collect()
+        })
+        .collect();
+    let mut max_time = 0;
+
+    for y in 0..height {
+        for x in 0..width {
+            if map[y][x] == Cells::Sea {
+                continue;
+            }
+
+            let mut visited = vec![vec![false; width]; height];
+            visited[y][x] = true;
+
+            let mut time = 0;
+            let mut queue = VecDeque::from([((y, x), 0)]);
+
+            while let Some(((r, c), t)) = queue.pop_front() {
+                time = time.max(t);
+
+                let adjacents = [
+                    (r.saturating_sub(1), c),
+                    (r, c.saturating_sub(1)),
+                    ((r + 1).min(height - 1), c),
+                    (r, (c + 1).min(width - 1)),
+                ];
+
+                for &(adj_r, adj_c) in adjacents.iter().filter(|&&adj| adj != (r, c)) {
+                    if visited[adj_r][adj_c] || map[adj_r][adj_c] == Cells::Sea {
+                        continue;
+                    }
+
+                    visited[adj_r][adj_c] = true;
+                    queue.push_back(((adj_r, adj_c), t + 1));
+                }
+            }
+
+            max_time = time.max(max_time);
+        }
+    }
+
+    println!("{max_time}");
+}
+
+fn parse_int(buf: &str) -> usize {
+    buf.parse().unwrap()
+}
