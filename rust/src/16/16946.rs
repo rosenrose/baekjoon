39d4f8203a90ac7002de2dd757b239508489f3f3
rosenrose <crazytempler@gmail.com>
2023-05-25 00:54:37 +0900

    16946 수정

--- rust/src/16/16946.rs
+++ rust/src/16/16946.rs
@@ -10,17 +10,16 @@ fn main() {
         parse_int(input.next().unwrap()),
         parse_int(input.next().unwrap()),
     );
-    let mut map: Vec<Vec<_>> = input
-        .map(|row| row.as_bytes().iter().map(|ch| (ch - b'0') as i32).collect())
-        .collect();
+    let map: Vec<_> = input.map(str::as_bytes).collect();
 
     let mut visited = vec![vec![false; width as usize]; height as usize];
+    let mut counts = vec![vec![1; width as usize]; height as usize];
 
     for y in 0..height {
         for x in 0..width {
             let start = (y as usize, x as usize);
 
-            if visited[start.0][start.1] || map[start.0][start.1] != 0 {
+            if visited[start.0][start.1] || map[start.0][start.1] == b'1' {
                 continue;
             }
 
@@ -31,6 +30,7 @@ fn main() {
 
             while let Some((r, c)) = stack.pop() {
                 count += 1;
+                counts[r as usize][c as usize] = 0;
 
                 let adjacents = [
                     ((r - 1).max(0), c),
@@ -48,7 +48,7 @@ fn main() {
 
                     visited[adj.0][adj.1] = true;
 
-                    if map[adj.0][adj.1] != 0 {
+                    if map[adj.0][adj.1] == b'1' {
                         walls.push((adj_r, adj_c));
                         continue;
                     }
@@ -60,15 +60,15 @@ fn main() {
             for (r, c) in walls {
                 let wall = (r as usize, c as usize);
 
-                map[wall.0][wall.1] += count;
+                counts[wall.0][wall.1] = ((counts[wall.0][wall.1] as i32 + count) % 10) as u8;
                 visited[wall.0][wall.1] = false;
             }
         }
     }
 
-    for row in map {
+    for row in counts {
         for count in row {
-            write!(output, "{}", count % 10).unwrap();
+            write!(output, "{count}").unwrap();
         }
         writeln!(output, "").unwrap();
     }
