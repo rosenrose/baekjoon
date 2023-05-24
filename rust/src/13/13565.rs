caa22b6897ba786cbce3ca14f8652668099d4446
rosenrose <crazytempler@gmail.com>
2023-05-24 17:35:36 +0900

    13565 침투

---
+++ rust/src/13/13565.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+
+    let (height, width) = (
+        parse_int(input.next().unwrap()),
+        parse_int(input.next().unwrap()),
+    );
+    let mut map: Vec<Vec<_>> = input
+        .map(|row| row.chars().map(|ch| ch == '1').collect())
+        .collect();
+
+    for x in 0..width {
+        if map[0][x] {
+            continue;
+        }
+
+        map[0][x] = true;
+        let mut stack = vec![(0, x)];
+
+        while let Some((r, c)) = stack.pop() {
+            if r == height - 1 {
+                println!("YES");
+                return;
+            }
+
+            let adjacents = [
+                (r.saturating_sub(1), c),
+                (r, c.saturating_sub(1)),
+                ((r + 1).min(height - 1), c),
+                (r, (c + 1).min(width - 1)),
+            ];
+
+            for &(adj_r, adj_c) in adjacents.iter().filter(|&&adj| adj != (r, c)) {
+                if map[adj_r][adj_c] {
+                    continue;
+                }
+
+                map[adj_r][adj_c] = true;
+                stack.push((adj_r, adj_c));
+            }
+        }
+    }
+
+    println!("NO");
+}
+
+fn parse_int(buf: &str) -> usize {
+    buf.parse().unwrap()
+}
