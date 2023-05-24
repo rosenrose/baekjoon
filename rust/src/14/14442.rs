155f88020373baee5c1fce1f19976def21920fef
rosenrose <crazytempler@gmail.com>
2023-05-25 00:30:05 +0900

    14442 벽 부수고 이동하기 2

---
+++ rust/src/14/14442.rs
@@ -0,0 +1,50 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut it = input.by_ref().take(3).flat_map(str::parse::<i16>);
+
+    let (height, width, k) = (it.next().unwrap(), it.next().unwrap(), it.next().unwrap());
+    let map: Vec<_> = input.map(str::as_bytes).collect();
+
+    let mut visited = vec![vec![[false; 11]; width as usize]; height as usize];
+    visited[0][0][0] = true;
+
+    let mut queue = VecDeque::from([((0, 0), 1, 0)]);
+    let mut min_dist = i32::MAX;
+
+    while let Some(((r, c), dist, broken_count)) = queue.pop_front() {
+        if (r, c) == (height - 1, width - 1) {
+            min_dist = dist.min(min_dist);
+            continue;
+        }
+
+        let adjacents = [
+            ((r - 1).max(0), c),
+            (r, (c - 1).max(0)),
+            ((r + 1).min(height - 1), c),
+            (r, (c + 1).min(width - 1)),
+        ];
+
+        for &(adj_r, adj_c) in adjacents.iter().filter(|&&adj| adj != (r, c)) {
+            let adj = (adj_r as usize, adj_c as usize);
+
+            if visited[adj.0][adj.1][broken_count as usize] {
+                continue;
+            }
+
+            visited[adj.0][adj.1][broken_count as usize] = true;
+            let is_wall = map[adj.0][adj.1] == b'1';
+
+            if is_wall && broken_count == k as u8 {
+                continue;
+            }
+
+            queue.push_back(((adj_r, adj_c), dist + 1, broken_count + u8::from(is_wall)));
+        }
+    }
+
+    println!("{}", if min_dist == i32::MAX { -1 } else { min_dist });
+}
