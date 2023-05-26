e7bdcab34a5c076c4e5b987cceabd961d17ab6d4
rosenrose <crazytempler@gmail.com>
2023-05-26 20:43:35 +0900

    1303 전쟁 - 전투

---
+++ rust/src/01/1303.rs
@@ -0,0 +1,61 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+
+    let (width, height) = (
+        parse_int(input.next().unwrap()),
+        parse_int(input.next().unwrap()),
+    );
+    let map: Vec<_> = input.map(str::as_bytes).collect();
+
+    let mut visited = vec![vec![false; width]; height];
+    let (mut white, mut blue) = (0, 0);
+
+    for y in 0..height {
+        for x in 0..width {
+            if visited[y][x] {
+                continue;
+            }
+
+            visited[y][x] = true;
+            let mut count = 0;
+            let mut stack = vec![(y, x)];
+
+            while let Some((r, c)) = stack.pop() {
+                count += 1;
+
+                let adjacents = [
+                    (r.saturating_sub(1), c),
+                    (r, c.saturating_sub(1)),
+                    ((r + 1).min(height - 1), c),
+                    (r, (c + 1).min(width - 1)),
+                ];
+
+                for (adj_r, adj_c) in adjacents {
+                    if visited[adj_r][adj_c] || map[r][c] != map[adj_r][adj_c] {
+                        continue;
+                    }
+
+                    visited[adj_r][adj_c] = true;
+                    stack.push((adj_r, adj_c));
+                }
+            }
+
+            let power = count * count;
+
+            if map[y][x] == b'W' {
+                white += power;
+            } else {
+                blue += power;
+            }
+        }
+    }
+
+    println!("{white} {blue}");
+}
+
+fn parse_int(buf: &str) -> usize {
+    buf.parse().unwrap()
+}
