2c92b561cdcc4ffd5e57e75557758ee6267f7cc8
rosenrose <crazytempler@gmail.com>
2023-05-31 00:29:23 +0900

    17135 캐슬 디펜스

---
+++ rust/src/17/17135.rs
@@ -0,0 +1,113 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut input = || input.next().unwrap();
+
+    let (height, width, max_range) = (input(), input(), input());
+    let map: Vec<Vec<_>> = (0..height)
+        .map(|_| (0..width).map(|_| input() == 1).collect())
+        .collect();
+
+    let mut archers = [0; 3];
+    let mut max_count = 0;
+
+    for a in 0..width - 2 {
+        for b in a + 1..width - 1 {
+            for c in b + 1..width {
+                (archers[0], archers[1], archers[2]) = (a, b, c);
+
+                let count = simulate(map.clone(), &archers, max_range);
+                max_count = count.max(max_count);
+            }
+        }
+    }
+
+    println!("{max_count}");
+}
+
+fn simulate(mut map: Vec<Vec<bool>>, archers: &[usize], max_range: usize) -> i32 {
+    (0..map.len())
+        .map(|_| {
+            let count = attack(&mut map, archers, max_range);
+            move_down(&mut map);
+
+            count
+        })
+        .sum()
+}
+
+fn attack(map: &mut Vec<Vec<bool>>, archers: &[usize], max_range: usize) -> i32 {
+    let (width, height) = (map[0].len(), map.len());
+    let mut final_targets = Vec::with_capacity(archers.len());
+
+    for &archer in archers {
+        let mut targets = Vec::new();
+        let mut visited = vec![vec![false; width]; height];
+        visited[height - 1][archer] = true;
+
+        let mut queue = VecDeque::from([((height - 1, archer), 1)]);
+
+        while let Some(((r, c), dist)) = queue.pop_front() {
+            if dist <= max_range && map[r][c] {
+                targets.push(((r, c), dist));
+            }
+
+            if dist == max_range {
+                continue;
+            }
+
+            let adjacents = [
+                (r.saturating_sub(1), c),
+                (r, c.saturating_sub(1)),
+                ((r + 1).min(height - 1), c),
+                (r, (c + 1).min(width - 1)),
+            ];
+
+            for (adj_r, adj_c) in adjacents {
+                if visited[adj_r][adj_c] {
+                    continue;
+                }
+
+                visited[adj_r][adj_c] = true;
+                queue.push_back(((adj_r, adj_c), dist + 1));
+            }
+        }
+
+        if targets.is_empty() {
+            continue;
+        }
+
+        let (target_coord, _) = targets
+            .select_nth_unstable_by_key(0, |&((_, c), dist)| (dist, c))
+            .1;
+
+        if !final_targets.contains(target_coord) {
+            final_targets.push(*target_coord)
+        }
+    }
+
+    let count = final_targets.len() as i32;
+
+    for (r, c) in final_targets {
+        map[r][c] = false;
+    }
+
+    count
+}
+
+fn move_down(map: &mut Vec<Vec<bool>>) {
+    let (width, height) = (map[0].len(), map.len());
+
+    for r in (1..height).rev() {
+        for c in 0..width {
+            map[r][c] = map[r - 1][c];
+        }
+    }
+
+    for c in 0..width {
+        map[0][c] = false;
+    }
+}
