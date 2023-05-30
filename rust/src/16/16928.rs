7e8f5225a49bfc7f39b86bb3b0c299197e0aed2c
rosenrose <crazytempler@gmail.com>
2023-05-30 16:05:54 +0900

    16928 뱀과 사다리 게임

---
+++ rust/src/16/16928.rs
@@ -0,0 +1,44 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut input = || input.next().unwrap();
+
+    const SIZE: usize = 10 * 10;
+    let mut board: Vec<_> = (0..=SIZE).collect();
+    let (n, m) = (input(), input());
+
+    for (x, y) in (0..n).map(|_| (input(), input())) {
+        board[x] = y
+    }
+    for (u, v) in (0..m).map(|_| (input(), input())) {
+        board[u] = v;
+    }
+
+    let (start, end) = (1, SIZE);
+    let mut visited = [false; SIZE + 1];
+    visited[start] = true;
+
+    let mut queue = VecDeque::from([(start, 0)]);
+
+    while let Some((num, count)) = queue.pop_front() {
+        let next_count = count + 1;
+        let adjacents = [1, 2, 3, 4, 5, 6].map(|i| board.get(num + i));
+
+        for &&adj in adjacents.iter().flatten() {
+            if adj == end {
+                println!("{next_count}");
+                return;
+            }
+
+            if visited[adj] {
+                continue;
+            }
+
+            visited[adj] = true;
+            queue.push_back((adj, next_count));
+        }
+    }
+}
