58d37e98d41dc5b17f93bd307f0621e9c3dd761e
rosenrose <crazytempler@gmail.com>
2023-05-30 17:42:17 +0900

    17626 Four Squares

---
+++ rust/src/17/17626.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: usize = buf.trim().parse().unwrap();
+    let mut memo = [4; 50_000 + 1];
+
+    for i in (1..).take_while(|i| i * i <= n) {
+        memo[i * i] = 1;
+    }
+
+    println!("{}", square_count(n, &mut memo));
+}
+
+fn square_count(num: usize, memo: &mut [u8]) -> u8 {
+    for i in 2..=num {
+        for j in (1..).take_while(|j| j * j <= i) {
+            memo[i] = memo[i].min(1 + memo[i - j * j]);
+        }
+    }
+
+    memo[num]
+}
