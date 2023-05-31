5dd82e7eab28049cbbaf9e7e46a4a0923ba6b0d0
rosenrose <crazytempler@gmail.com>
2023-05-31 15:53:48 +0900

    2307 수정

--- rust/src/02/2307.rs
+++ rust/src/02/2307.rs
@@ -9,33 +9,74 @@ fn main() {
 
     let (n, m) = (input(), input());
     let mut adjacency_list = vec![Vec::new(); n + 1];
-    let mut edges = Vec::with_capacity(m);
 
     for (a, b, t) in (0..m).map(|_| (input(), input(), input() as i32)) {
         adjacency_list[a].push((b, t));
         adjacency_list[b].push((a, t));
-        edges.push((a, b));
     }
 
     let (start, end) = (1, n);
-    let escape_time = dijkstra(&adjacency_list, start, (0, 0))[end];
-    let max_delayed_time = edges
-        .iter()
-        .map(|&edge| dijkstra(&adjacency_list, start, edge)[end])
-        .max()
-        .unwrap();
+    let (times, prevs) = dijkstra_with_path(&adjacency_list, start);
+    let escape_time = times[end];
+
+    let mut max_delay_time = 0;
+    let mut node = end;
+
+    while node != start {
+        let parent = prevs[node];
+        let delay_time = dijkstra_except_edge(&adjacency_list, start, (parent, node))[end];
+
+        max_delay_time = delay_time.max(max_delay_time);
+        node = parent;
+    }
 
     println!(
         "{}",
-        if max_delayed_time == i32::MAX {
+        if max_delay_time == i32::MAX {
             -1
         } else {
-            max_delayed_time - escape_time
+            max_delay_time - escape_time
         }
     );
 }
 
-fn dijkstra(graph: &[Vec<(usize, i32)>], start: usize, blocked_edge: (usize, usize)) -> Vec<i32> {
+fn dijkstra_with_path(graph: &[Vec<(usize, i32)>], start: usize) -> (Vec<i32>, Vec<usize>) {
+    let mut dists = vec![i32::MAX; graph.len()];
+    dists[start] = 0;
+
+    let mut prevs = vec![0; graph.len()];
+    let mut queue = BinaryHeap::from([Reverse((0, start))]);
+
+    while let Some(Reverse((dist, node))) = queue.pop() {
+        let min_dist = dists[node];
+
+        if dist > min_dist {
+            continue;
+        }
+
+        for &(adj, weight) in &graph[node] {
+            let adj_min_dist = dists[adj];
+            let new_dist = min_dist + weight;
+
+            if new_dist >= adj_min_dist {
+                continue;
+            }
+
+            dists[adj] = new_dist;
+            prevs[adj] = node;
+
+            queue.push(Reverse((new_dist, adj)));
+        }
+    }
+
+    (dists, prevs)
+}
+
+fn dijkstra_except_edge(
+    graph: &[Vec<(usize, i32)>],
+    start: usize,
+    blocked_edge: (usize, usize),
+) -> Vec<i32> {
     let mut dists = vec![i32::MAX; graph.len()];
     dists[start] = 0;
 
