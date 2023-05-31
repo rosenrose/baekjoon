0e2d08d0d66b57f89801fb0f74f533c29f81c425
rosenrose <crazytempler@gmail.com>
2023-05-31 16:13:00 +0900

    14938 서강그라운드

---
+++ rust/src/14/14938.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut input = || input.next().unwrap();
+
+    let (n, range, m) = (input(), input() as i32, input());
+    let items: Vec<_> = (0..n).map(|_| input() as i32).collect();
+
+    let mut adjacency_matrix: Vec<Vec<_>> = (0..n)
+        .map(|i| (0..n).map(|j| if i == j { 0 } else { i32::MAX }).collect())
+        .collect();
+
+    for (a, b, len) in (0..m).map(|_| (input() - 1, input() - 1, input() as i32)) {
+        adjacency_matrix[a][b] = len;
+        adjacency_matrix[b][a] = len;
+    }
+
+    floyd_warshall(&mut adjacency_matrix);
+
+    let max_count: i32 = adjacency_matrix
+        .iter()
+        .map(|row| {
+            row.iter()
+                .enumerate()
+                .filter_map(|(i, &dist)| (dist <= range).then_some(items[i]))
+                .sum()
+        })
+        .max()
+        .unwrap();
+
+    println!("{max_count}");
+}
+
+fn floyd_warshall(graph: &mut Vec<Vec<i32>>) {
+    let len = graph.len();
+
+    for k in 0..len {
+        for i in 0..len {
+            for j in 0..len {
+                graph[i][j] = graph[i][j].min(graph[i][k].saturating_add(graph[k][j]));
+            }
+        }
+    }
+}
