bcb26aff7dbf4672cb47ee5a4e444dc09e32e25c
rosenrose <crazytempler@gmail.com>
2023-06-05 01:52:44 +0900

    14716 현수막

---
+++ rust/src/14/14716.rs
@@ -0,0 +1,59 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+
+    let (height, width) = (input.next().unwrap(), input.next().unwrap());
+    let map: Vec<Vec<_>> = (0..height)
+        .map(|_| input.by_ref().take(width).map(|num| num == 1).collect())
+        .collect();
+
+    let mut count = 0;
+    let mut visited = vec![vec![false; width]; height];
+
+    let is_pass = |r: usize, c: usize, visited: &[Vec<bool>]| visited[r][c] || !map[r][c];
+
+    for y in 0..height {
+        for x in 0..width {
+            if is_pass(y, x, &visited) {
+                continue;
+            }
+
+            visited[y][x] = true;
+            let mut stack = vec![(y, x)];
+
+            while let Some((row, col)) = stack.pop() {
+                let (up, down, left, right) = (
+                    row.saturating_sub(1),
+                    (row + 1).min(height - 1),
+                    col.saturating_sub(1),
+                    (col + 1).min(width - 1),
+                );
+                let adjacents = [
+                    (up, left),
+                    (up, col),
+                    (up, right),
+                    (row, left),
+                    (row, right),
+                    (down, left),
+                    (down, col),
+                    (down, right),
+                ];
+
+                for (adj_r, adj_c) in adjacents {
+                    if is_pass(adj_r, adj_c, &visited) {
+                        continue;
+                    }
+
+                    visited[adj_r][adj_c] = true;
+                    stack.push((adj_r, adj_c));
+                }
+            }
+
+            count += 1;
+        }
+    }
+
+    println!("{count}");
+}
