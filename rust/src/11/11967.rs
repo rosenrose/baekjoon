be295582b7c4601b1d6bcce8d94f1f535880dfe4
rosenrose <crazytempler@gmail.com>
2023-06-10 13:55:35 +0900

    11967 불켜기

---
+++ rust/src/11/11967.rs
@@ -0,0 +1,62 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut input = || input.next().unwrap();
+
+    let (n, m) = (input(), input());
+    let start = (0, 0);
+    let mut map = vec![vec![false; n]; n];
+    map[start.0][start.1] = true;
+
+    let mut switches = vec![vec![Vec::new(); n]; n];
+
+    for [y, x, a, b] in (0..m).map(|_| [(); 4].map(|_| input() - 1)) {
+        switches[y][x].push((a, b));
+    }
+
+    let mut visited = vec![vec![false; n]; n];
+    visited[start.0][start.1] = true;
+
+    let mut visited_darks = vec![vec![false; n]; n];
+    let mut count = 1;
+    let mut queue = VecDeque::from([start]);
+
+    while let Some((r, c)) = queue.pop_front() {
+        for &switch in &switches[r][c] {
+            if !map[switch.0][switch.1] {
+                map[switch.0][switch.1] = true;
+                count += 1;
+            }
+
+            if visited_darks[switch.0][switch.1] && !visited[switch.0][switch.1] {
+                visited[switch.0][switch.1] = true;
+                queue.push_back(switch);
+            }
+        }
+
+        let adjacents = [
+            (r.saturating_sub(1), c),
+            (r, c.saturating_sub(1)),
+            ((r + 1).min(n - 1), c),
+            (r, (c + 1).min(n - 1)),
+        ];
+
+        for (adj_r, adj_c) in adjacents {
+            if visited[adj_r][adj_c] {
+                continue;
+            }
+
+            if map[adj_r][adj_c] {
+                visited[adj_r][adj_c] = true;
+                queue.push_back((adj_r, adj_c));
+            } else {
+                visited_darks[adj_r][adj_c] = true;
+            }
+        }
+    }
+
+    println!("{count}");
+}
