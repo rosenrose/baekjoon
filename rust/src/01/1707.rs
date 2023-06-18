ca5accadb752c2783d14ce727f802c5c8ca140f1
rosenrose <crazytempler@gmail.com>
2023-06-18 20:52:57 +0900

    1707 수정

--- rust/src/01/1707.rs
+++ rust/src/01/1707.rs
@@ -5,50 +5,55 @@ fn main() {
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let mut input = || input.next().unwrap();
 
-    'outer: for _ in 0..input() {
+    for _ in 0..input() {
         let (v, e) = (input(), input());
         let mut adjacency_array = (vec![i32::MAX; v + 1], vec![(0, 0); e << 1]);
 
         for (i, (a, b)) in (0..e).map(|i| (i << 1, (input(), input()))) {
             let prev = adjacency_array.0[a];
             adjacency_array.0[a] = i as i32;
-            adjacency_array.1[i] = (b as i16, prev);
+            adjacency_array.1[i] = (b as i32, prev);
 
             let prev = adjacency_array.0[b];
             adjacency_array.0[b] = (i + 1) as i32;
-            adjacency_array.1[i + 1] = (a as i16, prev);
+            adjacency_array.1[i + 1] = (a as i32, prev);
         }
 
-        let mut visited = vec![None; v + 1];
+        let is_bipartite = dfs(&adjacency_array);
 
-        for start in 1..=v {
-            if visited[start].is_some() {
-                continue;
-            }
-
-            visited[start] = Some(true);
-            let mut stack = vec![start as i16];
-
-            while let Some(node) = stack.pop() {
-                let color = visited[node as usize].unwrap();
-                let mut edge = adjacency_array.0[node as usize];
-
-                while let Some(&(adj, next_edge)) = adjacency_array.1.get(edge as usize) {
-                    if let Some(adj_color) = visited[adj as usize] {
-                        if adj_color == color {
-                            println!("NO");
-                            continue 'outer;
-                        }
-                    } else {
-                        visited[adj as usize] = Some(!color);
-                        stack.push(adj);
-                    }
-
-                    edge = next_edge;
-                }
-            }
-        }
-
-        println!("YES");
+        println!("{}", if is_bipartite { "YES" } else { "NO" });
     }
+}
+
+fn dfs((nodes, edges): &(Vec<i32>, Vec<(i32, i32)>)) -> bool {
+    let mut visited = vec![None; nodes.len()];
+
+    for start in 1..nodes.len() {
+        if visited[start].is_some() {
+            continue;
+        }
+
+        visited[start] = Some(true);
+        let mut stack = vec![start as i32];
+
+        while let Some(node) = stack.pop() {
+            let color = visited[node as usize].unwrap();
+            let mut edge = nodes[node as usize];
+
+            while let Some(&(adj, next_edge)) = edges.get(edge as usize) {
+                if let Some(adj_color) = visited[adj as usize] {
+                    if adj_color == color {
+                        return false;
+                    }
+                } else {
+                    visited[adj as usize] = Some(!color);
+                    stack.push(adj);
+                }
+
+                edge = next_edge;
+            }
+        }
+    }
+
+    true
 }
