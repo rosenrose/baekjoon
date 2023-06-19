9ffcc3e30ec07d42311c9a97d9fc80791aaf410d
rosenrose <crazytempler@gmail.com>
2023-06-19 17:11:47 +0900

    23968 알고리즘 수업 - 버블 정렬 1

---
+++ rust/src/23/23968.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let [_, k] = [(); 2].map(|_| input.next().unwrap());
+    let arr: Vec<_> = input.collect();
+
+    if let Some((a, b)) = bubble_sort(arr, k) {
+        println!("{a} {b}");
+    } else {
+        println!("-1");
+    }
+}
+
+fn bubble_sort(mut arr: Vec<i32>, k: i32) -> Option<(i32, i32)> {
+    let n = arr.len();
+    let mut count = 0;
+
+    for i in 0..n - 1 {
+        for j in 0..n - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+                count += 1;
+
+                if count == k {
+                    return Some((arr[j], arr[j + 1]));
+                }
+            }
+        }
+    }
+
+    None
+}
