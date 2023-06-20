233e66c13b97637d541230b2bd0c7e560ec7cde6
rosenrose <crazytempler@gmail.com>
2023-06-21 00:29:37 +0900

    23970 알고리즘 수업 - 버블 정렬 3

---
+++ rust/src/23/23970.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let n = input.next().unwrap() as usize;
+    let arr_a: Vec<_> = input.by_ref().take(n).collect();
+    let mut count = 0;
+    let arr_b: Vec<_> = input
+        .enumerate()
+        .map(|(i, num)| {
+            if arr_a[i] == num {
+                count += 1;
+            }
+
+            num
+        })
+        .collect();
+
+    println!("{}", u8::from(bubble_sort(arr_a, arr_b, count)));
+}
+
+fn bubble_sort(mut arr_a: Vec<i32>, arr_b: Vec<i32>, mut count: usize) -> bool {
+    let n = arr_a.len();
+
+    if count == n {
+        return true;
+    }
+
+    for i in 0..n - 1 {
+        for j in 0..n - 1 - i {
+            if arr_a[j] <= arr_a[j + 1] {
+                continue;
+            }
+
+            for idx in [j, j + 1] {
+                if arr_a[idx] == arr_b[idx] {
+                    count -= 1;
+                }
+            }
+
+            arr_a.swap(j, j + 1);
+
+            for idx in [j, j + 1] {
+                if arr_a[idx] == arr_b[idx] {
+                    count += 1;
+                }
+            }
+
+            if count == n {
+                return true;
+            }
+        }
+    }
+
+    false
+}
