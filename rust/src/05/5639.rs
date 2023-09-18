81dae10f6a386c15036cbcabd28bffbdc8f1170f
rosenrose <crazytempler@gmail.com>
2023-09-18 18:30:45 +0900

    5639 수정

--- rust/src/05/5639.rs
+++ rust/src/05/5639.rs
@@ -6,31 +6,23 @@ fn main() {
     let mut output = String::new();
 
     let pre_order: Vec<_> = buf.lines().flat_map(str::parse::<i32>).collect();
-    let post_order = build_post_order(&pre_order, 0, pre_order.len() - 1);
-
-    for node in post_order {
-        writeln!(output, "{node}").unwrap();
-    }
+    visit_post_order(&pre_order, &mut output);
 
     print!("{output}");
 }
 
-fn build_post_order(pre_order: &[i32], start: usize, end: usize) -> Vec<i32> {
-    if start > end {
-        return Vec::new();
-    }
+fn visit_post_order(pre_order: &[i32], output: &mut String) {
+    let Some(&root) = pre_order.get(0) else {
+        return;
+    };
 
-    let root = pre_order[start];
-    let mut post_order = Vec::new();
-    let mut right_tree_start = start + 1;
+    let right_start = pre_order
+        .iter()
+        .position(|&node| node > root)
+        .unwrap_or(pre_order.len());
 
-    while right_tree_start <= end && pre_order[right_tree_start] < root {
-        right_tree_start += 1;
-    }
+    visit_post_order(&pre_order[1..right_start], output);
+    visit_post_order(&pre_order[right_start..], output);
 
-    post_order.extend(build_post_order(pre_order, start + 1, right_tree_start - 1));
-    post_order.extend(build_post_order(pre_order, right_tree_start, end));
-    post_order.push(root);
-
-    post_order
+    writeln!(output, "{root}").unwrap();
 }
