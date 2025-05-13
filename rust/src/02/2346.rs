9b8157e4a34d07cc25ae678c1381a348c59592ce
rosenrose <crazytempler@gmail.com>
2025-05-13 09:03:57 +0900

    2346 풍선 터뜨리기

---
+++ rust/src/02/2346.rs
@@ -0,0 +1,23 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let mut ballons = input.enumerate().skip(1).collect::<VecDeque<_>>();
+
+    while let Some((idx, num)) = ballons.pop_front() {
+        print!("{idx} ");
+
+        if ballons.is_empty() {
+            break;
+        }
+
+        if num > 0 {
+            ballons.rotate_left((num as usize - 1) % ballons.len());
+        } else {
+            ballons.rotate_right(num.abs() as usize % ballons.len());
+        }
+    }
+}
