42d4d10444e9dc0a8497e79ce6c2413929a7f544
rosenrose <crazytempler@gmail.com>
2025-05-14 10:50:04 +0900

    11284 11285 수정

--- rust/src/11/11284.rs
+++ rust/src/11/11284.rs
@@ -1,36 +1,71 @@
+const OFFSET: u32 = '가' as u32;
+const LAST_COUNT: u32 = '갛' as u32 - OFFSET + 1;
+const MIDDLE_COUNT: u32 = ('깋' as u32 - OFFSET + 1) / LAST_COUNT;
+
 fn main() {
-    const FIRSTS: [char; 19] = [
-        'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ',
-        'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
-    ];
-    const LASTS: [char; 27] = [
-        'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ',
-        'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
-    ];
-    const MEDIAL_COUNT: u32 = 21;
-    const LAST_COUNT: u32 = LASTS.len() as u32 + 1;
+    let consonants: [char; 30] = std::array::from_fn(|i| char::from_u32('ㄱ' as u32 + i as u32).unwrap());
+    let firsts: Vec<_> = consonants
+        .iter()
+        .filter(|ch| matches!(ch, 'ㄱ'..='ㄲ' | 'ㄴ' | 'ㄷ'..='ㄹ' | 'ㅁ'..='ㅃ' | 'ㅅ'..='ㅎ'))
+        .collect();
+    let lasts: Vec<_> = consonants
+        .iter()
+        .filter(|ch| !matches!(ch, 'ㄸ' | 'ㅃ' | 'ㅉ'))
+        .collect();
 
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
 
     let mut code = buf.chars().nth(0).unwrap() as u32;
-    code -= '가' as u32;
+    code -= OFFSET;
 
-    let first = FIRSTS[(code / (MEDIAL_COUNT * LAST_COUNT)) as usize];
-    let first = char::from_u32(first as u32).unwrap();
-    code %= MEDIAL_COUNT * LAST_COUNT;
+    let first = firsts[(code / (LAST_COUNT * MIDDLE_COUNT)) as usize];
+    code %= LAST_COUNT * MIDDLE_COUNT;
 
-    let medial = char::from_u32('ㅏ' as u32 + (code / LAST_COUNT)).unwrap();
+    let middle = char::from_u32('ㅏ' as u32 + (code / LAST_COUNT)).unwrap();
     code %= LAST_COUNT;
 
-    println!("{first}\n{medial}");
+    println!("{first}\n{middle}");
 
     if code == 0 {
         println!("");
-    } else {
-        let last = LASTS[(code - 1) as usize];
-        let last = char::from_u32(last as u32).unwrap();
-
-        println!("{last}");
+        return;
     }
+
+    let last = lasts[(code - 1) as usize];
+
+    println!("{last}");
 }
+/*
+      초성 종성
+ㄱ 0   0   1
+ㄲ 1   1   2
+ㄳ 2       3
+ㄴ 3   2   4
+ㄵ 4       5
+ㄶ 5       6
+ㄷ 6   3   7
+ㄸ 7   4
+ㄹ 8   5   8
+ㄺ 9       9
+ㄻ 10      10
+ㄼ 11      11
+ㄽ 12      12
+ㄾ 13      13
+ㄿ 14      14
+ㅀ 15      15
+ㅁ 16  6   16
+ㅂ 17  7   17
+ㅃ 18  8
+ㅄ 19      18
+ㅅ 20  9   19
+ㅆ 21  10  20
+ㅇ 22  11  21
+ㅈ 23  12  22
+ㅉ 24  13
+ㅊ 25  14  23
+ㅋ 26  15  24
+ㅌ 27  16  25
+ㅍ 28  17  26
+ㅎ 29  18  27
+*/
--- rust/src/11/11285.rs
+++ rust/src/11/11285.rs
@@ -1,33 +1,67 @@
 use std::io;
 
+const OFFSET: u32 = '가' as u32;
+const LAST_COUNT: u32 = '갛' as u32 - OFFSET + 1;
+const MIDDLE_COUNT: u32 = ('깋' as u32 - OFFSET + 1) / LAST_COUNT;
+
 fn main() {
-    const FIRSTS: [char; 19] = [
-        'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ',
-        'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
-    ];
-    const LASTS: [char; 27] = [
-        'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ',
-        'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
-    ];
-    const MEDIAL_COUNT: u32 = 21;
-    const LAST_COUNT: u32 = LASTS.len() as u32 + 1;
+    let consonants: [char; 30] = std::array::from_fn(|i| char::from_u32('ㄱ' as u32 + i as u32).unwrap());
+    let firsts: Vec<_> = consonants
+        .iter()
+        .filter(|ch| matches!(ch, 'ㄱ'..='ㄲ' | 'ㄴ' | 'ㄷ'..='ㄹ' | 'ㅁ'..='ㅃ' | 'ㅅ'..='ㅎ'))
+        .collect();
+    let lasts: Vec<_> = consonants
+        .iter()
+        .filter(|ch| !matches!(ch, 'ㄸ' | 'ㅃ' | 'ㅉ'))
+        .collect();
 
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.lines().map(|s| s.chars().nth(0));
     let mut input = || input.next().unwrap();
 
-    let (first, medial) = (input().unwrap(), input().unwrap());
-    let mut code = '가' as u32;
+    let (first, middle) = (input().unwrap(), input().unwrap());
+    let mut code = OFFSET;
 
-    code += FIRSTS.iter().position(|&c| c == first).unwrap() as u32 * (MEDIAL_COUNT * LAST_COUNT);
+    code += firsts.iter().position(|&&ch| ch == first).unwrap() as u32 * (LAST_COUNT * MIDDLE_COUNT);
 
-    code += (medial as u32 - 'ㅏ' as u32) * LAST_COUNT;
+    code += (middle as u32 - 'ㅏ' as u32) * LAST_COUNT;
 
-    code += if let Some(last) = input() {
-        LASTS.iter().position(|&c| c == last).unwrap() as u32 + 1
-    } else {
-        0
-    };
+    if let Some(last) = input() {
+        code += lasts.iter().position(|&&ch| ch == last).unwrap() as u32 + 1;
+    }
 
     println!("{}", char::from_u32(code).unwrap());
 }
+/*
+      초성 종성
+ㄱ 0   0   1
+ㄲ 1   1   2
+ㄳ 2       3
+ㄴ 3   2   4
+ㄵ 4       5
+ㄶ 5       6
+ㄷ 6   3   7
+ㄸ 7   4
+ㄹ 8   5   8
+ㄺ 9       9
+ㄻ 10      10
+ㄼ 11      11
+ㄽ 12      12
+ㄾ 13      13
+ㄿ 14      14
+ㅀ 15      15
+ㅁ 16  6   16
+ㅂ 17  7   17
+ㅃ 18  8
+ㅄ 19      18
+ㅅ 20  9   19
+ㅆ 21  10  20
+ㅇ 22  11  21
+ㅈ 23  12  22
+ㅉ 24  13
+ㅊ 25  14  23
+ㅋ 26  15  24
+ㅌ 27  16  25
+ㅍ 28  17  26
+ㅎ 29  18  27
+*/
