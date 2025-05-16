62b10539f8f7981c51df977a43efdef773b1f44b
rosenrose <crazytempler@gmail.com>
2025-05-16 17:30:14 +0900

    10815 10816 hash table

--- rust/src/10/10815.rs
+++ rust/src/10/10815.rs
@@ -2,12 +2,24 @@ use std::collections::HashSet;
 use std::fmt::Write;
 use std::io;
 
+const MOD: usize = 650_011;
+
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
     let mut output = String::new();
 
     let n = input.next().unwrap() as usize;
+    // let mut hash_table = [None; MOD];
+
+    // for num in input.by_ref().take(n) {
+    //     insert(num, &mut hash_table);
+    // }
+
+    // for num in input.skip(1) {
+    //     write!(output, "{} ", has(num, &hash_table) as u8).unwrap();
+    // }
+
     let mut cards = HashSet::with_capacity(n);
 
     for num in input.by_ref().take(n) {
@@ -19,4 +31,32 @@ fn main() {
     }
 
     print!("{output}");
+}
+
+fn insert(input: i32, hash_table: &mut [Option<i32>]) {
+    let mut idx = input.rem_euclid(MOD as i32) as usize;
+
+    while let Some(num) = hash_table[idx] {
+        if num == input {
+            return;
+        }
+
+        idx = (idx + 1) % MOD;
+    }
+
+    hash_table[idx] = Some(input);
+}
+
+fn has(input: i32, hash_table: &[Option<i32>]) -> bool {
+    let mut idx = input.rem_euclid(MOD as i32) as usize;
+
+    while let Some(num) = hash_table[idx] {
+        if num == input {
+            return true;
+        }
+
+        idx = (idx + 1) % MOD;
+    }
+
+    false
 }
--- rust/src/10/10816.rs
+++ rust/src/10/10816.rs
@@ -2,12 +2,30 @@ use std::collections::HashMap;
 use std::fmt::Write;
 use std::io;
 
+const MOD: usize = 650_011;
+
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
     let mut output = String::new();
 
     let n = input.next().unwrap() as usize;
+    // let mut key_table = [None; MOD];
+    // let mut value_table = [None; MOD];
+
+    // for num in input.by_ref().take(n) {
+    //     insert(num, &mut key_table, &mut value_table);
+    // }
+
+    // for num in input.skip(1) {
+    //     write!(
+    //         output,
+    //         "{} ",
+    //         get(num, &key_table, &value_table).unwrap_or(0)
+    //     )
+    //     .unwrap();
+    // }
+
     let mut counts = HashMap::with_capacity(n >> 1);
 
     for num in input.by_ref().take(n) {
@@ -19,4 +37,35 @@ fn main() {
     }
 
     print!("{output}");
+}
+
+fn insert(input: i32, key_table: &mut [Option<i32>], value_table: &mut [Option<i32>]) {
+    let mut idx = input.rem_euclid(MOD as i32) as usize;
+
+    while let Some(num) = key_table[idx] {
+        if num == input {
+            break;
+        }
+
+        idx = (idx + 1) % MOD;
+    }
+
+    key_table[idx] = Some(input);
+    let value = value_table[idx].get_or_insert(0);
+
+    *value += 1;
+}
+
+fn get(input: i32, key_table: &[Option<i32>], value_table: &[Option<i32>]) -> Option<i32> {
+    let mut idx = input.rem_euclid(MOD as i32) as usize;
+
+    while let Some(num) = key_table[idx] {
+        if num == input {
+            break;
+        }
+
+        idx = (idx + 1) % MOD;
+    }
+
+    value_table[idx]
 }
